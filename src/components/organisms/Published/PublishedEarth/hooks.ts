b498deb5bde3b9a005ef1fe9806575ca695311ca
HideBa <49897538+HideBa@users.noreply.github.com>
2021-06-04 21:27:06 +0900

    fix: ga-typo2 (#10)

--- src/components/organisms/Published/PublishedEarth/hooks.ts
+++ src/components/organisms/Published/PublishedEarth/hooks.ts
@@ -25,9 +25,9 @@ export default (alias?: string) => {
   const googleAnalyticsData: { enableGA?: boolean; trackingId?: string } = useMemo(
     () => ({
       enableGA: data?.property.googleAnalytics?.enableGA,
-      trackingId: data?.property.googleAnalytcs?.trackingId,
+      trackingId: data?.property.googleAnalytics?.trackingId,
     }),
-    [data?.property.googleAnalytcs?.trackingId, data?.property.googleAnalytics?.enableGA],
+    [data?.property.googleAnalytics?.enableGA, data?.property.googleAnalytics?.trackingId],
   );
 
   useEffect(() => {
