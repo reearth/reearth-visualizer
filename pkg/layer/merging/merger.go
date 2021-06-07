06cd4464c9b20aad30faa856bfbd36af4fed1218
yk-eukarya <81808708+yk-eukarya@users.noreply.github.com>
2021-06-08 00:33:31 +0300

    fix: Invisible layer issue in published project (#7)
    Co-authored-by: Ya Ka <eng.ymk@gmail.com>
    Co-authored-by: HideBa <49897538+HideBa@users.noreply.github.com>

--- pkg/layer/merging/merger.go
+++ pkg/layer/merging/merger.go
@@ -38,7 +38,7 @@ func (m *Merger) MergeLayer(ctx context.Context, l layer.Layer, parent *layer.Gr
 
 		children := make([]MergedLayer, 0, len(layers))
 		for _, c := range layers {
-			if c == nil {
+			if c == nil || !(*c).IsVisible() {
 				continue
 			}
 			ml, err := m.MergeLayer(ctx, *c, lg)
