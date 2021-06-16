339d613c1c1148124cd9739c7306595a83042c09
HideBa <49897538+HideBa@users.noreply.github.com>
2021-06-16 17:43:38 +0900

    fix: force logout when me query returns null (#15)
    Co-authored-by: rot1024 <aayhrot@gmail.com>

--- src/components/pages/TopPage/hooks.ts
+++ src/components/pages/TopPage/hooks.ts
@@ -14,7 +14,7 @@ export default () => {
 
   const [{ currentTeam }, setLocalState] = useLocalState(s => ({ currentTeam: s.currentTeam }));
 
-  const { data } = useTeamsQuery({ skip: !isAuthenticated });
+  const { data, loading } = useTeamsQuery({ skip: !isAuthenticated });
   const teamId = currentTeam?.id || data?.me?.myTeam.id;
 
   useEffect(() => {
@@ -24,10 +24,10 @@ export default () => {
   }, [isAuthenticated, navigate, currentTeam, setLocalState, data, teamId]);
 
   useEffect(() => {
-    if (authError) {
+    if (authError || (!loading && !data?.me)) {
       logout();
     }
-  }, [authError, logout]);
+  }, [authError, data?.me, loading, logout]);
 
   return {
     isLoading,
