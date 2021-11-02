ee655a4bb0bbcecb53c834aa4bcb4b045f211e37
rot1024 <aayhrot@gmail.com>
2021-11-02 19:51:49 +0900

    chore: fix temp script

--- .github/workflows/release.js
+++ .github/workflows/release.js
@@ -5,16 +5,15 @@ const header = "# Changelog\nAll notable changes to this project will be documen
 
 module.exports = async ({ github, tag }) => {
   const newTag = removeVFromTag(tag);
-  const releaseData = await Promise.all(repos.map(r => github.rest.repos.getReleaseByTag({
+  const releases = await Promise.all(repos.map(r => github.rest.repos.getReleaseByTag({
     owner: "reearth",
     repo: "reearth-" + r,
     tag: `v${newTag}`,
   })));
-  console.log(JSON.stringify(releaseData[0], null, "  "));
 
   // generate CHANGELOG_latest.md
   const changelogLatest = repos.flatMap((r, i) =>
-    [`## reearth-${r}`, "", releaseData[i].body, ""]
+    [`## reearth-${r}`, "", releases[i].data.body, ""]
   ).join("\n");
   writeFileSync("CHANGELOG_latest.md", changelogLatest);
 
@@ -26,7 +25,7 @@ module.exports = async ({ github, tag }) => {
     // ignore
   }
   const pos = changelog.indexOf("## "); // first version section
-  const newChangelog = `## ${newTag} - ${formatDate(new Date())}\n\n${changelogLatest.replace(/^#/gm, "##")}`;
+  const newChangelog = `${formatHeader(tag)}\n\n${changelogLatest.replace(/^#/gm, "##")}`;
   if (pos >= 0) {
     changelog = changelog.slice(0, pos) + newChangelog + "\n\n" + changelog.slice(pos);
   } else {
@@ -35,7 +34,11 @@ module.exports = async ({ github, tag }) => {
   writeFileSync("CHANGELOG.md", changelog);
 };
 
-function formatDate(d) {
+function formatHeader(version, date) {
+  return `## ${removeVFromTag(version)} - ${formatDate(date)}`;
+}
+
+function formatDate(d = new Date()) {
   return `${d.getUTCFullYear()}-${("0" + (d.getUTCMonth() + 1)).slice(-2)}-${("0" + d.getUTCDate()).slice(-2)}`;
 }
 
