a4f17f7da3ead8e9118f24e48f09b15a07972084
Basel Issmail <78056580+issmail-basel@users.noreply.github.com>
2022-01-31 09:07:20 +0200

    fix: plugin blocks cannot be deleted (#164)

--- src/components/molecules/EarthEditor/PropertyPane/PropertyItem/index.tsx
+++ src/components/molecules/EarthEditor/PropertyPane/PropertyItem/index.tsx
@@ -259,7 +259,7 @@ const PropertyItem: React.FC<Props> = ({
 
   const itemTitle = intl.formatMessage({ defaultMessage: "Basic" });
   const handleDelete = useCallback(() => {
-    if (!onRemovePane || !item?.title) return;
+    if (!onRemovePane) return;
     if (item?.title === itemTitle) {
       setModal(true);
     } else {
