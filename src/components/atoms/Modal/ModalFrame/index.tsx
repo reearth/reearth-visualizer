c8ec354bcae619029420e9cb5f6747815b0aa467
nina992 <89770889+nina992@users.noreply.github.com>
2022-04-13 10:27:54 +0300

    fix: redirect after esc button in any setting page (#193)
    * Fix the redirect after exc button in any setting page
    
    * handle esc only on opened modals
    
    * code styling change
    
    Co-authored-by: nina992 <nouralali992@gmail.com>

--- src/components/atoms/Modal/ModalFrame/index.tsx
+++ src/components/atoms/Modal/ModalFrame/index.tsx
@@ -21,7 +21,9 @@ const Modal: React.FC<Props> = ({ size, isVisible, onClose, children }) => {
     unmountOnExit: true,
   });
 
-  const handleClose = useCallback(() => onClose?.(), [onClose]);
+  const handleClose = useCallback(() => {
+    if (isVisible) onClose?.();
+  }, [onClose, isVisible]);
 
   useKeyPressEvent("Escape", handleClose);
 
