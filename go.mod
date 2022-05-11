ddaeaaee6f3591717c7f162fd19d48dbcf743b44
rot1024 <aayhrot@gmail.com>
2022-05-11 20:15:31 +0900

    refactor: retry mongo lock (#145)

--- go.mod
+++ go.mod
@@ -61,6 +61,7 @@ require (
 	github.com/agnivade/levenshtein v1.1.1 // indirect
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
 	github.com/alecthomas/units v0.0.0-20210927113745-59d0afb8317a // indirect
+	github.com/avast/retry-go/v4 v4.0.4 // indirect
 	github.com/caos/logging v0.0.2 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- internal/infrastructure/mongo/lock.go
+++ internal/infrastructure/mongo/lock.go
@@ -3,10 +3,9 @@ package mongo
 import (
 	"context"
 	"errors"
-	"math/rand"
 	"sync"
-	"time"
 
+	"github.com/avast/retry-go/v4"
 	"github.com/google/uuid"
 	"github.com/reearth/reearth-backend/internal/usecase/repo"
 	"github.com/reearth/reearth-backend/pkg/log"
@@ -22,10 +21,7 @@ type Lock struct {
 }
 
 func NewLock(c *mongo.Collection) (repo.Lock, error) {
-	hostid, err := uuidString()
-	if err != nil {
-		return nil, err
-	}
+	hostid := uuid.NewString()
 
 	l := lock.NewClient(c)
 	if err := l.CreateIndexes(context.Background()); err != nil {
@@ -43,31 +39,15 @@ func (r *Lock) Lock(ctx context.Context, name string) error {
 		return repo.ErrAlreadyLocked
 	}
 
-	lockID, err := uuidString()
-	if err != nil {
-		return err
-	}
-
+	lockID := uuid.NewString()
 	log.Infof("lock: trying to lock: id=%s, name=%s, host=%s", name, lockID, r.hostid)
-	// wait and retry
-	const retry = 10
-	for i := 0; i < retry; i++ {
-		if err := r.l.XLock(ctx, name, lockID, r.details()); err != nil {
-			if errors.Is(err, lock.ErrAlreadyLocked) {
-				log.Infof("lock: failed to lock (%d/%d): name=%s, id=%s, host=%s", i+1, retry, name, lockID, r.hostid)
-				if i >= retry {
-					return repo.ErrFailedToLock
-				}
 
-				time.Sleep(time.Second * time.Duration(rand.Intn(1)+(i+1)))
-				continue
-			}
-
-			log.Infof("lock: failed to lock: name=%s, id=%s, host=%s, err=%s", name, lockID, r.hostid, err)
-			return repo.ErrFailedToLock
-		} else {
-			break
-		}
+	if err := retry.Do(
+		func() error { return r.l.XLock(ctx, name, lockID, r.details()) },
+		retry.RetryIf(func(err error) bool { return errors.Is(err, lock.ErrAlreadyLocked) }),
+	); err != nil {
+		log.Infof("lock: failed to lock: name=%s, id=%s, host=%s, err=%s", name, lockID, r.hostid, err)
+		return repo.ErrFailedToLock
 	}
 
 	r.setLockID(name, lockID)
@@ -88,15 +68,6 @@ func (r *Lock) Unlock(ctx context.Context, name string) error {
 	r.deleteLockID(name)
 	log.Infof("lock: unlocked: name=%s, id=%s, host=%s", name, lockID, r.hostid)
 	return nil
-}
-
-func uuidString() (string, error) {
-	u, err := uuid.NewUUID()
-	if err != nil {
-		return "", rerror.ErrInternalBy(err)
-	}
-
-	return u.String(), nil
 }
 
 func (r *Lock) details() lock.LockDetails {
