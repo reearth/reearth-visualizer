35f9db587c2f2d396db0a6f6398c879db7667eec
KeisukeYamashita <19yamashita15@gmail.com>
2022-05-11 11:02:11 +0900

    test: add Mongo Asset's `FindByID` unit testing (#139)

---
+++ internal/infrastructure/mongo/mongo_test.go
@@ -0,0 +1,43 @@
+package mongo
+
+import (
+	"context"
+	"encoding/hex"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/reearth/reearth-backend/internal/infrastructure/mongo/mongodoc"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/mongo/driver/uuid"
+)
+
+func connect(t *testing.T) func() (*mongodoc.Client, func()) {
+	t.Helper()
+
+	// Skip unit testing if "REEARTH_DB" is not configured
+	// See details: https://github.com/reearth/reearth/issues/273
+	db := os.Getenv("REEARTH_DB")
+	if db == "" {
+		t.SkipNow()
+		return nil
+	}
+
+	c, _ := mongo.Connect(
+		context.Background(),
+		options.Client().
+			ApplyURI(db).
+			SetConnectTimeout(time.Second*10),
+	)
+
+	return func() (*mongodoc.Client, func()) {
+		database, _ := uuid.New()
+		databaseName := "reearth-test-" + hex.EncodeToString(database[:])
+		client := mongodoc.NewClient(databaseName, c)
+
+		return client, func() {
+			_ = c.Database(databaseName).Drop(context.Background())
+		}
+	}
+}
