ca06266bd121492ae924544d667a605daf00906f
rot1024 <aayhrot@gmail.com>
2022-11-10 22:40:10 +0900

    feat(server): suppprt h2c

--- server/internal/app/graphql.go
+++ server/internal/app/graphql.go
@@ -2,11 +2,13 @@ package app
 
 import (
 	"context"
+	"time"
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/handler/extension"
 	"github.com/99designs/gqlgen/graphql/handler/lru"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/labstack/echo/v4"
 	"github.com/ravilushqa/otelgqlgen"
 	"github.com/reearth/reearth/server/internal/adapter"
@@ -14,7 +16,11 @@ import (
 	"github.com/vektah/gqlparser/v2/gqlerror"
 )
 
-const enableDataLoaders = true
+const (
+	enableDataLoaders = true
+	maxUploadSize     = 10 * 1024 * 1024 * 1024 // 10GB
+	maxMemorySize     = 100 * 1024 * 1024       // 100MB
+)
 
 func GraphqlAPI(
 	conf GraphQLConfig,
@@ -24,7 +30,26 @@ func GraphqlAPI(
 		Resolvers: gql.NewResolver(),
 	})
 
-	srv := handler.NewDefaultServer(schema)
+	srv := handler.New(schema)
+
+	srv.AddTransport(transport.Websocket{
+		KeepAlivePingInterval: 10 * time.Second,
+	})
+	srv.AddTransport(transport.Options{})
+	srv.AddTransport(transport.GET{})
+	srv.AddTransport(transport.POST{})
+	srv.AddTransport(transport.MultipartForm{
+		MaxUploadSize: maxUploadSize,
+		MaxMemory:     maxMemorySize,
+	})
+
+	srv.SetQueryCache(lru.New(1000))
+
+	srv.Use(extension.Introspection{})
+	srv.Use(extension.AutomaticPersistedQuery{
+		Cache: lru.New(100),
+	})
+
 	srv.Use(otelgqlgen.Middleware())
 
 	if conf.ComplexityLimit > 0 {
