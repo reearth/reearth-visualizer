22575864ab8324c6610888fee9b49759fd79d367
lby <icesunex@hotmail.com>
2022-11-18 09:03:34 +0800

    fix: correct flyToGround destination camera (#356)
    * fix: correct destination camera
    
    * refactor: clean up
    
    * refactor: clean up

--- src/components/molecules/Visualizer/Engine/Cesium/common.ts
+++ src/components/molecules/Visualizer/Engine/Cesium/common.ts
@@ -37,6 +37,7 @@ import { tweenInterval } from "@reearth/util/raf";
 import { Camera } from "@reearth/util/value";
 
 import { CameraOptions, Clock } from "../../Plugin/types";
+import { FlyToDestination } from "../ref";
 
 export const layerIdField = `__reearth_layer_id`;
 
@@ -630,12 +631,13 @@ export function moveRight(scene: Scene, amount: number) {
 }
 
 export async function moveOverTerrain(viewer: Viewer, offset = 0) {
-  const camera = viewer.scene.camera;
-  const height = await sampleTerrainHeight(viewer.scene, camera.position);
+  const camera = getCamera(viewer);
+  if (camera?.lng === undefined || camera?.lat === undefined) return;
+  const height = await sampleTerrainHeight(viewer.scene, camera.lng, camera.lat);
   if (height && height !== 0) {
     const innerCamera = getCamera(viewer);
     if (innerCamera && innerCamera?.height < height + offset) {
-      camera.moveUp(height + offset - innerCamera.height);
+      viewer.scene.camera.moveUp(height + offset - innerCamera.height);
     }
   }
 }
@@ -643,28 +645,20 @@ export async function moveOverTerrain(viewer: Viewer, offset = 0) {
 export async function flyToGround(
   viewer: Viewer,
   cancelCameraFlight: MutableRefObject<(() => void) | undefined>,
-  camera?: {
-    lat?: number;
-    lng?: number;
-    height?: number;
-    heading?: number;
-    pitch?: number;
-    roll?: number;
-    fov?: number;
-  },
+  camera: FlyToDestination,
   options?: {
     duration?: number;
     easing?: (time: number) => number;
   },
   offset = 0,
 ) {
-  const height = await sampleTerrainHeight(viewer.scene, viewer.scene.camera.position);
+  if (camera.lng === undefined || camera.lat === undefined) return;
+  const height = await sampleTerrainHeight(viewer.scene, camera.lng, camera.lat);
   const tarHeight = height ? height + offset : offset;
-  const groundCamera = { ...camera, height: tarHeight };
   cancelCameraFlight.current?.();
   cancelCameraFlight.current = flyTo(
     viewer.scene?.camera,
-    { ...getCamera(viewer), ...groundCamera },
+    { ...getCamera(viewer), ...camera, height: tarHeight },
     options,
   );
 }
@@ -689,13 +683,14 @@ function rotateVectorAboutAxis(vector: Cartesian3, rotateAxis: Cartesian3, rotat
 
 async function sampleTerrainHeight(
   scene: Scene,
-  position: Cartesian3,
+  lng: number,
+  lat: number,
 ): Promise<number | undefined> {
   const terrainProvider = scene.terrainProvider;
   if (terrainProvider instanceof EllipsoidTerrainProvider) return 0;
 
   const [sample] = await sampleTerrainMostDetailed(terrainProvider, [
-    Cartographic.fromCartesian(position, scene.globe.ellipsoid, new Cartographic()),
+    Cartographic.fromDegrees(lng, lat),
   ]);
   return sample.height;
 }
