7e18b82870171bd2d52f214fb4516743d8a62779
keiya sasaki <34934510+keiya01@users.noreply.github.com>
2022-12-19 15:40:39 +0900

    refactor: move feature context (#381)

--- src/core/engines/Cesium/index.tsx
+++ src/core/engines/Cesium/index.tsx
@@ -80,107 +80,103 @@ const Cesium: React.ForwardRefRenderFunction<EngineRef, EngineProps> = (
   });
 
   return (
-    <featureContext.Provider value={context}>
-      <Viewer
-        ref={cesium}
-        onUpdate={handleUpdate}
-        className={className}
-        animation
-        timeline
-        fullscreenButton={false}
-        homeButton={false}
-        geocoder={false}
-        infoBox={false}
-        baseLayerPicker={false}
-        navigationHelpButton={false}
-        projectionPicker={false}
-        sceneModePicker={false}
-        creditContainer={creditContainer}
-        style={{
-          width: small ? "300px" : "auto",
-          height: small ? "300px" : "100%",
-          display: ready ? undefined : "none",
-          cursor: isLayerDragging ? "grab" : undefined,
-          ...style,
-        }}
-        requestRenderMode={!property?.timeline?.animation && !isLayerDraggable && !shouldRender}
-        maximumRenderTimeChange={
-          !property?.timeline?.animation && !isLayerDraggable && !shouldRender
-            ? Infinity
-            : undefined
+    <Viewer
+      ref={cesium}
+      onUpdate={handleUpdate}
+      className={className}
+      animation
+      timeline
+      fullscreenButton={false}
+      homeButton={false}
+      geocoder={false}
+      infoBox={false}
+      baseLayerPicker={false}
+      navigationHelpButton={false}
+      projectionPicker={false}
+      sceneModePicker={false}
+      creditContainer={creditContainer}
+      style={{
+        width: small ? "300px" : "auto",
+        height: small ? "300px" : "100%",
+        display: ready ? undefined : "none",
+        cursor: isLayerDragging ? "grab" : undefined,
+        ...style,
+      }}
+      requestRenderMode={!property?.timeline?.animation && !isLayerDraggable && !shouldRender}
+      maximumRenderTimeChange={
+        !property?.timeline?.animation && !isLayerDraggable && !shouldRender ? Infinity : undefined
+      }
+      shadows={!!property?.atmosphere?.shadows}
+      onClick={handleClick}
+      onDoubleClick={mouseEventHandles.doubleclick}
+      onMouseDown={mouseEventHandles.mousedown}
+      onMouseUp={mouseEventHandles.mouseup}
+      onRightClick={mouseEventHandles.rightclick}
+      onRightDown={mouseEventHandles.rightdown}
+      onRightUp={mouseEventHandles.rightup}
+      onMiddleClick={mouseEventHandles.middleclick}
+      onMiddleDown={mouseEventHandles.middledown}
+      onMiddleUp={mouseEventHandles.middleup}
+      onMouseMove={mouseEventHandles.mousemove}
+      onMouseEnter={mouseEventHandles.mouseenter}
+      onMouseLeave={mouseEventHandles.mouseleave}
+      onWheel={mouseEventHandles.wheel}>
+      <Event onMount={handleMount} onUnmount={handleUnmount} />
+      <Clock property={property} onTick={onTick} />
+      <ImageryLayers tiles={property?.tiles} cesiumIonAccessToken={cesiumIonAccessToken} />
+      <Entity>
+        <Indicator property={property} />
+      </Entity>
+      <ScreenSpaceEventHandler useDefault>
+        {/* remove default click event */}
+        <ScreenSpaceEvent type={ScreenSpaceEventType.LEFT_CLICK} />
+        {/* remove default double click event */}
+        <ScreenSpaceEvent type={ScreenSpaceEventType.LEFT_DOUBLE_CLICK} />
+      </ScreenSpaceEventHandler>
+      <ScreenSpaceCameraController
+        maximumZoomDistance={
+          property?.cameraLimiter?.cameraLimitterEnabled
+            ? property.cameraLimiter?.cameraLimitterTargetArea?.height ?? Number.POSITIVE_INFINITY
+            : Number.POSITIVE_INFINITY
         }
-        shadows={!!property?.atmosphere?.shadows}
-        onClick={handleClick}
-        onDoubleClick={mouseEventHandles.doubleclick}
-        onMouseDown={mouseEventHandles.mousedown}
-        onMouseUp={mouseEventHandles.mouseup}
-        onRightClick={mouseEventHandles.rightclick}
-        onRightDown={mouseEventHandles.rightdown}
-        onRightUp={mouseEventHandles.rightup}
-        onMiddleClick={mouseEventHandles.middleclick}
-        onMiddleDown={mouseEventHandles.middledown}
-        onMiddleUp={mouseEventHandles.middleup}
-        onMouseMove={mouseEventHandles.mousemove}
-        onMouseEnter={mouseEventHandles.mouseenter}
-        onMouseLeave={mouseEventHandles.mouseleave}
-        onWheel={mouseEventHandles.wheel}>
-        <Event onMount={handleMount} onUnmount={handleUnmount} />
-        <Clock property={property} onTick={onTick} />
-        <ImageryLayers tiles={property?.tiles} cesiumIonAccessToken={cesiumIonAccessToken} />
+        enableCollisionDetection={!property?.default?.allowEnterGround}
+      />
+      <Camera
+        onChange={handleCameraChange}
+        percentageChanged={0.2}
+        onMoveEnd={handleCameraMoveEnd}
+      />
+      {cameraViewBoundaries && property?.cameraLimiter?.cameraLimitterShowHelper && (
         <Entity>
-          <Indicator property={property} />
+          <PolylineGraphics
+            positions={cameraViewBoundaries}
+            width={1}
+            material={Color.RED}
+            arcType={ArcType.RHUMB}
+          />
         </Entity>
-        <ScreenSpaceEventHandler useDefault>
-          {/* remove default click event */}
-          <ScreenSpaceEvent type={ScreenSpaceEventType.LEFT_CLICK} />
-          {/* remove default double click event */}
-          <ScreenSpaceEvent type={ScreenSpaceEventType.LEFT_DOUBLE_CLICK} />
-        </ScreenSpaceEventHandler>
-        <ScreenSpaceCameraController
-          maximumZoomDistance={
-            property?.cameraLimiter?.cameraLimitterEnabled
-              ? property.cameraLimiter?.cameraLimitterTargetArea?.height ?? Number.POSITIVE_INFINITY
-              : Number.POSITIVE_INFINITY
-          }
-          enableCollisionDetection={!property?.default?.allowEnterGround}
-        />
-        <Camera
-          onChange={handleCameraChange}
-          percentageChanged={0.2}
-          onMoveEnd={handleCameraMoveEnd}
-        />
-        {cameraViewBoundaries && property?.cameraLimiter?.cameraLimitterShowHelper && (
-          <Entity>
-            <PolylineGraphics
-              positions={cameraViewBoundaries}
-              width={1}
-              material={Color.RED}
-              arcType={ArcType.RHUMB}
-            />
-          </Entity>
-        )}
-        {cameraViewOuterBoundaries && property?.cameraLimiter?.cameraLimitterShowHelper && (
-          <Entity>
-            <PolylineGraphics
-              positions={cameraViewOuterBoundaries}
-              width={1}
-              material={cameraViewBoundariesMaterial}
-              arcType={ArcType.RHUMB}
-            />
-          </Entity>
-        )}
-        <Scene backgroundColor={backgroundColor} />
-        <SkyBox show={property?.default?.skybox ?? true} />
-        <Fog
-          enabled={property?.atmosphere?.fog ?? true}
-          density={property?.atmosphere?.fog_density}
-        />
-        <Sun show={property?.atmosphere?.enable_sun ?? true} />
-        <SkyAtmosphere show={property?.atmosphere?.sky_atmosphere ?? true} />
-        <Globe property={property} cesiumIonAccessToken={cesiumIonAccessToken} />
-        {ready ? children : null}
-      </Viewer>
-    </featureContext.Provider>
+      )}
+      {cameraViewOuterBoundaries && property?.cameraLimiter?.cameraLimitterShowHelper && (
+        <Entity>
+          <PolylineGraphics
+            positions={cameraViewOuterBoundaries}
+            width={1}
+            material={cameraViewBoundariesMaterial}
+            arcType={ArcType.RHUMB}
+          />
+        </Entity>
+      )}
+      <Scene backgroundColor={backgroundColor} />
+      <SkyBox show={property?.default?.skybox ?? true} />
+      <Fog
+        enabled={property?.atmosphere?.fog ?? true}
+        density={property?.atmosphere?.fog_density}
+      />
+      <Sun show={property?.atmosphere?.enable_sun ?? true} />
+      <SkyAtmosphere show={property?.atmosphere?.sky_atmosphere ?? true} />
+      <Globe property={property} cesiumIonAccessToken={cesiumIonAccessToken} />
+      <featureContext.Provider value={context}>{ready ? children : null}</featureContext.Provider>
+    </Viewer>
   );
 };
 
