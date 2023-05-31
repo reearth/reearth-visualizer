77bfad354e0efcf8d5ff6810e6babaeb56821703
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-05-31 11:16:27 +0900

    delete test component

--- web/src/beta/components/CoolestComponent.tsx
+++
@@ -1,9 +0,0 @@
-const CoolestComponent = () => {
-  function CoolFunction(a: number, b: number) {
-    return a + b;
-  }
-  console.log(CoolFunction(1, 2));
-  return <div>CoolestComponent</div>;
-};
-
-export default CoolestComponent;
