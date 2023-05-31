c29ad13e37c15d11871acbd03eb26dafd2f6f8bf
koji endo <kouendou@fab.pasona.tech>
2023-05-31 14:55:34 +0900

    feat(web): copy Icon component from classic.

---
+++ web/src/beta/components/Icon/icons.ts
@@ -0,0 +1,287 @@
+/* eslint-disable import/order */
+
+// Primitives
+import Layer from "./Icons/layerIcon.svg";
+import LayerAdd from "./Icons/layerAddIcon.svg";
+import PrimMarker from "./Icons/primMarkerIcon.svg";
+import PrimPhotoOverlay from "./Icons/primPhotoIcon.svg";
+import PrimResource from "./Icons/primResourceIcon.svg";
+import PrimSphere from "./Icons/primSphereIcon.svg";
+import PrimRect from "./Icons/primRect.svg";
+import PrimModel from "./Icons/primModel.svg";
+import PrimTileset from "./Icons/primTileset.svg";
+
+// Cluster
+import Cluster from "./Icons/cluster.svg";
+
+// Built-in Widgets
+import Widgets from "./Icons/widgetsIcon.svg";
+import WidgetMenu from "./Icons/widgetMenu.svg";
+import WidgetButton from "./Icons/widgetButton.svg";
+import WidgetStory from "./Icons/widgetStorytelling.svg";
+import StorytellingMenu from "./Icons/storytellingMenu.svg";
+import WidgetSplash from "./Icons/widgetSplashscreen.svg";
+
+// Workspace
+import Workspaces from "./Icons/workspaces.svg";
+import WorkspaceAdd from "./Icons/workspaceAdd.svg";
+import MemberAdd from "./Icons/memberAdd.svg";
+
+// Dataset
+import Dataset from "./Icons/datasetIcon.svg";
+import DatasetAdd from "./Icons/datasetAdd.svg";
+import File from "./Icons/fileIcon.svg";
+import PcIcon from "./Icons/pcIcon.svg";
+import GoogleDriveIcon from "./Icons/googleDriveIcon.svg";
+import SheetFileIcon from "./Icons/sheet-file.svg";
+import Update from "./Icons/update.svg";
+import Download from "./Icons/download.svg";
+import Spinner from "./Icons/spinner.svg";
+
+// Asset
+import AssetGrid from "./Icons/assetGrid.svg";
+import AssetGridSmall from "./Icons/assetGridSmall.svg";
+import AssetList from "./Icons/assetList.svg";
+import FilterName from "./Icons/filterName.svg";
+import FilterNameReverse from "./Icons/filterNameReverse.svg";
+import FilterSize from "./Icons/filterSize.svg";
+import FilterSizeReverse from "./Icons/filterSizeReverse.svg";
+import FilterTime from "./Icons/filterTime.svg";
+import FilterTimeReverse from "./Icons/filterTimeReverse.svg";
+import AssetNoSupport from "./Icons/assetNoSupport.svg";
+
+// Titles/Sections
+import Scene from "./Icons/sceneIcon.svg";
+import Infobox from "./Icons/infoboxIcon.svg";
+import Export from "./Icons/exportIcon.svg";
+import Dashboard from "./Icons/dashboardIcon.svg";
+import EarthEditor from "./Icons/scene.svg";
+import Settings from "./Icons/settings.svg";
+
+// Infobox Blocks
+import InfoVideo from "./Icons/infoboxVideoIcon.svg";
+import InfoText from "./Icons/infoboxTextIcon.svg";
+import InfoTable from "./Icons/infoboxTableIcon.svg";
+import InfoLocation from "./Icons/infoboxLocationIcon.svg";
+import InfoHTML from "./Icons/infoboxHTMLIcon.svg";
+
+// Arrows
+import ArrowToggle from "./Icons/arrowToggle.svg";
+import ArrowUp from "./Icons/arrowUp.svg";
+import ArrowDown from "./Icons/arrowDown.svg";
+import ArrowRight from "./Icons/arrowRight.svg";
+import ArrowLeft from "./Icons/arrowLeft.svg";
+import ArrowSelect from "./Icons/arrowSelect.svg";
+import ArrowLong from "./Icons/arrowLong.svg";
+import ArrowLongLeft from "./Icons/arrowLongLeft.svg";
+import ArrowLongRight from "./Icons/arrowLongRight.svg";
+import ArrowUpDown from "./Icons/arrowUpDown.svg";
+
+// Fields / Actions
+import TextLeft from "./Icons/textAlignLeft.svg";
+import TextRight from "./Icons/textAlignRight.svg";
+import TextCenter from "./Icons/textAlignCenter.svg";
+import TextJustify from "./Icons/textAlignJustify.svg";
+import TextItalic from "./Icons/textItalic.svg";
+import TextBold from "./Icons/textBold.svg";
+import TextUnderline from "./Icons/textUnderline.svg";
+import CameraCapture from "./Icons/captureIcon.svg";
+import Visible from "./Icons/viewIcon.svg";
+import NotVisible from "./Icons/viewNotIcon.svg";
+import Upload from "./Icons/upload.svg";
+import Logout from "./Icons/logoutIcon.svg";
+import FolderAdd from "./Icons/folderAddIcon.svg";
+import Folder from "./Icons/folderIcon.svg";
+import Minus from "./Icons/minus.svg";
+import Plus from "./Icons/plus.svg";
+import PlusSquare from "./Icons/plusSquare.svg";
+import Edit from "./Icons/editIcon.svg";
+import Bin from "./Icons/binIcon.svg";
+import Cancel from "./Icons/cancel.svg";
+import Link from "./Icons/linkIcon.svg";
+import Publish from "./Icons/publish.svg";
+import Unpublish from "./Icons/unpublish.svg";
+import NewProject from "./Icons/newProject.svg";
+import NewWorkspace from "./Icons/newWorkspace.svg";
+import Preview from "./Icons/preview.svg";
+import Install from "./Icons/install.svg";
+import CopyCode from "./Icons/copyCode.svg";
+import FieldClear from "./Icons/fieldClear.svg";
+import FieldLinked from "./Icons/fieldLinked.svg";
+import ZoomToLayer from "./Icons/zoomToLayer.svg";
+
+// Misc
+import Logo from "./Icons/reearthLogo.svg";
+import LogoColorful from "./Icons/reearthLogoColorful.svg";
+import Camera from "./Icons/camera.svg";
+import Help from "./Icons/helpIcon.svg";
+import Alert from "./Icons/alert.svg";
+import QuestionMark from "./Icons/questionMarkIcon.svg";
+import CheckCircle from "./Icons/checkCircle.svg";
+import CheckMark from "./Icons/check.svg";
+import Search from "./Icons/search.svg";
+import Avatar from "./Icons/avatar.svg";
+import Scale from "./Icons/scale.svg";
+import MapIcon from "./Icons/map.svg";
+import NoProjects from "./Icons/noProjects.svg";
+import MenuForDevice from "./Icons/menuForDevice.svg";
+import Moon from "./Icons/moon.svg";
+import Sun from "./Icons/sun.svg";
+import CheckCircle2 from "./Icons/checkCircle2.svg";
+import MailCircle from "./Icons/mailCircle.svg";
+import Tag from "./Icons/tag.svg";
+import Resize from "./Icons/resize.svg";
+
+// Plug-ins
+import Plugin from "./Icons/plugin.svg";
+import UploadZipPlugin from "./Icons/uploadZipPlugin.svg";
+import PublicGitHubRepo from "./Icons/publicGitHubRepo.svg";
+import PrivateGitHubRepo from "./Icons/privateGitHubRepo.svg";
+import Marketplace from "./Icons/marketplace.svg";
+
+// Indicator
+import Crosshair from "./Icons/crosshair.svg";
+
+// Timeline
+import Timeline from "./Icons/timeline.svg";
+import PlayRight from "./Icons/play-right.svg";
+import PlayLeft from "./Icons/play-left.svg";
+import Ellipse from "./Icons/ellipse.svg";
+
+// Navigator
+import WidgetNavigator from "./Icons/widgetNavigator.svg";
+import NavigatorAngle from "./Icons/navigatorAngle.svg";
+import Compass from "./Icons/compass.svg";
+import CompassFocus from "./Icons/compassFocus.svg";
+import House from "./Icons/house.svg";
+
+export default {
+  outline: Layer,
+  layer: Layer,
+  layerAdd: LayerAdd,
+  marker: PrimMarker,
+  photooverlay: PrimPhotoOverlay,
+  resource: PrimResource,
+  ellipsoid: PrimSphere,
+  rect: PrimRect,
+  model: PrimModel,
+  tileset: PrimTileset,
+  cluster: Cluster,
+  widget: Widgets,
+  widgets: Widgets,
+  menu: WidgetMenu,
+  button: WidgetButton,
+  storytelling: WidgetStory,
+  storytellingMenu: StorytellingMenu,
+  splashscreen: WidgetSplash,
+  workspaces: Workspaces,
+  workspaceAdd: WorkspaceAdd,
+  memberAdd: MemberAdd,
+  dataset: Dataset,
+  datasetAdd: DatasetAdd,
+  file: File,
+  update: Update,
+  download: Download,
+  spinner: Spinner,
+  googleDrive: GoogleDriveIcon,
+  sheetFile: SheetFileIcon,
+  computer: PcIcon,
+  assetNoSupport: AssetNoSupport,
+  assetGrid: AssetGrid,
+  assetGridSmall: AssetGridSmall,
+  assetList: AssetList,
+  filterName: FilterName,
+  filterNameReverse: FilterNameReverse,
+  filterSize: FilterSize,
+  filterSizeReverse: FilterSizeReverse,
+  filterTime: FilterTime,
+  filterTimeReverse: FilterTimeReverse,
+  scene: Scene,
+  dashboard: Dashboard,
+  earthEditor: EarthEditor,
+  settings: Settings,
+  export: Export,
+  infobox: Infobox,
+  text: InfoText,
+  html: InfoHTML,
+  video: InfoVideo,
+  image: PrimPhotoOverlay,
+  location: InfoLocation,
+  arrowLong: ArrowLong,
+  arrowLongLeft: ArrowLongLeft,
+  arrowLongRight: ArrowLongRight,
+  arrowToggle: ArrowToggle,
+  arrowSelect: ArrowSelect,
+  arrowUp: ArrowUp,
+  arrowDown: ArrowDown,
+  arrowLeft: ArrowLeft,
+  arrowRight: ArrowRight,
+  arrowUpDown: ArrowUpDown,
+  alignLeft: TextLeft,
+  alignRight: TextRight,
+  alignCenter: TextCenter,
+  alignJustify: TextJustify,
+  italic: TextItalic,
+  bold: TextBold,
+  underline: TextUnderline,
+  visible: Visible,
+  hidden: NotVisible,
+  logout: Logout,
+  edit: Edit,
+  help: Help,
+  question: QuestionMark,
+  alert: Alert,
+  avatar: Avatar,
+  folder: Folder,
+  folderAdd: FolderAdd,
+  bin: Bin,
+  dl: InfoTable,
+  checkCircle: CheckCircle,
+  check: CheckMark,
+  minus: Minus,
+  plus: Plus,
+  plusSquare: PlusSquare,
+  cancel: Cancel,
+  upload: Upload,
+  capture: CameraCapture,
+  camera: Camera,
+  link: Link,
+  publish: Publish,
+  unpublish: Unpublish,
+  search: Search,
+  scale: Scale,
+  logo: Logo,
+  logoColorful: LogoColorful,
+  newProject: NewProject,
+  newWorkspace: NewWorkspace,
+  preview: Preview,
+  copyCode: CopyCode,
+  fieldLinked: FieldLinked,
+  fieldClear: FieldClear,
+  map: MapIcon,
+  noProjects: NoProjects,
+  moon: Moon,
+  sun: Sun,
+  checkCircle2: CheckCircle2,
+  mailCircle: MailCircle,
+  install: Install,
+  uploadZipPlugin: UploadZipPlugin,
+  privateGitHubRepo: PrivateGitHubRepo,
+  publicGitHubRepo: PublicGitHubRepo,
+  marketplace: Marketplace,
+  menuForDevice: MenuForDevice,
+  plugin: Plugin,
+  tag: Tag,
+  resize: Resize,
+  crosshair: Crosshair,
+  ellipse: Ellipse,
+  playRight: PlayRight,
+  playLeft: PlayLeft,
+  timeline: Timeline,
+  navigator: WidgetNavigator,
+  navigatorAngle: NavigatorAngle,
+  compass: Compass,
+  compassFocus: CompassFocus,
+  house: House,
+  zoomToLayer: ZoomToLayer,
+};
