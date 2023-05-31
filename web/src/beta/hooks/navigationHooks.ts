4f16781f3a4fcd43eb82fbbaf546d706d2ba5f40
Hiroki Isogai <prog@d2deck.com>
2023-05-31 17:34:49 +0900

    refactor(web): Beta tab routing (#448)

--- web/src/beta/hooks/navigationHooks.ts
+++ web/src/beta/hooks/navigationHooks.ts
@@ -1,14 +1,14 @@
 import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
-export type Tab = "scene" | "story" | "widgets" | "publish";
+import { Tab } from "@reearth/beta/features/Navbar";
 
-export const useEditorNavigation = ({ sceneId }: { sceneId?: string }) => {
+export const useEditorNavigation = ({ sceneId }: { sceneId: string }) => {
   const navigate = useNavigate();
 
   return useCallback(
     (tab: Tab) => {
-      navigate(tab !== "scene" ? `/scene/${sceneId}/${tab}` : "");
+      navigate(`/scene/${sceneId}/${tab}`);
     },
     [sceneId, navigate],
   );
