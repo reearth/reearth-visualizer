e39afef988e0ddf2d16188e5fa129ceab6688c24
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-06-01 14:23:15 +0900

    update theming

---
+++ web/.storybook/ReEarthTheme.ts
@@ -0,0 +1,9 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+  base: "light",
+  brandTitle: "My custom Storybook",
+  brandUrl: "https://example.com",
+  brandImage: "https://storybook.js.org/images/placeholders/350x150.png",
+  brandTarget: "_self",
+});
--- web/.storybook/manager.ts
+++ web/.storybook/manager.ts
@@ -1,9 +1,16 @@
-import { addons } from "@storybook/addons";
+import { addons } from "@storybook/manager-api";
 import { create } from "@storybook/theming/create";
 
 addons.setConfig({
   theme: create({
     base: "dark",
+    fontBase: '"Open Sans", sans-serif',
+    fontCode: "monospace",
     brandTitle: "Re:Earth",
+    brandUrl: "https://github.com/reearth/reearth",
+    brandImage: "/logo.svg",
+    brandTarget: "_self",
+    colorPrimary: "#E95518",
+    colorSecondary: "#4770FF",
   }),
 });
