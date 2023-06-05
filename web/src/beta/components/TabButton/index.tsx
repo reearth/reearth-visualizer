fc2de7360d0ec2c534882b4abe058f24571fb73f
長田淳史 <>
2023-06-05 15:16:24 +0900

    chore(web): Update TabButton component

--- web/src/beta/components/TabButton/index.tsx
+++ web/src/beta/components/TabButton/index.tsx
@@ -21,26 +21,15 @@ type ButtonProps = {
 };
 
 const Button = styled.button<ButtonProps>`
-  // Button Settings
   background: ${({ disabled }) => (disabled ? "#232226" : "inherit")};
-  display: flex;
-  flex-direction: row;
-  align-items: flex-start;
   padding: 8px 12px;
-  gap: 10px;
   height: 35px;
-  left: 0px;
-  top: 0px;
   border-radius: 4px;
   :hover {
     background: #232226;
     transition: all 0.5s ease;
   }
-
-  // Text Settings
   color: ${({ disabled }) => (disabled ? "#C7C5C5" : "#4A4A4A")};
-  font-style: normal;
-  font-weight: 700;
   font-size: 14px;
   line-height: 19px;
   text-align: center;
