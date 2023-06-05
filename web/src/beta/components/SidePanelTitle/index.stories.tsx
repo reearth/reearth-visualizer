85689c5236441020a1b4b6301a8f297543f68e36
atnagata <114915037+atnagata@users.noreply.github.com>
2023-06-05 14:47:57 +0900

    Update web/src/beta/components/SidePanelTitle/index.stories.tsx
    Co-authored-by: keiya sasaki <keiya.s.0210@gmail.com>

--- web/src/beta/components/SidePanelTitle/index.stories.tsx
+++ web/src/beta/components/SidePanelTitle/index.stories.tsx
@@ -3,7 +3,6 @@ import { Meta, StoryObj } from "@storybook/react";
 import SidePanelTitle from ".";
 
 export default {
-  title: "beta/components/SidePanelTitle",
   component: SidePanelTitle,
 } as Meta;
 
