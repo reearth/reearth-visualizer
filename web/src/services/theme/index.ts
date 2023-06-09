12a1b07509a76dc2404dff9a08dbfb9e6078a71e
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-06-09 15:52:01 +0900

    remove old dark/light them files

--- web/src/services/theme/index.ts
+++ web/src/services/theme/index.ts
@@ -8,7 +8,7 @@ export { default as Provider } from "./provider";
 export { default as PublishedAppProvider } from "./publishedAppProvider";
 export { usePublishTheme, publishTheme, mask, type SceneThemeOptions } from "./publishTheme";
 export { default as GlobalStyles } from "./common/globalstyle";
-export { default as lightTheme } from "./lightheme";
+export { default as lightTheme } from "./lightTheme";
 export { default as darkTheme } from "./darkTheme";
 
 export type { PublishTheme } from "./types";
