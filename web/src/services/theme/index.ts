558936aa2f7259caa083a9315060a3ebab2c24d5
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-06-13 11:19:16 +0900

    remove reexport

--- web/src/services/theme/index.ts
+++ web/src/services/theme/index.ts
@@ -5,7 +5,7 @@ export { default as metrics, metricsSizes } from "./reearthTheme/common/metrics"
 export { default } from "./reearthTheme/darkTheme";
 export { default as Provider } from "./provider";
 export { default as PublishedAppProvider } from "./publishedAppProvider";
-export { usePublishTheme, mask, type SceneThemeOptions } from "./publishTheme";
+export { usePublishTheme, mask } from "./publishTheme";
 export { default as GlobalStyles } from "./reearthTheme/common/globalStyles";
 export { default as lightTheme } from "./reearthTheme/lightTheme";
 export { default as darkTheme } from "./reearthTheme/darkTheme";
