1c7ec59bb87cb351a0a4a45a77e997c55b08bae0
koji endo <kouendou@fab.pasona.tech>
2023-06-20 16:12:51 +0900

    fix(web): Update color and font order for new theme.

--- web/src/beta/components/CheckboxField/index.tsx
+++ web/src/beta/components/CheckboxField/index.tsx
@@ -1,28 +1,25 @@
 import type { FC } from "react";
 
-import { styled, useTheme, PublishTheme } from "@reearth/services/theme";
+import { styled, useTheme } from "@reearth/services/theme";
 
 import Icon from "../Icon";
 import Text from "../Text";
 
 export type Props = {
-  publishedTheme?: PublishTheme;
   onClick?: (value: boolean) => void;
   checked?: boolean;
   label: string;
 };
 
-const CheckBoxField: FC<Props> = ({ onClick, checked, label, publishedTheme }) => {
+const CheckBoxField: FC<Props> = ({ onClick, checked, label }) => {
   const theme = useTheme();
   return (
     <Field>
-      <BoxFeild
-        onClick={() => onClick?.(checked !== undefined ? checked : false)}
-        publishedTheme={publishedTheme}>
-        {checked && <CheckMark icon="checkmark" publishedTheme={publishedTheme} />}
+      <BoxFeild onClick={() => onClick?.(checked !== undefined ? checked : false)}>
+        {checked && <CheckMark icon="checkmark" />}
       </BoxFeild>
       {label && (
-        <Text size="xs" color={publishedTheme?.mainText || theme.main.text}>
+        <Text size="footnote" color={theme.general.content.main}>
           {label}
         </Text>
       )}
@@ -38,20 +35,20 @@ const Field = styled.div`
   height: 20px;
 `;
 
-const BoxFeild = styled.button<{ publishedTheme?: PublishTheme }>`
+const BoxFeild = styled.button`
   box-sizing: border-box;
   width: 20px;
   height: 20px;
-  border: 1px solid ${({ theme, publishedTheme }) => publishedTheme?.weakText || theme.main.weak};
+  border: 1px solid ${({ theme }) => theme.general.content.weak};
   border-radius: 4px;
 `;
 
-const CheckMark = styled(Icon)<{ publishedTheme?: PublishTheme }>`
+const CheckMark = styled(Icon)`
   padding-left: 15%;
   padding-right: 10%;
   padding-top: 25%;
   padding-bottom: 20.83%;
-  color: ${({ theme, publishedTheme }) => publishedTheme?.mainText || theme.main.text};
+  color: ${({ theme }) => theme.general.content.main};
 `;
 
 export default CheckBoxField;
