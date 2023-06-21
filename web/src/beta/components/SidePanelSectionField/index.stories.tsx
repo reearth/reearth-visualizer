b35b2ce4478bdc0693f2600085b85e0a4d8e325f
koji endo <kouendou@fab.pasona.tech>
2023-06-21 10:23:26 +0900

    fix(web): Update color and font order for new theme

--- web/src/beta/components/SidePanelSectionField/index.stories.tsx
+++ web/src/beta/components/SidePanelSectionField/index.stories.tsx
@@ -1,7 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
 
-import { colors } from "@reearth/services/theme";
-
 import Component from ".";
 
 const meta: Meta<typeof Component> = {
@@ -14,17 +12,6 @@ type Story = StoryObj<typeof Component>;
 
 export const Default: Story = {
   args: {
-    publishedTheme: {
-      strongText: "",
-      mainText: colors.publish.dark.text.main,
-      weakText: "",
-      strongIcon: "",
-      mainIcon: "",
-      weakIcon: "",
-      select: "",
-      mask: "",
-      background: "",
-    },
     title: "Title",
     children: <p>Item</p>,
   },
--- web/src/beta/components/SidePanelSectionField/index.tsx
+++ web/src/beta/components/SidePanelSectionField/index.tsx
@@ -1,28 +1,30 @@
 import { useState, ReactNode } from "react";
 
-import { styled, useTheme, PublishTheme } from "@reearth/services/theme";
+import { styled, useTheme } from "@reearth/services/theme";
 
 import Icon from "../Icon";
 import Text from "../Text";
 
 const SidePanelSectionField: React.FC<{
-  publishedTheme?: PublishTheme;
   title: string;
   children?: ReactNode;
-}> = ({ publishedTheme, title, children }) => {
+}> = ({ title, children }) => {
   const theme = useTheme();
   const [opened, setOpened] = useState(false);
 
   return (
     <Field>
       <Header onClick={() => setOpened(!opened)}>
-        <Text size="xs" color={theme.other.white} otherProperties={{ height: "16px" }}>
+        <Text
+          size="footnote"
+          color={theme.general.content.strong}
+          otherProperties={{ height: "16px" }}>
           {title}
         </Text>
         <ArrowIcon
           icon="arrowToggle"
           size={12}
-          color={publishedTheme?.mainText || theme.main.text}
+          color={theme.general.content.main}
           opened={opened}
         />
       </Header>
@@ -34,7 +36,7 @@ const SidePanelSectionField: React.FC<{
 const Field = styled.div`
   box-sizing: border-box;
 
-  border-bottom: 1px solid ${props => props.theme.main.paleBg};
+  border-bottom: 1px solid ${props => props.theme.general.bg.weak};
 `;
 const Header = styled.div`
   box-sizing: border-box;
