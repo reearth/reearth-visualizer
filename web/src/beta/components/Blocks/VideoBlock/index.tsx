043a9e1066bd0a5c1dee57bed0dc32ea51401bfb
JAESUNG_PARK <131335554+FabPARKJAESUNG@users.noreply.github.com>
2023-06-22 10:13:44 +0900

    Fix(web): modification max-height

--- web/src/beta/components/Blocks/VideoBlock/index.tsx
+++ web/src/beta/components/Blocks/VideoBlock/index.tsx
@@ -1,6 +1,8 @@
 import { CSSProperties, FC } from "react";
 import Player from "react-player";
 
+import { styled } from "@reearth/services/theme";
+
 type Props = {
   url: string;
   maxHeight?: CSSProperties["maxHeight"];
@@ -8,7 +10,14 @@ type Props = {
 };
 
 const VideoBlock: FC<Props> = ({ url, maxHeight, controls }) => {
-  return <Player url={url} controls={controls} maxHeight={maxHeight} />;
+  return (
+    <Wrapper maxHeight={maxHeight}>
+      <Player url={url} controls={controls} height={"100%"} />
+    </Wrapper>
+  );
 };
-
+const Wrapper = styled.div<{ maxHeight: CSSProperties["maxHeight"] }>`
+  display: flex;
+  height: ${props => (props.maxHeight ? props.maxHeight : "100%")};
+`;
 export default VideoBlock;
