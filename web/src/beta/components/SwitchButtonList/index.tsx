5f35283fefd94b2615a1e29530c97618b7542a9d
JAESUNG_PARK <131335554+FabPARKJAESUNG@users.noreply.github.com>
2023-06-23 18:23:16 +0900

    Fix(web): Modification by feedback

--- web/src/beta/components/SwitchButtonList/index.tsx
+++ web/src/beta/components/SwitchButtonList/index.tsx
@@ -1,7 +1,5 @@
-import type { FC } from "react";
-
 import Text from "@reearth/beta/components/Text";
-import useManageSwitchState, { SwitchField } from "@reearth/beta/hooks/useManageSwitchState/hooks";
+import { SwitchField } from "@reearth/beta/hooks/useManageSwitchState/hooks";
 import { styled, useTheme } from "@reearth/services/theme";
 
 type AdditionalField = {
@@ -13,22 +11,17 @@ export type Props = {
   list: SwitchAdditionalField[];
   onClick?: (id: string) => void;
 };
-const SwitchButtonList: FC<Props> = ({ list, onClick }) => {
+const SwitchButtonList: React.FC<Props> = ({ list, onClick }) => {
   const theme = useTheme();
-  const { handleActivate, fields } = useManageSwitchState({ fields: list });
-  const handleClick = (id: string) => {
-    handleActivate(id);
-    onClick?.(id);
-  };
   return (
     <>
-      {fields.map((item, index) => (
+      {list.map((item, index) => (
         <SwitchButton
           key={item.id}
           disabled={!!item?.active}
-          onClick={() => handleClick(item.id)}
+          onClick={() => onClick?.(item.id)}
           first={index === 0}
-          end={index === fields.length - 1}>
+          end={index === list.length - 1}>
           <Text
             size="footnote"
             color={item.active ? theme.general.content.strong : theme.general.content.main}>
@@ -48,8 +41,10 @@ const SwitchButton = styled.button<{
     props.disabled ? props.theme.general.select : props.theme.general.bg.strong};
   padding: 8px;
   height: 32px;
-  border-radius: ${props =>
-    props.first ? "4px 0px 0px 4px" : props.end ? "0px 4px 4px 0px" : "0px"};
+  border-top-left-radius: ${props => (props.first ? "4px" : "0px")};
+  border-top-right-radius: ${props => (props.end ? "4px" : "0px")};
+  border-bottom-left-radius: ${props => (props.first ? "4px" : "0px")};
+  border-bottom-right-radius: ${props => (props.end ? "4px" : "0px")};
   :hover {
     background: ${props => props.theme.general.select};
     transition: all 0.5s ease;
