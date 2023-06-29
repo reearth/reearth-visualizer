391bc3c638e99f7a753d1bc3e00a7f3ffb4aabd6
rot1024 <aayhrot@gmail.com>
2023-06-29 19:39:55 +0900

    add test

--- server/e2e/dataset_export_test.go
+++ server/e2e/dataset_export_test.go
@@ -44,4 +44,12 @@ func TestDatasetExport(t *testing.T) {
 		ContentType("text/csv")
 	res.Header("Content-Disposition").Equal("attachment;filename=test.csv")
 	res.Body().Equal("f1,f2,f3,location_lng,location_lat\ntest,123,true,12.000000,11.000000\n")
+
+	res = e.GET("/api/datasets/{}.json", dssId).
+		WithHeader("X-Reearth-Debug-User", uId.String()).
+		Expect().
+		Status(http.StatusOK).
+		ContentType("application/json")
+	res.Header("Content-Disposition").Equal("attachment;filename=test.csv.json")
+	res.Body().Equal("[{\"f1\":\"test\",\"f2\":123,\"f3\":true,\"location\":{\"lat\":11,\"lng\":12}}]\n")
 }
