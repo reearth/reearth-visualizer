6c2a7b1f6d83f98e4a9b8a0c1f57ca274ffc1296
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-07-02 18:02:28 +0900

    new beta specific queries/muts: project user

---
+++ web/src/services/gql/queries/user.ts
@@ -0,0 +1,107 @@
+import { gql } from "@apollo/client";
+
+export const GET_USER_BY_SEARCH = gql`
+  query GetUserBySearch($nameOrEmail: String!) {
+    searchUser(nameOrEmail: $nameOrEmail) {
+      id
+      name
+      email
+    }
+  }
+`;
+
+export const GET_ME = gql`
+  query GetMe {
+    me {
+      id
+      name
+      email
+      lang
+      theme
+      myTeam {
+        id
+        name
+        policyId
+        policy {
+          id
+          name
+          projectCount
+          memberCount
+          publishedProjectCount
+          layerCount
+          assetStorageSize
+          datasetSchemaCount
+          datasetCount
+        }
+      }
+      teams {
+        id
+        name
+        members {
+          user {
+            id
+            name
+            email
+          }
+          userId
+          role
+        }
+        policyId
+        policy {
+          id
+          name
+          projectCount
+          memberCount
+          publishedProjectCount
+          layerCount
+          assetStorageSize
+          datasetSchemaCount
+          datasetCount
+        }
+      }
+      auths
+    }
+  }
+`;
+
+export const UPDATE_ME = gql`
+  mutation UpdateMe(
+    $name: String
+    $email: String
+    $lang: Lang
+    $theme: Theme
+    $password: String
+    $passwordConfirmation: String
+  ) {
+    updateMe(
+      input: {
+        name: $name
+        email: $email
+        lang: $lang
+        theme: $theme
+        password: $password
+        passwordConfirmation: $passwordConfirmation
+      }
+    ) {
+      me {
+        id
+        name
+        email
+        lang
+        theme
+        myTeam {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const DELETE_ME = gql`
+  mutation DeleteMe($userId: ID!) {
+    deleteMe(input: { userId: $userId }) {
+      userId
+    }
+  }
+`;
