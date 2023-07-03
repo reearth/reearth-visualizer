69ff1a885ccc4eb38d70ff30eaa67b707e60748a
isoppp <prog@d2deck.com>
2023-07-03 12:03:42 +0900

    fix style

--- web/src/beta/features/Editor/Tabs/Storytelling/StoryPanel/index.tsx
+++ web/src/beta/features/Editor/Tabs/Storytelling/StoryPanel/index.tsx
@@ -8,15 +8,15 @@ type Props = {};
 export const StoryPanel: FC<Props> = () => {
   return (
     <Wrapper>
-      <IndicatorWrapper>
-        <StoryPageIndicator
-          currentPage={3}
-          currentPageProgress={33}
-          maxPage={6}
-          onChangePage={page => console.log(page)}
-        />
-      </IndicatorWrapper>
-      <div>StoryPanel</div>
+      <StoryPageIndicator
+        currentPage={3}
+        currentPageProgress={33}
+        maxPage={6}
+        onChangePage={page => console.log(page)}
+      />
+      <Content>
+        <div>StoryPanel</div>
+      </Content>
     </Wrapper>
   );
 };
@@ -24,16 +24,10 @@ export const StoryPanel: FC<Props> = () => {
 export default StoryPanel;
 
 const Wrapper = styled.div`
-  position: relative;
-  box-sizing: border-box;
   width: 462px;
-  padding: 10px 24px;
   background-color: ${({ theme }) => theme.colors.scrollBar.bg.light};
 `;
 
-const IndicatorWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
+const Content = styled.div`
+  padding: 10px 24px;
 `;
