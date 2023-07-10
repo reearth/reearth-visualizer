9db19e869636336bdcb6c12f776751a23808e476
pyshx <piyushchauhan1004@gmail.com>
2023-07-10 13:26:20 +0530

    fix(web): fix authentication issue with aws pages

--- web/src/services/auth/index.tsx
+++ web/src/services/auth/index.tsx
@@ -1,5 +1,5 @@
-import { withAuthenticationRequired } from "@auth0/auth0-react";
-import { withAuthenticator } from "@aws-amplify/ui-react";
+// import { withAuthenticationRequired } from "@auth0/auth0-react";
+// import { withAuthenticator } from "@aws-amplify/ui-react";
 import { ReactNode } from "react";
 
 import { useAuthenticationRequired } from "./useAuth";
@@ -7,17 +7,22 @@ import { useAuthenticationRequired } from "./useAuth";
 export { AuthProvider } from "./authProvider";
 export { useAuth, useCleanUrl, useAuthenticationRequired } from "./useAuth";
 
-export const AuthenticationRequiredPage: React.FC<{ children?: ReactNode }> = ({ children }) => {
+const AuthenticationRequiredPage: React.FC<{ children?: ReactNode }> = ({ children }) => {
   const [isAuthenticated] = useAuthenticationRequired(); // TODO: show error
   return isAuthenticated && children ? <>{children}</> : null;
 };
 
-export const withAuthorisation = (): ((props: any) => React.FC<any>) => {
-  const authProvider = window.REEARTH_CONFIG?.authProvider;
+// const withAuthorisation = (): ((props: any) => React.FC<any>) => {
+//   const authProvider = window.REEARTH_CONFIG?.authProvider;
+//   if (authProvider === "cognito") {
+//     // Check if authProvider is explicitly defined as "cognito"
+//     return withAuthenticator as unknown as (props: any) => React.FC<any>;
+//   } else if (authProvider === "auth0") {
+//     // Handle the case when authProvider is undefined
+//     return withAuthenticationRequired as unknown as (props: any) => React.FC<any>;
+//   }
+//   // Handle other authProvider values
+//   return (props: any) => props;
+// };
 
-  if (authProvider === "cognito") {
-    return withAuthenticator as unknown as (props: any) => React.FC<any>;
-  }
-
-  return withAuthenticationRequired as unknown as (props: any) => React.FC<any>;
-};
+export const AuthenticatedPage = AuthenticationRequiredPage;
