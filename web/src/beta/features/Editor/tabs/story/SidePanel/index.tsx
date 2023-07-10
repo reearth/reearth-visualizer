ecf85514ad04a2fe5cc1a392c52c295ed1556bc7
isoppp <prog@d2deck.com>
2023-07-10 16:42:10 +0900

    extract components

--- web/src/beta/features/Editor/tabs/story/SidePanel/index.tsx
+++ web/src/beta/features/Editor/tabs/story/SidePanel/index.tsx
@@ -1,12 +1,7 @@
-import { FC, useMemo } from "react";
-
 import SidePanelCommon from "@reearth/beta/features/Editor/SidePanel";
+import ContentPage from "@reearth/beta/features/Editor/Tabs/story/SidePanel/ContentPage";
+import ContentStory from "@reearth/beta/features/Editor/Tabs/story/SidePanel/ContentStory";
 import { useT } from "@reearth/services/i18n";
-import { styled } from "@reearth/services/theme";
-
-import Action from "./Action";
-import Item from "./Item";
-import PageItemWrapper from "./PageItemWrapper";
 
 // TODO: these are currently rough definition
 type Props = {
@@ -19,58 +14,8 @@ type Props = {
   onPageAdd: () => void;
 };
 
-const SidePanel: FC<Props> = ({ onStoryAdd, onSelectStory, onPageAdd, onSelectPage }) => {
+const SidePanel: React.FC<Props> = ({ onStoryAdd, onSelectStory, onPageAdd, onSelectPage }) => {
   const t = useT();
-
-  const StoryContent = useMemo(() => {
-    return (
-      <ContentInner>
-        <ContentUp>
-          {[...Array(100)].map((_, i) => (
-            <Item
-              key={i}
-              onItemClick={() => onSelectStory(i.toString())}
-              onActionClick={() => console.log("onActionClick")}>
-              Story{i} / Story{i} / Story{i} / Story{i} / Story{i} / Story{i} / Story{i} / Story{i}{" "}
-              / Story{i}
-            </Item>
-          ))}
-        </ContentUp>
-        <ContentBottom>
-          <Action
-            icon="book"
-            iconColor="#ffffff"
-            iconSize={16}
-            title={`+ ${t("New Story")}`}
-            onClick={onStoryAdd}
-          />
-        </ContentBottom>
-      </ContentInner>
-    );
-  }, [onSelectStory, onStoryAdd, t]);
-
-  const PageContent = useMemo(() => {
-    return (
-      <ContentInner>
-        <ContentUp>
-          {[...Array(100)].map((_, i) => (
-            <PageItemWrapper key={i} pageCount={i + 1} isSwipable={i % 2 === 0}>
-              <Item
-                key={i}
-                onItemClick={() => onSelectPage(i.toString())}
-                onActionClick={() => console.log("onActionClick")}>
-                Page
-              </Item>
-            </PageItemWrapper>
-          ))}
-        </ContentUp>
-        <ContentBottom>
-          <Action icon="square" title={`+ ${t("New Page")}`} onClick={onPageAdd} />
-          <Action icon="swiper" title={`+ ${t("New Swipe")}`} onClick={onPageAdd} />
-        </ContentBottom>
-      </ContentInner>
-    );
-  }, [onPageAdd, onSelectPage, t]);
 
   return (
     <SidePanelCommon
@@ -80,12 +25,12 @@ const SidePanel: FC<Props> = ({ onStoryAdd, onSelectStory, onPageAdd, onSelectPa
           id: "story",
           title: t("Story"),
           maxHeight: "33%",
-          children: StoryContent,
+          children: <ContentStory onSelectStory={onSelectStory} onStoryAdd={onStoryAdd} />,
         },
         {
           id: "page",
           title: t("Page"),
-          children: PageContent,
+          children: <ContentPage onPageAdd={onPageAdd} onSelectPage={onSelectPage} />,
         },
       ]}
     />
@@ -93,30 +38,3 @@ const SidePanel: FC<Props> = ({ onStoryAdd, onSelectStory, onPageAdd, onSelectPa
 };
 
 export default SidePanel;
-
-const ContentInner = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
-
-const ContentUp = styled.div`
-  flex-grow: 1;
-  height: 0;
-  overflow-y: auto;
-  ::-webkit-scrollbar {
-    display: none;
-  }
-
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  box-sizing: border-box;
-`;
-
-const ContentBottom = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  margin-top: 8px;
-`;
