4aa32a8414a57ff75c9c07370554ce827caaf623
pyshx <piyushchauhan1004@gmail.com>
2023-07-10 13:37:54 +0530

    refactor

--- web/src/services/auth/index.tsx
+++ web/src/services/auth/index.tsx
@@ -1,5 +1,5 @@
-// import { withAuthenticationRequired } from "@auth0/auth0-react";
-// import { withAuthenticator } from "@aws-amplify/ui-react";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 import { ReactNode } from "react";
 
 import { useAuthenticationRequired } from "./useAuth";
@@ -12,17 +12,17 @@ const AuthenticationRequiredPage: React.FC<{ children?: ReactNode }> = ({ childr
   return isAuthenticated && children ? <>{children}</> : null;
 };
 
-// const withAuthorisation = (): ((props: any) => React.FC<any>) => {
-//   const authProvider = window.REEARTH_CONFIG?.authProvider;
-//   if (authProvider === "cognito") {
-//     // Check if authProvider is explicitly defined as "cognito"
-//     return withAuthenticator as unknown as (props: any) => React.FC<any>;
-//   } else if (authProvider === "auth0") {
-//     // Handle the case when authProvider is undefined
-//     return withAuthenticationRequired as unknown as (props: any) => React.FC<any>;
-//   }
-//   // Handle other authProvider values
-//   return (props: any) => props;
-// };
+const withAuthorisation = (): ((props: any) => React.FC<any>) => {
+  const authProvider = window.REEARTH_CONFIG?.authProvider;
+  if (authProvider === "cognito") {
+    // Check if authProvider is explicitly defined as "cognito"
+    return withAuthenticator as unknown as (props: any) => React.FC<any>;
+  } else if (authProvider === "auth0") {
+    // Handle the case when authProvider is undefined
+    return withAuthenticationRequired as unknown as (props: any) => React.FC<any>;
+  }
+  // Handle other authProvider values
+  return (props: any) => props;
+};
 
-export const AuthenticatedPage = AuthenticationRequiredPage;
+export const AuthenticatedPage = withAuthorisation()(AuthenticationRequiredPage);
