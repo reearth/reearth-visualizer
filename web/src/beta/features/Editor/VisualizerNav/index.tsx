f77f683e99ff5185379a325d0425e580bd4034b9
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-07-13 00:52:43 +0900

    add VisualizerNav component

--- web/src/beta/features/Editor/VisualizerNav/index.tsx
+++ web/src/beta/features/Editor/VisualizerNav/index.tsx
@@ -1,13 +1,21 @@
 import styled from "@emotion/styled";
+import { ReactNode } from "react";
 
-type Props = {};
+type Props = {
+  children?: ReactNode;
+};
 
-const VisualizerNav: React.FC<Props> = () => {
-  return <Wrapper>VisualizerNav</Wrapper>;
+const VisualizerNav: React.FC<Props> = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
 };
 
 export default VisualizerNav;
 
 const Wrapper = styled.div`
-  height: 48px; // dummy
+  display: flex;
+  gap: 24px;
+  background: ${({ theme }) => theme.general.bg.main};
+  border-radius: 4px 4px 0 0;
+  padding: 8px;
+  height: 48px;
 `;
