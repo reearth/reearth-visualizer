517b1b42e98d127928b4a2638fef90d5bf8a671b
nina992 <89770889+nina992@users.noreply.github.com>
2023-07-24 11:29:24 +0300

    feat(web): new beta modal design (#579)
    Co-authored-by: nina992 <nouralali992@gmail.com>

--- web/src/beta/components/Modal/ModalFrame/index.tsx
+++ web/src/beta/components/Modal/ModalFrame/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode, useRef, useCallback } from "react";
 import { useClickAway, useKeyPressEvent } from "react-use";
 
 import Icon from "@reearth/beta/components/Icon";
+import Text from "@reearth/beta/components/Text";
 import { styled } from "@reearth/services/theme";
 
 export type Props = {
@@ -10,10 +11,11 @@ export type Props = {
   children?: ReactNode;
   size?: "sm" | "md" | "lg";
   isVisible?: boolean;
+  title?: string;
   onClose?: () => void;
 };
 
-const Modal: React.FC<Props> = ({ className, size, isVisible, onClose, children }) => {
+const Modal: React.FC<Props> = ({ className, size, isVisible, title, onClose, children }) => {
   const ref = useRef<HTMLDivElement>(null);
   useClickAway(ref, () => onClose?.());
 
@@ -31,10 +33,13 @@ const Modal: React.FC<Props> = ({ className, size, isVisible, onClose, children 
   return state === "unmounted" ? null : (
     <Bg state={state}>
       <Wrapper className={className} ref={ref} size={size}>
-        {onClose && (
-          <CloseButton onClick={handleClose}>
-            <Icon icon="cancel" />
-          </CloseButton>
+        {!!title && (
+          <HeaderWrapper>
+            <ModalTitle size="body" weight="regular" color="#E0E0E0">
+              {title}
+            </ModalTitle>
+            {onClose && <CloseIcon icon="cancel" onClick={onClose} />}
+          </HeaderWrapper>
         )}
         <InnerWrapper>{children}</InnerWrapper>
       </Wrapper>
@@ -58,24 +63,39 @@ const Bg = styled.div<{ state: TransitionStatus }>`
 
 const Wrapper = styled.div<{ size?: string }>`
   margin: ${({ size }) => (size === "sm" ? "15%" : size === "lg" ? "4%" : "8%")} auto;
-  padding: 36px 32px;
-  border-radius: 3px;
+  padding-top: 36px;
+  border-radius: 8px;
+  background: #161616;
   width: ${({ size }) => (size === "sm" ? "372px" : size === "lg" ? "684px" : "620px")};
-  background: ${({ theme }) => theme.general.bg.strong};
   position: relative;
 `;
 
-const InnerWrapper = styled.div<{ size?: string }>`
+const InnerWrapper = styled.div`
   margin: 0 auto;
 `;
 
-const CloseButton = styled.span`
-  color: ${({ theme }) => theme.general.content.main};
-  font-size: 24px;
-  position: absolute;
-  right: 32px;
-  top: 20px;
+const ModalTitle = styled(Text)`
+  text-align: center;
+  margin-right: auto;
+`;
+
+const CloseIcon = styled(Icon)`
+  margin-left: auto;
   cursor: pointer;
 `;
 
+const HeaderWrapper = styled.div`
+  display: flex;
+  padding: 12px;
+  justify-content: space-between;
+  align-items: center;
+  align-self: stretch;
+  border-top-right-radius: 8px;
+  border-top-left-radius: 8px;
+  background: #393939;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+`;
 export default Modal;
