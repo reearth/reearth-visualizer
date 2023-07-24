46db3eaa3a4323a360cfbfac00140bb1c364a907
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-07-24 16:32:27 +0900

    fix apollo type issue

--- web/src/services/gql/provider/links/uploadLink.ts
+++ web/src/services/gql/provider/links/uploadLink.ts
@@ -1,7 +1,8 @@
+import { ApolloLink } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
 
 export default (endpoint: string) => {
   return createUploadLink({
     uri: endpoint,
-  });
+  }) as unknown as ApolloLink;
 };
