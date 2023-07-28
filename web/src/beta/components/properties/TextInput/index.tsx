a057b36ac62754c22ed9fc3d3ce49fffb45a8400
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-07-28 22:58:31 +0900

    add onChange and timeout to textinput

--- web/src/beta/components/properties/TextInput/index.tsx
+++ web/src/beta/components/properties/TextInput/index.tsx
@@ -1,3 +1,5 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+
 import { styled } from "@reearth/services/theme";
 
 import Property from "..";
@@ -5,12 +7,34 @@ import Property from "..";
 type Props = {
   name?: string;
   description?: string;
+  value?: string;
+  onChange?: (text: string) => void;
 };
 
-const TextInput: React.FC<Props> = ({ name, description }) => {
+const TextInput: React.FC<Props> = ({ name, description, value, onChange }) => {
+  const [currentValue, setCurrentValue] = useState(value);
+  const timeoutRef = useRef<NodeJS.Timeout>();
+
+  useEffect(() => {
+    timeoutRef.current = setTimeout(() => {
+      if (currentValue === undefined) return;
+      onChange?.(currentValue);
+    }, 3000);
+  }, [currentValue, onChange]);
+
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      clearTimeout(timeoutRef.current);
+      const newValue = e.currentTarget.value;
+
+      setCurrentValue(newValue);
+    },
+    [],
+  );
+
   return (
     <Property name={name} description={description}>
-      <StyledInput />
+      <StyledInput onChange={handleChange} />
     </Property>
   );
 };
@@ -20,8 +44,11 @@ export default TextInput;
 const StyledInput = styled.input`
   outline: none;
   background: ${({ theme }) => theme.bg[1]};
+  color: ${({ theme }) => theme.content.main};
   border: 1px solid ${({ theme }) => theme.outline.weak};
   border-radius: 4px;
+  padding: 4px 8px;
+  transition: all 0.3s;
 
   :focus {
     border-color: ${({ theme }) => theme.outline.main};
