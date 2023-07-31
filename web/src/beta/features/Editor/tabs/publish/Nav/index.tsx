37d14760d46180829cedde73c038834a1314fbb2
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-07-31 16:28:38 +0900

    basic publish status ui

--- web/src/beta/features/Editor/tabs/publish/Nav/index.tsx
+++ web/src/beta/features/Editor/tabs/publish/Nav/index.tsx
@@ -1,4 +1,6 @@
+import Icon from "@reearth/beta/components/Icon";
 import TabButton from "@reearth/beta/components/TabButton";
+import Text from "@reearth/beta/components/Text";
 import SecondaryNav from "@reearth/beta/features/Editor/SecondaryNav";
 import { useT } from "@reearth/services/i18n";
 import { styled } from "@reearth/services/theme";
@@ -7,12 +9,19 @@ export { navbarHeight } from "@reearth/beta/features/Editor/SecondaryNav";
 
 export type ProjectType = "default" | "story";
 
+export type PublishStatus = "unpublished" | "published";
+
 type Props = {
+  publishStatus?: PublishStatus;
   selectedProjectType?: ProjectType;
   onProjectTypeChange: (type: ProjectType) => void;
 };
 
-const Nav: React.FC<Props> = ({ selectedProjectType, onProjectTypeChange }) => {
+const Nav: React.FC<Props> = ({
+  publishStatus = "published",
+  selectedProjectType,
+  onProjectTypeChange,
+}) => {
   const t = useT();
   return (
     <StyledSecondaryNav>
@@ -28,7 +37,13 @@ const Nav: React.FC<Props> = ({ selectedProjectType, onProjectTypeChange }) => {
           onClick={() => onProjectTypeChange("story")}
         />
       </LeftSection>
-      <p>Publishing</p>
+      <Publishing>
+        <Status status={publishStatus} />
+        <Text size="body" customColor>
+          Published
+        </Text>
+        <Icon icon="arrowDown" size={16} />
+      </Publishing>
     </StyledSecondaryNav>
   );
 };
@@ -46,4 +61,35 @@ const StyledSecondaryNav = styled(SecondaryNav)`
 const LeftSection = styled.div`
   display: flex;
   gap: 4px;
+`;
+
+const Publishing = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: ${({ theme }) => theme.content.weak};
+  padding: 4px 8px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: all 0.4s;
+
+  :hover {
+    background: ${({ theme }) => theme.bg[2]};
+    p {
+      transition: all 0.4s;
+      color: ${({ theme }) => theme.content.main};
+    }
+    * {
+      opacity: 1;
+    }
+  }
+`;
+
+const Status = styled.div<{ status?: PublishStatus }>`
+  opacity: 0.5;
+  background: ${({ theme, status }) =>
+    status === "published" ? theme.select.strong : theme.bg[4]};
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
 `;
