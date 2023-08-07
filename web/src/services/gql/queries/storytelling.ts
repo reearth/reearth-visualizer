0bd78490ec9441b85338af0db2e914a932a6ebd9
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-08-07 21:50:58 +0900

    update storytelling queries/generated files

--- web/src/services/gql/queries/storytelling.ts
+++ web/src/services/gql/queries/storytelling.ts
@@ -4,7 +4,7 @@ export const CREATE_STORY = gql(`
   mutation CreateStory($input: CreateStoryInput!) {
     createStory(input: $input) {
       story {
-        ...StoryFragment
+        id
       }
     }
   }
@@ -14,7 +14,7 @@ export const UPDATE_STORY = gql(`
   mutation UpdateStory($input: UpdateStoryInput!) {
     updateStory(input: $input) {
       story {
-        ...StoryFragment
+        id
       }
     }
   }
@@ -32,7 +32,7 @@ export const CREATE_STORY_PAGE = gql(`
   mutation CreateStoryPage($input: CreateStoryPageInput!) {
     createStoryPage(input: $input) {
       story {
-        ...StoryFragment
+        id
       }
     }
   }
@@ -42,7 +42,7 @@ export const UPDATE_STORY_PAGE = gql(`
   mutation UpdateStoryPage($input: UpdateStoryPageInput!) {
     updateStoryPage(input: $input) {
       story {
-        ...StoryFragment
+        id
       }
     }
   }
@@ -52,7 +52,7 @@ export const DELETE_STORY_PAGE = gql(`
   mutation DeleteStoryPage($input: DeleteStoryPageInput!) {
     removeStoryPage(input: $input) {
       story {
-        ...StoryFragment
+        id
       }
     }
   }
@@ -62,7 +62,7 @@ export const MOVE_STORY_PAGE = gql(`
   mutation MoveStoryPage($input: MoveStoryPageInput!) {
     moveStoryPage(input: $input) {
       story {
-        ...StoryFragment
+        id
       }
     }
   }
