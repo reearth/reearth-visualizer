3cbcb6e5d1b42a555d726506a8f1d77474abb8ba
airslice <icesunex@hotmail.com>
2023-08-09 12:31:42 +0800

    refactor: update implement for tiles

--- web/src/classic/core/engines/Cesium/core/Imagery.tsx
+++ web/src/classic/core/engines/Cesium/core/Imagery.tsx
@@ -1,6 +1,5 @@
 import { ImageryProvider } from "cesium";
-import { isEqual } from "lodash-es";
-import { useMemo, useRef, useLayoutEffect, useState, useEffect, useCallback } from "react";
+import { useMemo, useState, useEffect, useCallback } from "react";
 import { ImageryLayer } from "resium";
 
 import { tiles as tilePresets } from "./presets";
@@ -28,43 +27,37 @@ export type Props = {
 };
 
 export default function ImageryLayers({ tiles, cesiumIonAccessToken }: Props) {
-  const { providers, updated } = useImageryProviders({
+  const { providers, providerKey } = useImageryProviders({
     tiles,
     cesiumIonAccessToken,
     presets: tilePresets,
   });
 
-  // force rerendering all layers when any provider is updated
-  // since Resium does not sort layers according to ImageryLayer component order
-  const counter = useRef(0);
-  useLayoutEffect(() => {
-    if (updated) counter.current++;
-  }, [providers, updated]);
+  const memoTiles = useMemo(
+    () =>
+      tiles
+        ?.map(({ id, ...tile }) => ({ ...tile, id, provider: providers[providerKey(tile)] }))
+        .filter(({ provider }) => !!provider) ?? [],
+    [tiles, providerKey, providers],
+  );
 
   return (
     <>
-      {tiles
-        ?.map(({ id, ...tile }) => ({ ...tile, id, provider: providers[id]?.[2] }))
-        .filter(({ provider }) => !!provider)
-        .map(({ id, tile_opacity: opacity, tile_minLevel: min, tile_maxLevel: max, provider }, i) =>
-          provider ? (
-            <ImageryLayer
-              key={`${id}_${i}_${counter.current}`}
-              imageryProvider={provider}
-              minimumTerrainLevel={min}
-              maximumTerrainLevel={max}
-              alpha={opacity}
-              index={i}
-            />
-          ) : null,
-        )}
+      {memoTiles.map(
+        ({ id, tile_opacity: opacity, tile_minLevel: min, tile_maxLevel: max, provider }, i) => (
+          <ImageryLayer
+            key={`${id}_${i}`}
+            imageryProvider={provider}
+            minimumTerrainLevel={min}
+            maximumTerrainLevel={max}
+            alpha={opacity}
+            index={i}
+          />
+        ),
+      )}
     </>
   );
 }
-
-type Providers = {
-  [id: string]: [string | undefined, string | undefined, ImageryProvider];
-};
 
 type ResolvedProviders = {
   [id: string]: ImageryProvider;
@@ -83,95 +76,33 @@ export function useImageryProviders({
       cesiumIonAccessToken?: string;
     }) => Promise<ImageryProvider> | ImageryProvider | null;
   };
-}): { providers: Providers; updated: boolean } {
-  const prevCesiumIonAccessToken = useRef(cesiumIonAccessToken);
-  const tileKeys = tiles.map(t => t.id).join(",");
-  const prevTileKeys = useRef(tileKeys);
-  const prevProviders = useRef<Providers>({});
-  const [presetProviders, setPresetProviders] = useState<ResolvedProviders>({});
+}): {
+  providers: ResolvedProviders;
+  updated?: boolean;
+  providerKey: (tile: Omit<Tile, "id">) => string;
+} {
+  const [resolvedPresetProviders, setResolvedPresetProviders] = useState<ResolvedProviders>({});
 
-  const typekey = useCallback(
-    (t: Tile) => (t.tile_type === "url" ? `url_${t.tile_url}` : t.tile_type || "default"),
-    [],
+  const providerKey = useCallback(
+    (t: Omit<Tile, "id">) =>
+      cesiumIonAccessToken +
+      (t.tile_type === "url" ? `url_${t.tile_url}` : t.tile_type || "default"),
+    [cesiumIonAccessToken],
   );
 
   useEffect(() => {
     tiles
-      .filter(t => t && !Object.keys(presetProviders).includes(typekey(t)))
-      .forEach(async tile => {
-        const addedProvider = await presets[tile.tile_type || "default"]({
-          url: tile.tile_url,
+      .filter(t => t && !Object.keys(resolvedPresetProviders).includes(providerKey(t)))
+      .forEach(async t => {
+        const addedProvider = await presets[t.tile_type || "default"]({
+          url: t.tile_url,
           cesiumIonAccessToken,
         });
         if (addedProvider) {
-          setPresetProviders(prev => ({ ...prev, [typekey(tile)]: addedProvider }));
+          setResolvedPresetProviders(prev => ({ ...prev, [providerKey(t)]: addedProvider }));
         }
       });
-  }, [tiles, cesiumIonAccessToken, presets, presetProviders, typekey]);
+  }, [tiles, cesiumIonAccessToken, presets, resolvedPresetProviders, providerKey]);
 
-  // Manage TileProviders so that TileProvider does not need to be recreated each time tiles are updated.
-  const { providers, updated } = useMemo(() => {
-    const isCesiumAccessTokenUpdated = prevCesiumIonAccessToken.current !== cesiumIonAccessToken;
-    const prevProvidersKeys = Object.keys(prevProviders.current);
-    const added = tiles.map(t => t.id).filter(t => t && !prevProvidersKeys.includes(t));
-
-    const rawProviders = [
-      ...Object.entries(prevProviders.current),
-      ...added.map(a => [a, undefined] as const),
-    ].map(([k, v]) => ({
-      key: k,
-      added: added.includes(k),
-      prevType: v?.[0],
-      prevUrl: v?.[1],
-      prevProvider: v?.[2],
-      tile: tiles.find(t => t.id === k),
-    }));
-
-    const providers = Object.fromEntries(
-      rawProviders
-        .map(
-          ({
-            key,
-            added,
-            prevType,
-            prevUrl,
-            prevProvider,
-            tile,
-          }):
-            | [string, [string | undefined, string | undefined, ImageryProvider | null | undefined]]
-            | null =>
-            !tile
-              ? null
-              : [
-                  key,
-                  added ||
-                  prevType !== tile.tile_type ||
-                  prevUrl !== tile.tile_url ||
-                  (isCesiumAccessTokenUpdated && (!tile.tile_type || tile.tile_type === "default"))
-                    ? [tile.tile_type, tile.tile_url, presetProviders[typekey(tile)]]
-                    : [prevType, prevUrl, prevProvider],
-                ],
-        )
-        .filter(
-          (e): e is [string, [string | undefined, string | undefined, ImageryProvider]] =>
-            !!e?.[1][2],
-        ),
-    );
-
-    const updated =
-      !!added.length ||
-      !!isCesiumAccessTokenUpdated ||
-      !isEqual(prevTileKeys.current, tileKeys) ||
-      rawProviders.some(
-        p => p.tile && (p.prevType !== p.tile.tile_type || p.prevUrl !== p.tile.tile_url),
-      );
-
-    prevTileKeys.current = tileKeys;
-    prevCesiumIonAccessToken.current = cesiumIonAccessToken;
-
-    return { providers, updated };
-  }, [cesiumIonAccessToken, tiles, tileKeys, presetProviders, typekey]);
-
-  prevProviders.current = providers;
-  return { providers, updated };
+  return { providers: resolvedPresetProviders, providerKey };
 }
