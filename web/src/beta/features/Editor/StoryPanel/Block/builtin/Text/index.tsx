1533c67398d265694e246c4c85cbd8a512e666c1
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-08-09 17:10:14 +0900

    setup basic func structure to textblock

--- web/src/beta/features/Editor/StoryPanel/Block/builtin/Text/index.tsx
+++ web/src/beta/features/Editor/StoryPanel/Block/builtin/Text/index.tsx
@@ -1,5 +1,24 @@
-const TextBlock: React.FC = () => {
-  return <div>TEXT BLOCK</div>;
+import { useCallback } from "react";
+
+import { CommonProps as BlockProps } from "../../types";
+import BlockWrapper from "../Wrapper";
+
+export type Props = BlockProps<Property>;
+
+export type Property = {};
+
+const TextBlock: React.FC<Props> = ({ block, isSelected, onClick, onRemove }) => {
+  const handleRemove = useCallback(() => onRemove?.(block?.id), [block?.id, onRemove]);
+
+  return (
+    <BlockWrapper
+      title={block?.title}
+      icon={block?.extensionId}
+      isSelected={isSelected}
+      onClick={onClick}
+      onRemove={handleRemove}
+    />
+  );
 };
 
 export default TextBlock;
