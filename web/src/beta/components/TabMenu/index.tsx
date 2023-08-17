f01630ce0aaab2913ca44991eaca03b195a37cfd
jashan <20891087+jashanbhullar@users.noreply.github.com>
2023-08-17 15:55:53 +0530

    styling fixes - Moved the pointer event to the tab buttons - Added resizable wrapper for the tab component

--- web/src/beta/components/TabMenu/index.tsx
+++ web/src/beta/components/TabMenu/index.tsx
@@ -2,8 +2,11 @@ import { FC, ReactNode } from "react";
 
 import Icon from "@reearth/beta/components/Icon";
 import Icons from "@reearth/beta/components/Icon/icons";
+import Resizable from "@reearth/beta/components/Resizable";
 import Text from "@reearth/beta/components/Text";
 import { styled, useTheme } from "@reearth/services/theme";
+
+import { Direction, Gutter } from "../Resizable/hooks";
 
 interface TabObject {
   icon: keyof typeof Icons;
@@ -23,36 +26,44 @@ const TabMenu: FC<Props> = ({ tabs, title, selectedTab, onSelectedTabChange }) =
 
   const selectedTabItem = tabs.find(({ key }) => key === selectedTab);
 
+  const resizableArgs = {
+    direction: "vertical" as Direction,
+    gutter: "end" as Gutter,
+    initialSize: 800,
+    minSize: 300,
+  };
+
   return (
-    <Wrapper>
-      <Title>
-        <Text size="body">{title}</Text>
-      </Title>
-      <Tabs>
-        {tabs.map(({ key, icon }) => (
-          <TabIconWrapper
-            key={key}
-            onClick={() => onSelectedTabChange(key)}
-            selected={key === selectedTab}>
-            <Icon icon={icon} alt={"Tab " + key} size={20} color={theme.content.main} />
-          </TabIconWrapper>
-        ))}
-      </Tabs>
-      <MainArea>{selectedTabItem ? selectedTabItem.component : null}</MainArea>
-    </Wrapper>
+    <Resizable {...resizableArgs}>
+      <Wrapper>
+        <Title>
+          <Text size="body">{title}</Text>
+        </Title>
+        <Tabs>
+          {tabs.map(({ key, icon }) => (
+            <TabIconWrapper
+              key={key}
+              onClick={() => onSelectedTabChange(key)}
+              selected={key === selectedTab}>
+              <Icon icon={icon} alt={"Tab " + key} size={20} color={theme.content.main} />
+            </TabIconWrapper>
+          ))}
+        </Tabs>
+        <MainArea>{selectedTabItem ? selectedTabItem.component : null}</MainArea>
+      </Wrapper>
+    </Resizable>
   );
 };
 
 export default TabMenu;
 
 const Wrapper = styled.div`
-  width: 286px;
   display: grid;
   border-radius: 10px;
   margin: 15px;
   grid-template-rows: 36px 1fr;
   grid-template-columns: 28px 1fr;
-  height: 600px;
+  min-height: 100vh;
   background: ${({ theme }) => theme.bg[2]};
 `;
 
@@ -66,7 +77,6 @@ const Title = styled.div`
 const Tabs = styled.div`
   grid-column: 1/2;
   background: ${({ theme }) => theme.bg[0]};
-  cursor: pointer;
 `;
 
 const TabIconWrapper = styled.div<{ selected: boolean }>`
@@ -74,6 +84,7 @@ const TabIconWrapper = styled.div<{ selected: boolean }>`
   width: 100%;
   display: flex;
   justify-content: center;
+  cursor: pointer;
   background: ${props => (props.selected ? props.theme.bg[1] : "inherit")};
 `;
 
