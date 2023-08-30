dbaee55a362c17ee45dc7a0a6e39856f50e44744
nina992 <nouralali992@gmail.com>
2023-08-30 18:40:58 +0300

    story

--- web/src/beta/components/fields/RadioGroupField/index.stories.tsx
+++ web/src/beta/components/fields/RadioGroupField/index.stories.tsx
@@ -2,6 +2,8 @@ import { useArgs } from "@storybook/preview-api";
 import { Meta, StoryObj } from "@storybook/react";
 import { useCallback } from "react";
 
+import { styled } from "@reearth/services/theme";
+
 import RadioGroupField from "./index";
 
 type Props = React.ComponentProps<typeof RadioGroupField>;
@@ -25,11 +27,21 @@ export const Default: Story = (args: Props) => {
   const handleChange = useCallback((value: string[]) => updateArgs({ value }), [updateArgs]);
 
   return (
-    <div>
+    <Wrapper>
       <RadioGroupField {...args} onChange={handleChange} />
-    </div>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10%;
+  margin-left: 2rem;
+  margin-top: 2rem;
+  height: 134px;
+  width: 289px;
+`;
+
 Default.args = {
   value: options,
   onChange: () => console.log("clicked"),
