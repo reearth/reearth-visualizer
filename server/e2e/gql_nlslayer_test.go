f84b532afd3a089666cbbd135089e5f343e13b31
pyshx <piyushchauhan1004@gmail.com>
2023-09-03 20:55:18 +0530

    fix

--- server/e2e/gql_nlslayer_test.go
+++ server/e2e/gql_nlslayer_test.go
@@ -1,6 +1,7 @@
 package e2e
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 
@@ -10,54 +11,52 @@ import (
 
 func addNLSLayerSimple(e *httpexpect.Expect, sId string) (GraphQLRequest, *httpexpect.Value, string) {
 	requestBody := GraphQLRequest{
-		OperationName: "addNLSLayerSimple",
-		Query: `mutation AddNLSLayerSimple($input: AddNLSLayerSimpleInput!) {
-			addNLSLayerSimple(input: $input) {
-				layers {
-					id
-					__typename
-				}
-				__typename
-			}
-		}`,
+		OperationName: "AddNLSLayerSimple",
+		Query: `mutation AddNLSLayerSimple($layerType: String!, $sceneId: ID!, $config: JSON, $index: Int) {
+            addNLSLayerSimple(input: { layerType: $layerType, sceneId: $sceneId, config: $config, index: $index}) {
+                layers {
+                    id
+					sceneId
+					layerType
+					config
+                }
+            }
+        }`,
 		Variables: map[string]any{
-			"input": map[string]any{
-				"parentLayerId": "parent12345",
-				"layerType":     "SimpleType",
-				"sceneID":       sId,
-				"config": map[string]any{
-					"data": map[string]any{
-						"type":           "ExampleType",
-						"url":            "https://example.com/data",
-						"value":          "sampleValue",
-						"layers":         "sampleLayerData",
-						"jsonProperties": []string{"prop1", "prop2"},
-						"updateInterval": 10,
-						"parameters": map[string]any{
-							"sampleKey": "sampleValue",
-						},
-						"time": map[string]any{
-							"property":          "time",
-							"interval":          5,
-							"updateClockOnLoad": true,
-						},
-						"csv": map[string]any{
-							"idColumn":              "id",
-							"latColumn":             "latitude",
-							"lngColumn":             "longitude",
-							"heightColumn":          "height",
-							"noHeader":              false,
-							"disableTypeConversion": true,
-						},
+			"layerType": "simple",
+			"sceneId":   sId,
+			"config": map[string]any{
+				"data": map[string]any{
+					"type":           "ExampleType",
+					"url":            "https://example.com/data",
+					"value":          "sampleValue",
+					"layers":         "sampleLayerData",
+					"jsonProperties": []string{"prop1", "prop2"},
+					"updateInterval": 10,
+					"parameters": map[string]any{
+						"sampleKey": "sampleValue",
 					},
-					"properties": "sampleProperties",
-					"defines": map[string]string{
-						"defineKey": "defineValue",
+					"time": map[string]any{
+						"property":          "time",
+						"interval":          5,
+						"updateClockOnLoad": true,
 					},
-					"events": "sampleEvents",
+					"csv": map[string]any{
+						"idColumn":              "id",
+						"latColumn":             "latitude",
+						"lngColumn":             "longitude",
+						"heightColumn":          "height",
+						"noHeader":              false,
+						"disableTypeConversion": true,
+					},
 				},
-				"index": 0,
+				"properties": "sampleProperties",
+				"defines": map[string]string{
+					"defineKey": "defineValue",
+				},
+				"events": "sampleEvents",
 			},
+			"index": 0,
 		},
 	}
 
@@ -70,22 +69,22 @@ func addNLSLayerSimple(e *httpexpect.Expect, sId string) (GraphQLRequest, *httpe
 		Status(http.StatusOK).
 		JSON()
 
+	fmt.Println("res: ", res.Raw())
+
 	layerId := res.Path("$.data.addNLSLayerSimple.layers.id").Raw().(string)
 	return requestBody, res, layerId
 }
 
 func removeNLSLayer(e *httpexpect.Expect, layerId string) (GraphQLRequest, *httpexpect.Value) {
 	requestBody := GraphQLRequest{
-		OperationName: "removeNLSLayer",
-		Query: `mutation RemoveNLSLayer($input: RemoveNLSLayerInput!) {
-			removeNLSLayer(input: $input) {
+		OperationName: "RemoveNLSLayer",
+		Query: `mutation RemoveNLSLayer($layerId: ID!) {
+			removeNLSLayer(input: {layerId: $layerId}) {
 				layerId
 			}
 		}`,
 		Variables: map[string]any{
-			"input": map[string]any{
-				"layerId": layerId,
-			},
+			"layerId": layerId,
 		},
 	}
 
@@ -103,9 +102,9 @@ func removeNLSLayer(e *httpexpect.Expect, layerId string) (GraphQLRequest, *http
 
 func updateNLSLayer(e *httpexpect.Expect, layerId string) (GraphQLRequest, *httpexpect.Value) {
 	requestBody := GraphQLRequest{
-		OperationName: "updateNLSLayer",
-		Query: `mutation UpdateNLSLayer($input: UpdateNLSLayerInput!) {
-			updateNLSLayer(input: $input) {
+		OperationName: "UpdateNLSLayer",
+		Query: `mutation UpdateNLSLayer($layerId: ID!, $name: String, $visible: Boolean) {
+			updateNLSLayer(input: {layerId: $layerId, name: $name, visible: $visible}) {
 				layer {
 					id
 					__typename
@@ -114,11 +113,9 @@ func updateNLSLayer(e *httpexpect.Expect, layerId string) (GraphQLRequest, *http
 			}
 		}`,
 		Variables: map[string]any{
-			"input": map[string]any{
-				"layerId": layerId,
-				"name":    "Updated Layer",
-				"visible": true,
-			},
+			"layerId": layerId,
+			"name":    "Updated Layer",
+			"visible": true,
 		},
 	}
 
@@ -134,6 +131,41 @@ func updateNLSLayer(e *httpexpect.Expect, layerId string) (GraphQLRequest, *http
 	return requestBody, res
 }
 
+func fetchSceneForNewLayers(e *httpexpect.Expect, sID string) (GraphQLRequest, *httpexpect.Value) {
+	fetchSceneRequestBody := GraphQLRequest{
+		OperationName: "GetScene",
+		Query: `query GetScene($sceneId: ID!) {
+		  node(id: $sceneId, type: SCENE) {
+			id
+			... on Scene {
+			  rootLayerId
+			  newLayers {
+				id
+				layerType
+				config
+		 	  }
+			  __typename
+			}
+			__typename
+		  }
+		}`,
+		Variables: map[string]any{
+			"sceneId": sID,
+		},
+	}
+
+	res := e.POST("/api/graphql").
+		WithHeader("Origin", "https://example.com").
+		WithHeader("X-Reearth-Debug-User", uID.String()).
+		WithHeader("Content-Type", "application/json").
+		WithJSON(fetchSceneRequestBody).
+		Expect().
+		Status(http.StatusOK).
+		JSON()
+
+	return fetchSceneRequestBody, res
+}
+
 func TestNLSLayerCRUD(t *testing.T) {
 	e := StartServer(t, &config.Config{
 		Origins: []string{"https://example.com"},
@@ -145,8 +177,27 @@ func TestNLSLayerCRUD(t *testing.T) {
 	pId := createProject(e)
 	_, _, sId := createScene(e, pId)
 
+	fmt.Println("sid: ", sId)
+
+	// fetch scene
+	_, res := fetchSceneForNewLayers(e, sId)
+
+	res.Object().
+		Value("data").Object().
+		Value("node").Object().
+		Value("newLayers").Array().
+		Length().Equal(0)
+
 	// Add NLSLayer
 	_, _, layerId := addNLSLayerSimple(e, sId)
+
+	_, res2 := fetchSceneForNewLayers(e, sId)
+
+	res2.Object().
+		Value("data").Object().
+		Value("node").Object().
+		Value("newLayers").Array().
+		Length().Equal(1)
 
 	// Update NLSLayer
 	_, _ = updateNLSLayer(e, layerId)
