ddfa9b43d660f79515dad5feb738a43d919dec98
nina992 <89770889+nina992@users.noreply.github.com>
2023-09-06 18:54:29 +0400

    Update web/src/beta/components/fields/LocationField/index.stories.tsx
    Co-authored-by: Jashanpreet Singh (json singh)  <20891087+jashanbhullar@users.noreply.github.com>

--- web/src/beta/components/fields/LocationField/index.stories.tsx
+++ web/src/beta/components/fields/LocationField/index.stories.tsx
@@ -11,8 +11,11 @@ export default meta;
 
 type Story = StoryObj<typeof LocationField>;
 
-export const LocationFieldUI: Story = {
-  render: () => (
-    <LocationField name="Location" description="The Location field" onChange={action("onchange")} />
-  ),
+export const Sample: Story = {
+  args: {
+    name: "Location Field",
+    description: "Location field description",
+    value: { lat: 87.43, lng: 107.53 },
+    onChange: () => console.log("clicked"),
+  },
 };
