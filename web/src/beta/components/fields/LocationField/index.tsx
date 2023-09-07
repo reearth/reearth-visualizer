a1ef5c0512daa5d5cd2128f2dad49b7aa4fbb26d
nina992 <nouralali992@gmail.com>
2023-09-07 12:15:24 +0400

    fix-recursive

--- web/src/beta/components/fields/LocationField/index.tsx
+++ web/src/beta/components/fields/LocationField/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 
 import { LatLng } from "@reearth/beta/utils/value";
 import { styled } from "@reearth/services/theme";
@@ -16,22 +16,18 @@ type Props = {
 const LocationField: React.FC<Props> = ({ name, description, value, onChange }) => {
   const [location, setLocation] = useState<LatLng>(value || { lat: 0, lng: 0 });
 
-  useEffect(() => {
-    if (value) setLocation(value);
-  }, [value]);
+  const handleChange = useCallback(
+    (coordination: string, newValue: number | undefined) => {
+      if (newValue === undefined) return;
 
-  const handleChange = useCallback((coordination: string, newValue: number | undefined) => {
-    if (newValue === undefined) return;
-
-    setLocation(prevLocation => ({
-      ...prevLocation,
-      [coordination === "Latitude" ? "lat" : "lng"]: newValue,
-    }));
-  }, []);
-
-  useEffect(() => {
-    if (location) onChange?.(location);
-  }, [location, onChange]);
+      setLocation(prevLocation => ({
+        ...prevLocation,
+        [coordination === "Latitude" ? "lat" : "lng"]: newValue,
+      }));
+      onChange?.(location);
+    },
+    [location, onChange],
+  );
 
   return (
     <Property name={name} description={description}>
