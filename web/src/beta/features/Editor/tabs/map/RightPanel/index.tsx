4f0fa43ae922f69723364c489af19642437c8938
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-09-13 17:41:54 +0900

    basic show scene settings

--- web/src/beta/features/Editor/tabs/map/RightPanel/index.tsx
+++ web/src/beta/features/Editor/tabs/map/RightPanel/index.tsx
@@ -1,23 +1,35 @@
+import { useMemo } from "react";
+
 import SidePanelCommon from "@reearth/beta/features/Editor/SidePanel";
+import Settings from "@reearth/beta/features/Editor/SidePanel/Settings";
+import { useSceneFetcher } from "@reearth/services/api";
+import { convert } from "@reearth/services/api/propertyApi/utils";
+import { useT } from "@reearth/services/i18n";
 
 type Props = {
   sceneId?: string;
+  showSceneSettings?: boolean;
 };
 
-const MapRightPanel: React.FC<Props> = () => {
+const MapRightPanel: React.FC<Props> = ({ sceneId, showSceneSettings }) => {
+  const t = useT();
+  const { useSceneQuery } = useSceneFetcher();
+
+  const { scene } = useSceneQuery({ sceneId });
+
+  const scenePropertyId = useMemo(() => scene?.property?.id, [scene?.property?.id]);
+  const sceneSettings = useMemo(() => convert(scene?.property), [scene?.property]);
+
   return (
     <SidePanelCommon
       location="right"
       contents={[
         {
           id: "map",
-          title: "Map Right Panel",
+          title: t("Inspector"),
           //   maxHeight: !selectedWidget ? "100%" : "40%",
-          children: (
-            <div>
-              Map Right Panel contentMap Right Panel contentMap Right Panel contentMap Right Panel
-              content
-            </div>
+          children: showSceneSettings && scenePropertyId && (
+            <Settings id={scenePropertyId} propertyItems={sceneSettings} />
           ),
         },
       ]}
