d2088aa9feee3d4cc47ae72649e14cd8a6d9ea1a
mkumbobeaty <charsbeaty@gmail.com>
2023-09-18 14:52:00 +0300

    implement add layers using wms

--- web/src/beta/features/Editor/DataSourceManager/WmsTiles/index.tsx
+++ web/src/beta/features/Editor/DataSourceManager/WmsTiles/index.tsx
@@ -1,10 +1,120 @@
-import { FC } from "react";
+import { FC, useState } from "react";
+
+import Button from "@reearth/beta/components/Button";
+import generateRandomString from "@reearth/beta/utils/generate-random-string";
 
 import { DataProps } from "..";
+import {
+  AddLayerWrapper,
+  AssetWrapper,
+  ColJustiftBetween,
+  DeleteLayerIcon,
+  Input,
+  InputGroup,
+  LayerWrapper,
+  SubmitWrapper,
+} from "../utils";
 
 const WmsTiles: FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
-  console.log(sceneId, onSubmit, onClose);
-  return <div>WmsTiles</div>;
+  const [urlValue, setUrlValue] = useState("");
+  const [layerValue, setLayerValue] = useState("");
+  const [layers, setLayers] = useState<string[]>([]);
+
+  const handleAddLayer = () => {
+    const exist = layers.some((layer: string) => layer === layerValue);
+    if (layerValue.trim() !== "") {
+      if (!exist) setLayers(prev => [...prev, layerValue]);
+      setLayerValue("");
+    }
+  };
+
+  const handleDeleteLayer = (idx: number) => {
+    const updatedLayers = [...layers];
+    updatedLayers.splice(idx, 1);
+    setLayers(updatedLayers);
+  };
+
+  const handleSubmit = () => {
+    onSubmit({
+      layerType: "simple",
+      sceneId,
+      title: generateRandomString(5),
+      visible: true,
+      config: {
+        data: {
+          url: urlValue !== "" ? urlValue : null,
+          type: "wms",
+          layers: layers.length <= 1 ? layers[0] : layers,
+        },
+        resource: {
+          clampToGround: true,
+        },
+        marker: {
+          heightReference: "clamp",
+        },
+        polygon: {
+          heightReference: "clamp",
+        },
+        polyline: {
+          clampToGround: true,
+        },
+      },
+    });
+    onClose();
+  };
+
+  return (
+    <ColJustiftBetween>
+      <AssetWrapper>
+        <InputGroup label="Resource URL" description="URL of the data source you want to add.">
+          <Input
+            type="text"
+            placeholder="https://"
+            value={urlValue}
+            onChange={e => setUrlValue(e.target.value)}
+          />
+        </InputGroup>
+        <InputGroup
+          label="Choose layer to add"
+          description="Layer of the data source you want to add.">
+          {layers.map((layer, index) => (
+            <LayerWrapper key={index}>
+              <Input type="text" placeholder={`${layer}`} disabled={true} />
+              <DeleteLayerIcon icon="bin" size={16} onClick={() => handleDeleteLayer(index)} />
+            </LayerWrapper>
+          ))}
+          <LayerWrapper>
+            <Input
+              type="text"
+              placeholder="layer name"
+              value={layerValue}
+              onChange={e => setLayerValue(e.target.value)}
+            />
+            <DeleteLayerIcon icon="bin" size={16} />
+          </LayerWrapper>
+          <AddLayerWrapper>
+            <Button
+              icon="plus"
+              text="Layer"
+              buttonType="primary"
+              size="small"
+              onClick={handleAddLayer}
+              disabled={!layerValue}
+            />
+          </AddLayerWrapper>
+        </InputGroup>
+      </AssetWrapper>
+      <SubmitWrapper>
+        <Button
+          text="Add to Layer"
+          buttonType="primary"
+          size="medium"
+          onClick={handleSubmit}
+          disabled={!urlValue}
+        />
+      </SubmitWrapper>
+    </ColJustiftBetween>
+  );
 };
 
 export default WmsTiles;
