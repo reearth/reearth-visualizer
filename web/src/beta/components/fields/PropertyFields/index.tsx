8580cb9e9c74416c36c7393bbe886f6a14cfd3c2
jashan <20891087+jashanbhullar@users.noreply.github.com>
2023-09-19 13:23:48 +0530

    - idk man, it works

--- web/src/beta/components/fields/PropertyFields/index.tsx
+++ web/src/beta/components/fields/PropertyFields/index.tsx
@@ -74,107 +74,109 @@ const PropertyFields: React.FC<Props> = ({ propertyId, item }) => {
           onItemDrop={onItemDrop}
         />
       )}
-      {item?.schemaFields.map(sf => {
-        const isList = item && "items" in item;
-        const value = !isList ? item.fields.find(f => f.id === sf.id)?.value : sf.defaultValue;
+      {isList &&
+        listItems.length > 0 &&
+        item?.schemaFields.map(sf => {
+          const isList = item && "items" in item;
+          const value = !isList ? item.fields.find(f => f.id === sf.id)?.value : sf.defaultValue;
 
-        const handleChange = handlePropertyValueUpdate(
-          item.schemaGroup,
-          propertyId,
-          sf.id,
-          sf.type,
-        );
+          const handleChange = handlePropertyValueUpdate(
+            item.schemaGroup,
+            propertyId,
+            sf.id,
+            sf.type,
+          );
 
-        return sf.type === "string" ? (
-          sf.ui === "color" ? (
-            <ColorField
+          return sf.type === "string" ? (
+            sf.ui === "color" ? (
+              <ColorField
+                key={sf.id}
+                name={sf.name}
+                value={(value as string) ?? ""}
+                description={sf.description}
+                onChange={handleChange}
+              />
+            ) : sf.ui === "selection" || sf.choices ? (
+              <SelectField
+                key={sf.id}
+                name={sf.name}
+                value={(value as string) ?? ""}
+                description={sf.description}
+                options={sf.choices}
+                onChange={handleChange}
+              />
+            ) : sf.ui === "buttons" ? (
+              <p key={sf.id}>Button radio field</p>
+            ) : (
+              <TextInput
+                key={sf.id}
+                name={sf.name}
+                value={(value as string) ?? ""}
+                description={sf.description}
+                onChange={handleChange}
+              />
+            )
+          ) : sf.type === "spacing" ? (
+            <SpacingInput
               key={sf.id}
               name={sf.name}
-              value={(value as string) ?? ""}
+              value={(value as SpacingValues) ?? ""}
               description={sf.description}
-              onChange={handleChange}
-            />
-          ) : sf.ui === "selection" || sf.choices ? (
-            <SelectField
-              key={sf.id}
-              name={sf.name}
-              value={(value as string) ?? ""}
-              description={sf.description}
-              options={sf.choices}
-              onChange={handleChange}
-            />
-          ) : sf.ui === "buttons" ? (
-            <p key={sf.id}>Button radio field</p>
-          ) : (
-            <TextInput
-              key={sf.id}
-              name={sf.name}
-              value={(value as string) ?? ""}
-              description={sf.description}
-              onChange={handleChange}
-            />
-          )
-        ) : sf.type === "spacing" ? (
-          <SpacingInput
-            key={sf.id}
-            name={sf.name}
-            value={(value as SpacingValues) ?? ""}
-            description={sf.description}
-            min={sf.min}
-            max={sf.max}
-            onChange={handleChange}
-          />
-        ) : sf.type === "bool" ? (
-          <ToggleField
-            key={sf.id}
-            name={sf.name}
-            checked={value as boolean}
-            description={sf.description}
-            onChange={handleChange}
-          />
-        ) : sf.type === "number" ? (
-          sf.ui === "slider" ? (
-            <SliderField
-              key={sf.id}
-              name={sf.name}
-              value={value as number}
               min={sf.min}
               max={sf.max}
+              onChange={handleChange}
+            />
+          ) : sf.type === "bool" ? (
+            <ToggleField
+              key={sf.id}
+              name={sf.name}
+              checked={value as boolean}
+              description={sf.description}
+              onChange={handleChange}
+            />
+          ) : sf.type === "number" ? (
+            sf.ui === "slider" ? (
+              <SliderField
+                key={sf.id}
+                name={sf.name}
+                value={value as number}
+                min={sf.min}
+                max={sf.max}
+                description={sf.description}
+                onChange={handleChange}
+              />
+            ) : (
+              <NumberField
+                key={sf.id}
+                name={sf.name}
+                value={value as number}
+                suffix={sf.suffix}
+                min={sf.min}
+                max={sf.max}
+                description={sf.description}
+                onChange={handleChange}
+              />
+            )
+          ) : sf.type === "latlng" ? (
+            <LocationField
+              key={sf.id}
+              name={sf.name}
+              value={value as LatLng}
+              description={sf.description}
+              onChange={handleChange}
+            />
+          ) : sf.type === "camera" ? (
+            <CameraField
+              key={sf.id}
+              name={sf.name}
+              value={value as CameraValue}
               description={sf.description}
               onChange={handleChange}
             />
           ) : (
-            <NumberField
-              key={sf.id}
-              name={sf.name}
-              value={value as number}
-              suffix={sf.suffix}
-              min={sf.min}
-              max={sf.max}
-              description={sf.description}
-              onChange={handleChange}
-            />
-          )
-        ) : sf.type === "latlng" ? (
-          <LocationField
-            key={sf.id}
-            name={sf.name}
-            value={value as LatLng}
-            description={sf.description}
-            onChange={handleChange}
-          />
-        ) : sf.type === "camera" ? (
-          <CameraField
-            key={sf.id}
-            name={sf.name}
-            value={value as CameraValue}
-            description={sf.description}
-            onChange={handleChange}
-          />
-        ) : (
-          <p key={sf.id}>{sf.name} field</p>
-        );
-      })}
+            <p key={sf.id}>{sf.name} field</p>
+          );
+        })}
     </>
   );
 };
