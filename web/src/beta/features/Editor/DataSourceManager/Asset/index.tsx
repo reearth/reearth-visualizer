0415c7e4cdc8766bc655f0a763bff53f80331035
mkumbobeaty <charsbeaty@gmail.com>
2023-09-21 18:41:31 +0300

    implement add layer from local

--- web/src/beta/features/Editor/DataSourceManager/Asset/index.tsx
+++ web/src/beta/features/Editor/DataSourceManager/Asset/index.tsx
@@ -2,6 +2,7 @@ import React, { useMemo } from "react";
 
 import Button from "@reearth/beta/components/Button";
 import SelectField from "@reearth/beta/components/fields/SelectField";
+import URLField from "@reearth/beta/components/fields/URLField";
 import RadioGroup from "@reearth/beta/components/RadioGroup";
 import Toggle from "@reearth/beta/components/Toggle";
 import generateRandomString from "@reearth/beta/utils/generate-random-string";
@@ -16,7 +17,6 @@ import {
   SourceTypeWrapper,
   SubmitWrapper,
   TextArea,
-  FileButtonWrapper,
 } from "../utils";
 
 const SelectDataType: React.FC<{ fileFormat: string; setFileFormat: (k: string) => void }> = ({
@@ -45,8 +45,8 @@ const Asset: React.FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
   const DataSourceOptions = useMemo(
     () => [
       { label: t("From URL"), keyValue: "url" },
-      { label: t("From Value"), keyValue: "value" },
       { label: t("From Local"), keyValue: "local" },
+      { label: t("From Value"), keyValue: "value" },
     ],
     [t],
   );
@@ -61,6 +61,7 @@ const Asset: React.FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
         parsedValue = value;
       }
     }
+
     onSubmit({
       layerType: "simple",
       sceneId,
@@ -68,7 +69,7 @@ const Asset: React.FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
       visible: true,
       config: {
         data: {
-          url: sourceType === "url" && value !== "" ? value : null,
+          url: (sourceType === "url" || sourceType === "local") && value !== "" ? value : null,
           type: fileFormat.toLowerCase(),
           value: parsedValue,
         },
@@ -87,10 +88,6 @@ const Asset: React.FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
       },
     });
     onClose();
-  };
-
-  const handleChooseFile = () => {
-    console.log("clicked");
   };
 
   return (
@@ -149,26 +146,7 @@ const Asset: React.FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
         {sourceType == "local" && (
           <>
             <SelectDataType fileFormat={fileFormat} setFileFormat={setFileFormat} />
-            <InputGroup
-              label={t("File")}
-              description={t("Asset URL of the data source you want to add.")}>
-              <Input
-                type="text"
-                placeholder={t("Asset url")}
-                value={value}
-                onChange={e => setValue(e.target.value)}
-              />
-            </InputGroup>
-            <FileButtonWrapper>
-              <Button
-                text={t("Choose")}
-                onClick={handleChooseFile}
-                buttonType="primary"
-                size="small"
-                icon="file"
-              />
-              <Button text={t("Upload")} buttonType="primary" size="small" icon="uploadSimple" />
-            </FileButtonWrapper>
+            <URLField fileType="asset" value={value} onChange={setValue} />
           </>
         )}
       </AssetWrapper>
