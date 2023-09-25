8c4b5b338d401f4b155f0b26163c30c2f914e84e
nina992 <89770889+nina992@users.noreply.github.com>
2023-09-25 13:29:07 +0400

    chore(web): add URL field (#674)
    Co-authored-by: nina992 <nouralali992@gmail.com>
    Co-authored-by: KaWaite <34051327+KaWaite@users.noreply.github.com>

--- web/src/beta/components/fields/PropertyFields/index.tsx
+++ web/src/beta/components/fields/PropertyFields/index.tsx
@@ -7,6 +7,7 @@ import SliderField from "@reearth/beta/components/fields/SliderField";
 import SpacingInput, { type SpacingValues } from "@reearth/beta/components/fields/SpacingInput";
 import TextInput from "@reearth/beta/components/fields/TextField";
 import ToggleField from "@reearth/beta/components/fields/ToggleField";
+import URLField from "@reearth/beta/components/fields/URLField";
 import type { FlyTo } from "@reearth/beta/lib/core/types";
 import type { Camera, LatLng } from "@reearth/beta/utils/value";
 import type { Item } from "@reearth/services/api/propertyApi/utils";
@@ -65,6 +66,16 @@ const PropertyFields: React.FC<Props> = ({ propertyId, item, currentCamera, onFl
               onChange={handleChange}
             />
           )
+        ) : sf.type === "url" ? (
+          <URLField
+            key={sf.id}
+            name={sf.name}
+            assetType={sf.ui === "image" ? "image" : sf.ui === "file" ? "file" : undefined}
+            fileType={sf.ui === "video" || sf.ui === undefined ? "URL" : "asset"}
+            value={(value as string) ?? ""}
+            description={sf.description}
+            onChange={handleChange}
+          />
         ) : sf.type === "spacing" ? (
           <SpacingInput
             key={sf.id}
