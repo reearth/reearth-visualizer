e6dd08fd113518dc702e0190bc4a0610e9813b3a
jashan <20891087+jashanbhullar@users.noreply.github.com>
2023-09-27 17:33:47 +0530

    - MD Block. Finally!

--- web/src/beta/lib/core/StoryPanel/Block/builtin/index.ts
+++ web/src/beta/lib/core/StoryPanel/Block/builtin/index.ts
@@ -3,6 +3,7 @@ import { merge } from "lodash-es";
 import { Component } from "..";
 
 import ImageBlock from "./Image";
+import MdBlock from "./Markdown";
 import TextBlock from "./Text";
 import TitleBlock from "./Title";
 import VideoBlock from "./Video";
@@ -12,18 +13,21 @@ export const TITLE_BUILTIN_STORY_BLOCK_ID = "reearth/titleStoryBlock"; // pseudo
 export const IMAGE_BUILTIN_STORY_BLOCK_ID = "reearth/imageStoryBlock";
 export const TEXT_BUILTIN_STORY_BLOCK_ID = "reearth/textStoryBlock";
 export const VIDEO_BUILTIN_STORY_BLOCK_ID = "reearth/videoStoryBlock";
+export const MD_BUILTIN_STORY_BLOCK_ID = "reearth/mdTextStoryBlock";
 
 export const AVAILABLE_STORY_BLOCK_IDS = [
   IMAGE_BUILTIN_STORY_BLOCK_ID,
   TEXT_BUILTIN_STORY_BLOCK_ID,
   VIDEO_BUILTIN_STORY_BLOCK_ID,
+  MD_BUILTIN_STORY_BLOCK_ID,
 ];
 
 export type ReEarthBuiltinStoryBlocks<T = unknown> = Record<
   | typeof TITLE_BUILTIN_STORY_BLOCK_ID
   | typeof IMAGE_BUILTIN_STORY_BLOCK_ID
   | typeof TEXT_BUILTIN_STORY_BLOCK_ID
-  | typeof VIDEO_BUILTIN_STORY_BLOCK_ID,
+  | typeof VIDEO_BUILTIN_STORY_BLOCK_ID
+  | typeof MD_BUILTIN_STORY_BLOCK_ID,
   T
 >;
 
@@ -35,6 +39,7 @@ const reearthBuiltin: BuiltinStoryBlocks<Component> = {
   [IMAGE_BUILTIN_STORY_BLOCK_ID]: ImageBlock,
   [TEXT_BUILTIN_STORY_BLOCK_ID]: TextBlock,
   [VIDEO_BUILTIN_STORY_BLOCK_ID]: VideoBlock,
+  [MD_BUILTIN_STORY_BLOCK_ID]: MdBlock,
 };
 
 const builtin = merge({}, reearthBuiltin);
