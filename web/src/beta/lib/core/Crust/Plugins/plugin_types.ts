3ab8ef188b39d05318321067a3b0f4ed2ba1fe90
keiya01 <keiya.s.0210@gmail.com>
2023-10-02 11:00:24 +0900

    rename

--- web/src/beta/lib/core/Crust/Plugins/plugin_types.ts
+++ web/src/beta/lib/core/Crust/Plugins/plugin_types.ts
@@ -83,7 +83,7 @@ export type Reearth = {
       ) => void;
       findFeatureById?: (layerId: string, featureId: string) => Feature | undefined;
       findFeaturesByIds?: (layerId: string, featureId: string[]) => Feature[] | undefined;
-      selectFeature?: (layers: { layerId?: string; featureId?: string[] }[]) => void;
+      selectFeatures?: (layers: { layerId?: string; featureId?: string[] }[]) => void;
       selectionReason?: LayerSelectionReason;
       // For compat
       overriddenInfobox?: LayerSelectionReason["defaultInfobox"];
