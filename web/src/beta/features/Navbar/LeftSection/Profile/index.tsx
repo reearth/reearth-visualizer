04ea5e1e3267d1862c9eb2769bee3015efb12771
jashan <20891087+jashanbhullar@users.noreply.github.com>
2023-10-05 11:59:46 +0530

    - Replaced dropdwon with Popover implementation - Deleted dropdown - Deleted MenuList (not being used anymore)

--- web/src/beta/features/Navbar/LeftSection/Profile/index.tsx
+++ web/src/beta/features/Navbar/LeftSection/Profile/index.tsx
@@ -1,38 +1,81 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
+import { Link } from "react-router-dom";
 
-// import Avatar from "@reearth/beta/components/Avatar";
-import Dropdown, { MenuItem } from "@reearth/beta/components/Dropdown";
+import Icon, { Icons } from "@reearth/beta/components/Icon";
+import * as Popover from "@reearth/beta/components/Popover";
 import Text from "@reearth/beta/components/Text";
 import { useT } from "@reearth/services/i18n";
 import { styled } from "@reearth/services/theme";
 
 import { Workspace } from "../../types";
 
-export type LoginProps = {
-  username: string;
-  currentWorkspace: Workspace;
-  personalWorkspace?: boolean;
-};
 export type Props = {
   workspaces?: Workspace[];
   personalWorkspace?: boolean;
+  currentWorkspace?: Workspace;
   onSignOut: () => void;
   onWorkspaceChange?: (workspaceId: string) => void;
   openModal?: () => void;
 };
 
-const Label: React.FC<LoginProps> = ({ currentWorkspace }) => (
-  <LabelWrapper>
-    <Text size="body" weight="bold" customColor>
-      {currentWorkspace.name}
-    </Text>
-  </LabelWrapper>
-);
+type ListItem = {
+  text?: string;
+  linkTo?: string;
+  breakpoint?: boolean;
+  icon?: Icons;
+  onClick?: () => void;
+  items?: ListItem[];
+};
 
-const HeaderProfile: React.FC<Props & Partial<LoginProps>> = ({
-  username = "",
+type MenuProps = {
+  label?: string;
+  items: ListItem[];
+  nested?: boolean;
+};
+
+const Menu: React.FC<MenuProps> = ({ label, items, nested }) => {
+  const [open, setOpen] = useState(false);
+
+  const handlePopOver = useCallback(() => setOpen(!open), [open]);
+
+  return (
+    <Popover.Provider
+      open={open}
+      placement={nested ? "right-end" : "bottom-start"}
+      onOpenChange={handlePopOver}>
+      <Popover.Trigger asChild>
+        <InputWrapper onClick={handlePopOver}>
+          <Label size="body" weight={nested ? "regular" : "bold"} open={open} customColor={!nested}>
+            {label}
+          </Label>
+          <ArrowIcon icon={nested ? "arrowRight" : "arrowDown"} open={open} size={15} />
+        </InputWrapper>
+      </Popover.Trigger>
+      <PickerWrapper attachToRoot>
+        {items?.map(({ text: value, linkTo, breakpoint, icon, onClick, items }, index) =>
+          breakpoint ? (
+            <Spacer key={index} />
+          ) : items ? (
+            <Menu label={value} items={items} nested />
+          ) : linkTo ? (
+            <StyledLinkButton to={linkTo}>
+              {icon ? <StyledIcon icon={icon} size={20} /> : null}
+              {value}
+            </StyledLinkButton>
+          ) : (
+            <Option size="body" key={index} onClick={onClick}>
+              {icon ? <StyledIcon icon={icon} size={20} /> : null}
+              {value}
+            </Option>
+          ),
+        )}
+      </PickerWrapper>
+    </Popover.Provider>
+  );
+};
+
+const HeaderProfile: React.FC<Props> = ({
   currentWorkspace = { id: undefined, name: "" },
-  personalWorkspace,
   workspaces = [],
   onSignOut,
   onWorkspaceChange,
@@ -40,7 +83,14 @@ const HeaderProfile: React.FC<Props & Partial<LoginProps>> = ({
 }) => {
   const t = useT();
 
-  const menuItems: MenuItem[] = [
+  const handleWorkspaceChange = useCallback(
+    (t: string) => {
+      onWorkspaceChange?.(t);
+    },
+    [onWorkspaceChange],
+  );
+
+  const menuItems: ListItem[] = [
     { text: t("Account Settings"), linkTo: "/settings/account" },
     {
       text: t("Workspaces"),
@@ -56,49 +106,94 @@ const HeaderProfile: React.FC<Props & Partial<LoginProps>> = ({
         { text: t("New Workspace"), icon: "workspaceAdd", onClick: openModal },
       ],
     },
-    { text: t("Log out"), onClick: onSignOut, breakpoint: true },
+    { text: t("Log out"), onClick: onSignOut },
     { breakpoint: true },
     { text: `v${__APP_VERSION__}` },
   ];
 
-  const handleWorkspaceChange = useCallback(
-    (t: string) => {
-      onWorkspaceChange?.(t);
-    },
-    [onWorkspaceChange],
-  );
-
-  return (
-    <StyledDropdown
-      openOnClick
-      direction="down"
-      gap="lg"
-      hasIcon
-      label={
-        <Label
-          username={username}
-          personalWorkspace={personalWorkspace}
-          currentWorkspace={currentWorkspace}
-        />
-      }
-      menu={{
-        width: 240,
-        items: menuItems,
-      }}
-    />
-  );
+  return <Menu label={currentWorkspace.name} items={menuItems} />;
 };
 
-const StyledDropdown = styled(Dropdown)`
-  height: 100%;
-  margin-left: -8px;
-  margin-right: -8px;
+const InputWrapper = styled.div`
+  display: flex;
+  position: relative;
+  cursor: pointer;
+  align-items: center;
+`;
+
+const Label = styled(Text)<{ open: boolean }>`
+  padding: 7px 12px;
+  /* The width + placement of the arrow icon */
+  padding-right: 30px;
+  width: 100%;
+  border-radius: 4px;
+  color: ${({ theme }) => theme.content.weak};
+
+  &:focus {
+    border: 1px solid ${({ theme }) => theme.select.strong};
+  }
+
+  &:focus-visible {
+    border: 1px solid ${({ theme }) => theme.select.strong};
+    outline: none;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.content.main};
+    background: ${({ theme }) => theme.bg[2]};
+  }
+`;
+
+const ArrowIcon = styled(Icon)<{ open: boolean }>`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: ${({ open }) => (open ? "translateY(-50%) scaleY(-1)" : "translateY(-50%)")};
   color: ${({ theme }) => theme.content.weak};
 `;
 
-const LabelWrapper = styled.div`
+const PickerWrapper = styled(Popover.Content)`
+  min-width: 200px;
+  outline: none;
+  border-radius: 4px;
+  background: ${({ theme }) => theme.bg[0]};
   display: flex;
-  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
+const Spacer = styled.div`
+  border-top: 0.5px solid ${({ theme }) => theme.outline.weak};
+  margin: 2px 0;
+`;
+
+const StyledLinkButton = styled(Link)`
+  text-decoration: none;
+  padding: 9px 12px;
+  font-size: 14px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  color: ${({ theme }) => theme.content.main};
+  :hover {
+    text-decoration: none;
+    background: ${({ theme }) => theme.bg[2]};
+  }
+`;
+
+const Option = styled(Text)`
+  padding: 9px 12px;
+  border-radius: 4px;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  &:hover {
+    background: ${({ theme }) => theme.bg[2]};
+  }
+`;
+
+const StyledIcon = styled(Icon)`
+  color: ${({ theme }) => theme.content.main};
+`;
 export default HeaderProfile;
