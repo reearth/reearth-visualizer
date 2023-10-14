f01c56a7f79dbf01f6a5c937a67288f7b9a2d285
mkumbobeaty <charsbeaty@gmail.com>
2023-10-14 12:17:27 +0300

    improve visibility

--- web/src/beta/lib/core/engines/Cesium/Feature/index.tsx
+++ web/src/beta/lib/core/engines/Cesium/Feature/index.tsx
@@ -100,7 +100,8 @@ export default function Feature({
       !!props.sceneProperty?.light?.sphericalHarmonicCoefficients;
     const useSceneSpecularEnvironmentMaps = !!props.sceneProperty?.light?.specularEnvironmentMaps;
 
-    const isVisible = layer.layer.visible !== false || isHidden;
+    const isVisible = layer.layer.visible !== false && !isHidden;
+
     const componentId =
       urlMD5 +
       generateIDWithMD5(
