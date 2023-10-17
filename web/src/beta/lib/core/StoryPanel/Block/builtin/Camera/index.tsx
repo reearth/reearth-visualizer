bac4ba86d6346526a09e053f9a7e4f7ed96fe423
Jashanpreet Singh (json singh) <20891087+jashanbhullar@users.noreply.github.com>
2023-10-17 10:18:46 +0530

    chore(web): Camera block (#729)

---
+++ web/src/beta/lib/core/StoryPanel/Block/builtin/Camera/index.tsx
@@ -0,0 +1,109 @@
+import { useCallback, useEffect, useMemo } from "react";
+
+import { ValueTypes } from "@reearth/beta/utils/value";
+
+import { getFieldValue } from "../../../utils";
+import { CommonProps as BlockProps } from "../../types";
+import usePropertyValueUpdate from "../common/useActionPropertyApi";
+import BlockWrapper from "../common/Wrapper";
+
+import CameraEditor, { Props as EditorProps } from "./Editor";
+
+export type Props = BlockProps;
+
+const CameraBlock: React.FC<Props> = ({ block, isSelected, ...props }) => {
+  const {
+    handlePropertyValueUpdate,
+    handleAddPropertyItem,
+    handleRemovePropertyItem,
+    handleMovePropertyItem,
+  } = usePropertyValueUpdate();
+
+  const items = useMemo(
+    () => getFieldValue(block?.property?.items ?? [], "") as EditorProps["items"],
+    [block?.property?.items],
+  );
+
+  const handleUpdate = useCallback(
+    (
+      itemId: string,
+      fieldId: string,
+      fieldType: keyof ValueTypes,
+      updatedValue: ValueTypes[keyof ValueTypes],
+    ) => {
+      const schemaGroup = block?.property?.items?.find(
+        i => i.schemaGroup === "default",
+      )?.schemaGroup;
+      if (!block?.property?.id || !itemId || !schemaGroup) return;
+
+      handlePropertyValueUpdate(
+        schemaGroup,
+        block?.property?.id,
+        fieldId,
+        fieldType,
+        itemId,
+      )(updatedValue);
+    },
+    [block?.property?.id, block?.property?.items, handlePropertyValueUpdate],
+  );
+
+  const handleItemAdd = useCallback(() => {
+    const schemaGroup = block?.property?.items?.find(i => i.schemaGroup === "default")?.schemaGroup;
+    if (!block?.property?.id || !schemaGroup) return;
+    handleAddPropertyItem(block.property.id, schemaGroup);
+  }, [block?.property?.id, block?.property?.items, handleAddPropertyItem]);
+
+  const handleItemRemove = useCallback(
+    (itemId: string) => {
+      const schemaGroup = block?.property?.items?.find(
+        i => i.schemaGroup === "default",
+      )?.schemaGroup;
+      if (!block?.property?.id || !itemId || !schemaGroup) return;
+
+      handleRemovePropertyItem(block.property.id, schemaGroup, itemId);
+    },
+    [block?.property?.id, block?.property?.items, handleRemovePropertyItem],
+  );
+
+  const handleItemMove = useCallback(
+    ({ id }: { id: string }, index: number) => {
+      const schemaGroup = block?.property?.items?.find(
+        i => i.schemaGroup === "default",
+      )?.schemaGroup;
+      if (!block?.property?.id || !id || !schemaGroup) return;
+
+      handleMovePropertyItem(block.property.id, schemaGroup, { id }, index);
+    },
+    [block?.property?.id, block?.property?.items, handleMovePropertyItem],
+  );
+
+  // if there's no item add 1 button.
+  // TODO: Should be added to block creationAPI for generic blocks that require at least 1 item
+  useEffect(() => {
+    if (items.length === 0) {
+      handleItemAdd();
+      return;
+    }
+  }, [items.length, handleItemAdd]);
+
+  return (
+    <BlockWrapper
+      icon={block?.extensionId}
+      isSelected={isSelected}
+      propertyId={block?.property?.id}
+      propertyItems={block?.property?.items}
+      settingsEnabled={false}
+      {...props}>
+      <CameraEditor
+        items={items}
+        onUpdate={handleUpdate}
+        onItemRemove={handleItemRemove}
+        onItemAdd={handleItemAdd}
+        onItemMove={handleItemMove}
+        inEditor={!!props.isEditable}
+      />
+    </BlockWrapper>
+  );
+};
+
+export default CameraBlock;
