43d01d95686f2f9109cb5765bae6a456bec80cb5
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-10-20 10:51:38 +0900

    wip beta zindexes

--- web/src/services/theme/reearthTheme/common/zIndex.ts
+++ web/src/services/theme/reearthTheme/common/zIndex.ts
@@ -2,6 +2,8 @@ const zIndex = {
   base: 1,
   hidden: -1,
   form: 100,
+  menuForDevice: 200,
+  settingHeader: 300,
   infoBox: 300,
   propertyFieldPopup: 500,
   descriptionBalloon: 550,
@@ -9,11 +11,35 @@ const zIndex = {
   pluginModal: 570,
   dropDown: 600,
   fullScreenModal: 700,
+  splashScreen: 700,
   loading: 800,
   notificationBar: 1000,
-  splashScreen: 700,
-  settingHeader: 300,
-  menuForDevice: 200,
+};
+
+export const newZIndex = {
+  visualizer: {
+    widget: 200,
+    storyPanel: 200,
+    storyBlock: 205,
+    storyPage: {
+      indicator: {
+        unselected: 210,
+        selected: 215,
+      },
+    },
+    pluginPopup: 300,
+    pluginModal: 305,
+    infobox: 400,
+  },
+  editor: {
+    navbar: 500,
+    panel: 500,
+    popover: 600,
+    modal: {
+      bg: 700,
+      self: 705,
+    },
+  },
 };
 
 export default zIndex;
