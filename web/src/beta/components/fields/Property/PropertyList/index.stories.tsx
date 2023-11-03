3c06a57c06436b7cbde38aac94e8d26c69181ecf
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-11-03 12:02:15 +0900

    fix story

--- web/src/beta/components/fields/Property/PropertyList/index.stories.tsx
+++ web/src/beta/components/fields/Property/PropertyList/index.stories.tsx
@@ -17,51 +17,12 @@ type Story = StoryObj<typeof ListField>;
 export const Default: Story = (args: Props) => {
   const [_, updateArgs] = useArgs();
 
-  const addItem = useCallback(() => {
-    const randomId = (Math.random() + 1).toString(36).substring(7);
-    updateArgs({
-      items: [
-        ...args.items,
-        {
-          id: randomId,
-          value: `Item ${randomId}`,
-        },
-      ],
-    });
-  }, [updateArgs, args.items]);
-
-  const removeItem = useCallback(
-    (key: string) => {
-      updateArgs({ items: args.items.filter(({ id }) => id != key) });
-    },
-    [updateArgs, args.items],
-  );
-
-  const onItemDrop = useCallback(
-    (item: { id: string; value: string }, index: number) => {
-      const items = [...args.items];
-      items.splice(
-        items.findIndex(x => x.id === item.id),
-        1,
-      );
-      items.splice(index, 0, item);
-      updateArgs({ items });
-    },
-    [updateArgs, args.items],
-  );
-
   const onSelect = useCallback((id: string) => updateArgs({ selected: id }), [updateArgs]);
 
   return (
     <Wrapper>
       <div>
-        <ListField
-          {...args}
-          addItem={addItem}
-          removeItem={removeItem}
-          onItemDrop={onItemDrop}
-          onSelect={onSelect}
-        />
+        <ListField {...args} onSelect={onSelect} />
       </div>
     </Wrapper>
   );
@@ -83,27 +44,27 @@ Default.args = {
   items: [
     {
       id: "w3tlwi",
-      value: "Item w3tlwi",
+      title: "Item w3tlwi",
     },
     {
       id: "77eg5",
-      value: "Item 77eg5",
+      title: "Item 77eg5",
     },
     {
       id: "7p218",
-      value: "Item 7p218",
+      title: "Item 7p218",
     },
     {
       id: "xquyo",
-      value: "Item xquyo",
+      title: "Item xquyo",
     },
     {
       id: "2mewj",
-      value: "Item 2mewj",
+      title: "Item 2mewj",
     },
     {
       id: "d2gmu",
-      value: "Item d2gmu",
+      title: "Item d2gmu",
     },
   ],
 };
