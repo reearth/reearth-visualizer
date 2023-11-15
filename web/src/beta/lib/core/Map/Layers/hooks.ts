01d534c3949ea558b33c219c5c16457e2e6b487f
keiya01 <keiya.s.0210@gmail.com>
2023-11-15 17:12:14 +0900

    chore(web): fix overriding layer structure

--- web/src/beta/lib/core/Map/Layers/hooks.ts
+++ web/src/beta/lib/core/Map/Layers/hooks.ts
@@ -316,6 +316,9 @@ export default function useHooks({
   const overriddenLayersRef = useRef(overriddenLayers);
   overriddenLayersRef.current = overriddenLayers;
 
+  // props layers: { visible: false }
+  // overriddenLayers: { _updateStyles: 5 }
+
   const override = useCallback(
     (id: string, layer?: (Partial<Layer> & { property?: any }) | null) => {
       const originalLayer = layerMap.get(id);
@@ -339,15 +342,22 @@ export default function useHooks({
         res.data.value = dataValue;
       }
 
-      const rawLayer = compat(res);
+      const property = layer?.property;
+      const rawLayer = compat({
+        ...(!originalLayer.compat && property ? { property } : {}),
+        ...res,
+      });
+
       if (!rawLayer) return;
 
       if (
-        rawLayer.type === "simple" &&
-        rawLayer.data?.value &&
+        originalLayer.type === "simple" &&
+        originalLayer.data?.value &&
         // If data isn't cachable, reuse layer id for performance.
-        DATA_CACHE_KEYS.some(k => !rawLayer.data?.[k]) &&
-        Object.isExtensible(rawLayer.data.value)
+        DATA_CACHE_KEYS.some(k => !originalLayer.data?.[k]) &&
+        Object.isExtensible(originalLayer.data.value) &&
+        rawLayer.type === "simple" &&
+        rawLayer?.data?.value
       ) {
         // If layer property is overridden, feature is legacy layer.
         // So we can set layer id to prevent unnecessary render.
