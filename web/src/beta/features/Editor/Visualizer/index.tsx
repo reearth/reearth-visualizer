8a2ae6d595932ee0f5ee921daf5950049b72ff02
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-12-14 16:58:21 +0900

    remove lazy import

--- web/src/beta/features/Editor/Visualizer/index.tsx
+++ web/src/beta/features/Editor/Visualizer/index.tsx
@@ -1,4 +1,4 @@
-import { MutableRefObject, lazy, useCallback } from "react";
+import { MutableRefObject, useCallback } from "react";
 
 import ContentPicker from "@reearth/beta/components/ContentPicker";
 import type { MapRef } from "@reearth/beta/lib/core/Map/ref";
@@ -8,14 +8,13 @@ import StoryPanel, {
   type InstallableStoryBlock,
 } from "@reearth/beta/lib/core/StoryPanel";
 import { type Props as VisualizerProps } from "@reearth/beta/lib/core/Visualizer";
+import CoreVisualizer from "@reearth/beta/lib/core/Visualizer";
 import type { Camera } from "@reearth/beta/utils/value";
 import type { Story } from "@reearth/services/api/storytellingApi/utils";
 import { config } from "@reearth/services/config";
 import { styled } from "@reearth/services/theme";
 
 import useHooks from "./hooks";
-
-const CoreVisualizer = lazy(() => import("@reearth/beta/lib/core/Visualizer"));
 
 export type Props = {
   visualizerRef?: MutableRefObject<MapRef | null>;
