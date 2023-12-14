68e967e4db862b39175b269f7794236a874700c5
KaWaite <34051327+KaWaite@users.noreply.github.com>
2023-12-14 16:44:57 +0900

    update routes

--- web/src/services/routing/index.tsx
+++ web/src/services/routing/index.tsx
@@ -1,4 +1,11 @@
-import { BrowserRouter as Router, Navigate, Route, Routes, useParams } from "react-router-dom";
+import {
+  Navigate,
+  Route,
+  useParams,
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
 
 import { styled } from "@reearth/services/theme";
 
@@ -27,60 +34,56 @@ import {
 } from "./lazyRoutes";
 
 export const AppRoutes = () => {
-  return (
-    <>
-      <StyledRouter>
-        <Routes>
-          {/* Beta routes - start */}
-          <Route path="scene/:sceneId/:tab" element={<BetaEditor />} />
-          <Route
-            path="settings/project/:projectId/:tab?/:subId?"
-            element={<BetaProjectSettings />}
-          />
-          <Route path="graphql" element={<GraphQLPlayground />} />
-          {/* Beta routes - end */}
-          {/* classic routes - start */}
-          <Route index={true} element={<RootPage />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignupPage />} />
-          <Route path="password-reset" element={<PasswordResetPage />} />
-          <Route path="dashboard/:workspaceId" element={<Dashboard />} />
-          <Route path="edit/:sceneId">
-            <Route index={true} element={<EarthEditor />} />
-            <Route path="preview" element={<Preview />} />
-          </Route>
-          <Route path="plugin-editor" element={<PluginEditor />} />
-          <Route path="settings">
-            <Route index={true} element={<Navigate to="/settings/account" />} />
-            <Route path="account" element={<AccountSettings />} />
-            <Route path="workspaces">
-              <Route index={true} element={<WorkspaceList />} />
-              <Route path=":workspaceId">
-                <Route index={true} element={<WorkspaceSettings />} />
-                <Route path="projects" element={<SettingsProjectList />} />
-                <Route path="asset" element={<AssetSettings />} />
-              </Route>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        {/* Beta routes - start */}
+        <Route path="scene/:sceneId/:tab" element={<BetaEditor />} />
+        <Route path="settings/project/:projectId/:tab?/:subId?" element={<BetaProjectSettings />} />
+        <Route path="graphql" element={<GraphQLPlayground />} />
+        {/* Beta routes - end */}
+        {/* classic routes - start */}
+        <Route index={true} element={<RootPage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="password-reset" element={<PasswordResetPage />} />
+        <Route path="dashboard/:workspaceId" element={<Dashboard />} />
+        <Route path="edit/:sceneId">
+          <Route index={true} element={<EarthEditor />} />
+          <Route path="preview" element={<Preview />} />
+        </Route>
+        <Route path="plugin-editor" element={<PluginEditor />} />
+        <Route path="settings">
+          <Route index={true} element={<Navigate to="/settings/account" />} />
+          <Route path="account" element={<AccountSettings />} />
+          <Route path="workspaces">
+            <Route index={true} element={<WorkspaceList />} />
+            <Route path=":workspaceId">
+              <Route index={true} element={<WorkspaceSettings />} />
+              <Route path="projects" element={<SettingsProjectList />} />
+              <Route path="asset" element={<AssetSettings />} />
             </Route>
-            <Route path="projects/:projectId">
-              <Route index={true} element={<ProjectSettings />} />
-              <Route path="public" element={<PublicSettings />} />
-              <Route path="dataset" element={<DatasetSettings />} />
-              <Route path="plugins" element={<PluginSettings />} />
-            </Route>
-            <Route path="*" element={<Navigate to="/settings/account" />} />
           </Route>
-          {...redirects.map(([from, to]) => (
-            <Route key={from} path={from} element={<Redirect to={to} />} />
-          ))}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </StyledRouter>
-      {/* classic routes - end */}
-    </>
+          <Route path="projects/:projectId">
+            <Route index={true} element={<ProjectSettings />} />
+            <Route path="public" element={<PublicSettings />} />
+            <Route path="dataset" element={<DatasetSettings />} />
+            <Route path="plugins" element={<PluginSettings />} />
+          </Route>
+          <Route path="*" element={<Navigate to="/settings/account" />} />
+        </Route>
+        {...redirects.map(([from, to]) => (
+          <Route key={from} path={from} element={<Redirect to={to} />} />
+        ))}
+        <Route path="*" element={<NotFound />} />
+        {/* classic routes - end */}
+      </>,
+    ),
   );
+  return <StyledRouter router={router} />;
 };
 
-const StyledRouter = styled(Router)`
+const StyledRouter = styled(RouterProvider)`
   height: 100%;
 `;
 
