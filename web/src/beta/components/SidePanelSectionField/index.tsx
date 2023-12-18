02aac2b79ed5df8fe77e406226e3da7ab640097e
Beatrice Mkumbo <charsbeaty@gmail.com>
2023-12-18 09:30:02 +0300

    chore(web): remember the editor settings (#825)

--- web/src/beta/components/SidePanelSectionField/index.tsx
+++ web/src/beta/components/SidePanelSectionField/index.tsx
@@ -11,13 +11,17 @@ const SidePanelSectionField: React.FC<{
   startCollapsed?: boolean;
   gap?: number;
   children?: ReactNode;
-}> = ({ className, title, startCollapsed, gap, children }) => {
+  storageKey?: string;
+}> = ({ className, title, startCollapsed, gap, children, storageKey }) => {
   const theme = useTheme();
-  const [opened, setOpened] = useState<boolean>();
+  const [opened, setOpened] = useState<boolean>(() => {
+    const storedValue = localStorage.getItem(storageKey || "");
+    return storedValue ? JSON.parse(storedValue) : !startCollapsed ?? true;
+  });
 
   useEffect(() => {
-    setOpened(!startCollapsed ?? true);
-  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+    localStorage.setItem(storageKey || "", JSON.stringify(opened));
+  }, [opened, storageKey]);
 
   return (
     <Field className={className}>
