580a9ebdffc7125e54bb75526c1a2db7cf15fdad
Piyush Chauhan <piyuschauhan1004@gmail.com>
2023-12-18 15:28:21 +0530

    remove console.log

--- web/src/beta/lib/core/engines/Cesium/Feature/HeatMap/index.tsx
+++ web/src/beta/lib/core/engines/Cesium/Feature/HeatMap/index.tsx
@@ -32,8 +32,6 @@ export default function HeatMap({ property, isVisible }: Props) {
     logarithmic = false,
   } = property ?? {};
 
-  console.log("width: ", width);
-
   const { scene } = useCesium();
 
   const boundingSphere = BoundingSphere.fromRectangle3D(
