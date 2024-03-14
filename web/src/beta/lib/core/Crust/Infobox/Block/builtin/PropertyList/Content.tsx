7e51eef40604bddd985795669c96ff87e1f862ae
KaWaite <34051327+KaWaite@users.noreply.github.com>
2024-03-14 13:02:59 +0900

    update manifest

--- web/src/beta/lib/core/Crust/Infobox/Block/builtin/PropertyList/Content.tsx
+++ web/src/beta/lib/core/Crust/Infobox/Block/builtin/PropertyList/Content.tsx
@@ -5,6 +5,7 @@ import "react18-json-view/src/dark.css";
 
 import Text from "@reearth/beta/components/Text";
 import { BlockContext } from "@reearth/beta/lib/core/shared/components/BlockWrapper";
+import Template from "@reearth/beta/lib/core/StoryPanel/Block/Template";
 import { useVisualizer } from "@reearth/beta/lib/core/Visualizer";
 import { styled } from "@reearth/services/theme";
 
@@ -38,7 +39,7 @@ type Props = {
   ) => Promise<void>;
 };
 
-const Content: React.FC<Props> = ({ block, isEditable }) => {
+const Content: React.FC<Props> = ({ block, isEditable, ...props }) => {
   const context = useContext(BlockContext);
   const visualizer = useVisualizer();
 
@@ -48,37 +49,41 @@ const Content: React.FC<Props> = ({ block, isEditable }) => {
 
   return (
     <Wrapper>
-      {properties?.map((field, idx) => {
-        const [key, value]: [string, any] = Object.entries(field)[0];
-        if (value && typeof value === "object") {
+      {!isEditable ? (
+        properties?.map((field, idx) => {
+          const [key, value]: [string, any] = Object.entries(field)[0];
+          if (value && typeof value === "object") {
+            return (
+              <ObjectWrapper key={key}>
+                <JsonView
+                  src={value}
+                  theme="a11y"
+                  collapsed={!!isEditable}
+                  style={{ wordWrap: "break-word", minWidth: 0, lineHeight: "1.5em" }}
+                />
+              </ObjectWrapper>
+            );
+          }
           return (
-            <ObjectWrapper key={key}>
-              <JsonView
-                src={value}
-                theme="a11y"
-                collapsed={!!isEditable}
-                style={{ wordWrap: "break-word", minWidth: 0, lineHeight: "1.5em" }}
-              />
-            </ObjectWrapper>
+            <PropertyWrapper key={idx} isEven={isEven(idx)}>
+              <TextWrapper>
+                <StyledText size="body" customColor otherProperties={{ userSelect: "auto" }}>
+                  {key}
+                </StyledText>
+              </TextWrapper>
+              <TextWrapper>
+                <StyledText size="body" customColor otherProperties={{ userSelect: "auto" }}>
+                  {value}
+                </StyledText>
+              </TextWrapper>
+            </PropertyWrapper>
           );
-        }
-        return (
-          <PropertyWrapper key={idx} isEven={isEven(idx)}>
-            <TextWrapper>
-              <StyledText size="body" customColor otherProperties={{ userSelect: "auto" }}>
-                {key}
-              </StyledText>
-            </TextWrapper>
-            <TextWrapper>
-              <StyledText size="body" customColor otherProperties={{ userSelect: "auto" }}>
-                {value}
-              </StyledText>
-            </TextWrapper>
-          </PropertyWrapper>
-        );
-      })}
+        })
+      ) : (
+        <Template icon={block?.extensionId} height={120} />
+      )}
       {context?.editMode && (
-        <ListEditor propertyId={block?.propertyId} property={block?.property?.default} />
+        <ListEditor propertyId={block?.propertyId} property={block?.property?.default} {...props} />
       )}
     </Wrapper>
   );
@@ -121,18 +126,14 @@ function filterChildObjectsToEnd(inputObject?: any): any[] {
   const arrayResult: any[] = [];
   const childObjects: [string, any][] = [];
 
-  // Iterate over the properties of the input object
   for (const key in inputObject) {
     if (typeof inputObject[key] === "object" && inputObject[key] !== null) {
-      // If the property value is an object, store it to process later
       childObjects.push([key, inputObject[key]]);
     } else {
-      // If it's not an object, push it to the result array
       arrayResult.push({ [key]: inputObject[key] });
     }
   }
 
-  // Push child objects to the end of the result array
   childObjects.forEach(([key, value]) => {
     arrayResult.push({ [key]: value });
   });
