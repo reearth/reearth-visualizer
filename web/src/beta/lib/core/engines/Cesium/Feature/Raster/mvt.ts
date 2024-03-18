b56c3adfcbd4f6b100ad9e84c80357546386d804
pyshx <piyushchauhan1004@gmail.com>
2024-03-18 20:57:32 +0530

    refactor

--- web/src/beta/lib/core/engines/Cesium/Feature/Raster/mvt.ts
+++ web/src/beta/lib/core/engines/Cesium/Feature/Raster/mvt.ts
@@ -1,9 +1,8 @@
-import { useEffect, useMemo, useRef } from "react";
+import { useMemo } from "react";
 
 import { MVTImageryProvider } from "@reearth/cesium-mvt-imagery-provider";
 
-import { MarkerAppearance, PolygonAppearance, PolylineAppearance } from "../../..";
-import { extractSimpleLayer, usePick } from "../utils";
+import { extractSimpleLayer } from "../utils";
 
 import { useData, useImageryProvider } from "./hooks";
 import type { Props } from "./types";
@@ -17,21 +16,10 @@ export const useMVT = ({
   const { show = true, minimumLevel, maximumLevel, credit } = property ?? {};
   const { type, url, layers } = useData(layer);
 
-  const cachedCalculatedLayerRef = useRef(layer);
-  const updatedAt = useRef<number>();
-
   const currentLayer = extractSimpleLayer(layer) || undefined;
-  console.log("currentLayer from reearth: ", currentLayer);
-
-  const layerPolygonAppearance = usePick(currentLayer?.polygon, polygonAppearanceFields);
-  const layerPolylineAppearance = usePick(currentLayer?.polyline, polylineAppearanceFields);
-  const layerMarkerAppearance = usePick(currentLayer?.marker, markerAppearanceFields);
-  const layerVisibility = currentLayer?.visible;
-
+  console.log("currentLayer at Reearth: ", currentLayer);
   const imageryProvider = useMemo(() => {
     if (!isVisible || !show || !url || !layers || type !== "mvt") return;
-    console.log("reached here!!!");
-    console.log("updatedAt: ", updatedAt.current);
     return new MVTImageryProvider({
       minimumLevel,
       maximumLevel,
@@ -40,50 +28,8 @@ export const useMVT = ({
       layerName: layers,
       layer: currentLayer,
       worker: true,
-      updatedAt: updatedAt.current || Date.now(),
     });
   }, [isVisible, show, url, layers, type, minimumLevel, maximumLevel, currentLayer, credit]);
 
-  const cacheKeyForUpdatedAt = useMemo(
-    () =>
-      JSON.stringify(layerPolygonAppearance) +
-      JSON.stringify(layerPolylineAppearance) +
-      JSON.stringify(layerMarkerAppearance) +
-      layerVisibility,
-    [layerPolygonAppearance, layerPolylineAppearance, layerMarkerAppearance, layerVisibility],
-  );
-
-  useEffect(() => {
-    if (layerVisibility !== undefined) {
-      updatedAt.current = Date.now();
-    }
-  }, [layerVisibility]);
-
-  useEffect(() => {
-    updatedAt.current = Date.now();
-  }, [cacheKeyForUpdatedAt]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  useEffect(() => {
-    cachedCalculatedLayerRef.current = layer;
-  }, [layer]);
-
   useImageryProvider(imageryProvider, layer?.id, property);
 };
-
-const polygonAppearanceFields: (keyof PolygonAppearance)[] = [
-  "show",
-  "fill",
-  "fillColor",
-  "stroke",
-  "strokeColor",
-  "strokeWidth",
-  "lineJoin",
-];
-
-const polylineAppearanceFields: (keyof PolylineAppearance)[] = [
-  "show",
-  "strokeColor",
-  "strokeWidth",
-];
-
-const markerAppearanceFields: (keyof MarkerAppearance)[] = ["show", "pointColor", "pointSize"];
