3726711419fccab53694fa39a586cb8eafd13765
pyshx <piyushchauhan1004@gmail.com>
2024-03-20 11:11:45 +0530

    remove ga from story publishing for now

--- server/internal/usecase/interactor/storytelling.go
+++ server/internal/usecase/interactor/storytelling.go
@@ -260,13 +260,13 @@ func (i *Storytelling) Publish(ctx context.Context, inp interfaces.PublishStoryI
 		return nil, err
 	}
 
-	prj, err := i.projectRepo.FindByScene(ctx, story.Scene())
-	if err != nil {
-		return nil, err
-	}
+	// prj, err := i.projectRepo.FindByScene(ctx, story.Scene())
+	// if err != nil {
+	// 	return nil, err
+	// }
 
-	enableGa := prj.EnableGA()
-	trackingId := prj.TrackingID()
+	// enableGa := prj.EnableGA()
+	// trackingId := prj.TrackingID()
 
 	//
 	// Commenting this out till the point we make a decision on this: @pyshx
@@ -359,7 +359,7 @@ func (i *Storytelling) Publish(ctx context.Context, inp interfaces.PublishStoryI
 				repo.TagLoaderFrom(i.tagRepo),
 				repo.TagSceneLoaderFrom(i.tagRepo, scenes),
 				repo.NLSLayerLoaderFrom(i.nlsLayerRepo),
-			).ForScene(scene).WithNLSLayers(&nlsLayers).WithLayerStyle(layerStyles).WithStory(story).Build(ctx, w, time.Now(), true, enableGa, trackingId)
+			).ForScene(scene).WithNLSLayers(&nlsLayers).WithLayerStyle(layerStyles).WithStory(story).Build(ctx, w, time.Now(), true, false, "")
 		}()
 
 		// Save
