895ef3c02203473edf4bc707b697971fafdc89c5
akiyatomohiro <thairwars@gmail.com>
2024-04-11 23:00:18 +0900

    feat(server): in-memory cache support for style

--- server/internal/usecase/gateway/redis.go
+++ server/internal/usecase/gateway/redis.go
@@ -5,4 +5,5 @@ import "context"
 type RedisGateway interface {
 	GetValue(ctx context.Context, key string) (string, error)
 	SetValue(ctx context.Context, key string, value interface{}) error
+	RemoveValue(ctx context.Context, key string) error
 }
