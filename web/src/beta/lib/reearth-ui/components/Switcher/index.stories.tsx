52c1fb26da990fad4b384e6fcf74aba8bda19c8c
airslice <icesunex@hotmail.com>
2024-05-28 14:55:08 +0800

    refactor: state and style

--- web/src/beta/lib/reearth-ui/components/Switcher/index.stories.tsx
+++ web/src/beta/lib/reearth-ui/components/Switcher/index.stories.tsx
@@ -1,6 +1,5 @@
 import { action } from "@storybook/addon-actions";
 import { Meta, StoryObj } from "@storybook/react";
-import { useState } from "react";
 
 import { Switcher, SwitcherProps } from ".";
 
@@ -12,43 +11,10 @@ export default meta;
 
 type Story = StoryObj<SwitcherProps>;
 
-export const On: Story = {
-  render: () => (
-    <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
-      <Switcher isOn={true} onClick={action("switcher-on-click")} />
-    </div>
-  ),
-};
-
-export const Off: Story = {
-  render: () => (
-    <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
-      <Switcher isOn={false} onClick={action("switcher-off-click")} />
-    </div>
-  ),
+export const Default: Story = {
+  render: () => <Switcher value={true} onChange={action("onChange")} />,
 };
 
 export const Disabled: Story = {
-  render: () => (
-    <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
-      <Switcher isOn={false} onClick={action("switcher-disabled-off-click")} disabled={true} />
-    </div>
-  ),
-};
-
-const ToggleComponent: React.FC = () => {
-  const [isOn, setIsOn] = useState(false);
-  const handleClick = () => {
-    action("switcher-toggle-click")();
-    setIsOn((prev: boolean) => !prev);
-  };
-  return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "8px" }}>
-      <Switcher isOn={isOn} onClick={handleClick} />
-    </div>
-  );
-};
-
-export const Toggle: Story = {
-  render: () => <ToggleComponent />,
+  render: () => <Switcher value={false} onChange={action("onChange")} disabled />,
 };
--- web/src/beta/lib/reearth-ui/components/Switcher/index.tsx
+++ web/src/beta/lib/reearth-ui/components/Switcher/index.tsx
@@ -1,16 +1,29 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 
 import { styled } from "@reearth/services/theme";
 
 export type SwitcherProps = {
-  isOn: boolean;
-  onClick: () => void;
+  value?: boolean;
   disabled?: boolean;
+  onChange?: (value: boolean) => void;
 };
 
-export const Switcher: FC<SwitcherProps> = ({ isOn, onClick, disabled }) => {
+export const Switcher: FC<SwitcherProps> = ({ value, disabled, onChange }) => {
+  const [isOn, setIsOn] = useState(!!value);
+
+  const handleClick = () => {
+    if (disabled) return;
+    const newValue = !isOn;
+    setIsOn(newValue);
+    onChange?.(newValue);
+  };
+
+  useEffect(() => {
+    setIsOn(!!value);
+  }, [value]);
+
   return (
-    <SwitcherContainer onClick={onClick} isOn={isOn} disabled={disabled}>
+    <SwitcherContainer onClick={handleClick} isOn={isOn} disabled={disabled}>
       <SwitcherCircle isOn={isOn} disabled={disabled} />
     </SwitcherContainer>
   );
@@ -28,7 +41,11 @@ const SwitcherContainer = styled.div<{ isOn: boolean; disabled?: boolean }>(
       : theme.outline.weaker,
     display: "flex",
     alignItems: "center",
+    boxShadow: theme.shadow.input,
     cursor: disabled ? "not-allowed" : "pointer",
+    transition: "background-color 0.3s",
+    padding: "1px",
+    boxSizing: "border-box",
   }),
 );
 
@@ -39,6 +56,6 @@ const SwitcherCircle = styled.div<{ isOn: boolean; disabled?: boolean }>(
     borderRadius: "50%",
     backgroundColor: disabled ? theme.outline.weak : theme.content.withBackground,
     transition: "transform 0.3s",
-    transform: isOn ? "translateX(12px)" : "translateX(0)",
+    transform: isOn ? "translateX(10px)" : "translateX(0)",
   }),
 );
