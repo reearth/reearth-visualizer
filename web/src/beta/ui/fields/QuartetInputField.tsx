3333567a313d993fc62868b7f6235a87a487b914
m-abe-dev <uuu.theman.jp@gmail.com>
2024-06-26 00:09:07 +0200

    fix textinputs and add fields

--- web/src/beta/ui/fields/QuartetInputField.tsx
+++ web/src/beta/ui/fields/QuartetInputField.tsx
@@ -1,27 +1,80 @@
-import { FC } from "react";
+import { FC, useCallback, useState } from "react";
 
-import { TextInput, TextInputProps } from "@reearth/beta/lib/reearth-ui";
+import { TextInput } from "@reearth/beta/lib/reearth-ui";
 import { styled } from "@reearth/services/theme";
 
 import CommonField, { CommonFieldProps } from "./CommonField";
 
-export type QuartetInputFieldProps = CommonFieldProps &
-  Pick<TextInputProps, "value" | "placeholder" | "onChange" | "onBlur" | "disabled">;
+export type QuadrupletInputFieldProps = CommonFieldProps & {
+  values?: [string, string, string, string];
+  placeholders?: [string, string, string, string];
+  disabled?: boolean;
+  onChange?: (values: [string, string, string, string]) => void;
+  onBlur?: (values: [string, string, string, string]) => void;
+};
 
-const QuartetInputField: FC<QuartetInputFieldProps> = ({ title, description, ...props }) => {
+const QuadrupletInputField: FC<QuadrupletInputFieldProps> = ({
+  title,
+  description,
+  values = ["", "", "", ""],
+  placeholders = ["", "", "", ""],
+  disabled,
+  onChange,
+  onBlur,
+}) => {
+  const [inputValues, setInputValues] = useState<[string, string, string, string]>(values);
+
+  const handleChange = useCallback(
+    (index: number, value: string) => {
+      const newValues = [...inputValues] as [string, string, string, string];
+      newValues[index] = value;
+      setInputValues(newValues);
+      onChange?.(newValues);
+    },
+    [inputValues, onChange],
+  );
+
+  const handleBlur = useCallback(() => {
+    onBlur?.(inputValues);
+  }, [inputValues, onBlur]);
+
   return (
     <CommonField title={title} description={description}>
       <InputWrapper>
-        <TextInput {...props} />
-        <TextInput {...props} />
-        <TextInput {...props} />
-        <TextInput {...props} />
+        <TextInput
+          value={inputValues[0]}
+          placeholder={placeholders[0]}
+          disabled={disabled}
+          onChange={value => handleChange(0, value)}
+          onBlur={handleBlur}
+        />
+        <TextInput
+          value={inputValues[1]}
+          placeholder={placeholders[1]}
+          disabled={disabled}
+          onChange={value => handleChange(1, value)}
+          onBlur={handleBlur}
+        />
+        <TextInput
+          value={inputValues[2]}
+          placeholder={placeholders[2]}
+          disabled={disabled}
+          onChange={value => handleChange(2, value)}
+          onBlur={handleBlur}
+        />
+        <TextInput
+          value={inputValues[3]}
+          placeholder={placeholders[3]}
+          disabled={disabled}
+          onChange={value => handleChange(3, value)}
+          onBlur={handleBlur}
+        />
       </InputWrapper>
     </CommonField>
   );
 };
 
-export default QuartetInputField;
+export default QuadrupletInputField;
 
 const InputWrapper = styled.div`
   display: flex;
