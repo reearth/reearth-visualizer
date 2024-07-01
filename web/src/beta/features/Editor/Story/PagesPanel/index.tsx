2e84bef4bba6afb5eb50204da5daf628e2dbe380
mkumbobeaty <charsbeaty@gmail.com>
2024-07-01 14:35:29 +0300

    implemented page panel

--- web/src/beta/features/Editor/Story/PagesPanel/index.tsx
+++ web/src/beta/features/Editor/Story/PagesPanel/index.tsx
@@ -1,135 +1,110 @@
-import { FC, useEffect, useState } from "react";
+import { FC, useCallback, useMemo, useState } from "react";
 
-import DragAndDropList from "@reearth/beta/components/DragAndDropList";
-import { getFieldValue } from "@reearth/beta/features/Visualizer/Crust/StoryPanel/utils";
+import { Button, DragAndDropList } from "@reearth/beta/lib/reearth-ui";
 import { Panel, PanelProps } from "@reearth/beta/ui/layout";
-import { isEmptyString } from "@reearth/beta/utils/util";
-import { useT } from "@reearth/services/i18n";
 import { styled } from "@reearth/services/theme";
 
 import { useStoryPage } from "../context";
 
-import Action from "./Action";
 import PageItem from "./PageItem";
-import PageItemWrapper from "./PageItemWrapper";
 
 type Props = Pick<PanelProps, "showCollapseArea" | "areaRef">;
+const PAGES_DRAG_HANDLE_CLASS_NAME = "reearth-visualizer-editor-layers-drag-handle";
 
 const PagesPanel: FC<Props> = ({ showCollapseArea, areaRef }) => {
-  const {
-    storyPages,
-    selectedStoryPage,
-    handleStoryPageSelect,
-    handleStoryPageAdd,
-    handleStoryPageDelete,
-    handleStoryPageMove,
-    handlePropertyValueUpdate,
-  } = useStoryPage();
+  const { storyPages, handleStoryPageSelect, handleStoryPageAdd, handleStoryPageMove } =
+    useStoryPage();
 
-  const t = useT();
   const [openedPageId, setOpenedPageId] = useState<string | undefined>(undefined);
 
-  const [items, setItems] = useState(storyPages ?? []);
+  const [isDragging, setIsDragging] = useState(false);
 
-  useEffect(() => {
-    setItems(storyPages ?? []);
-  }, [storyPages]);
+  const DraggableLayerItems = useMemo(
+    () =>
+      storyPages?.map((storyPage, index) => ({
+        id: storyPage.id,
+        content: (
+          <PageItem
+            pageCount={index + 1}
+            storyPage={storyPage}
+            dragHandleClassName={PAGES_DRAG_HANDLE_CLASS_NAME}
+            isDragging={isDragging}
+          />
+        ),
+      })),
+    [storyPages, isDragging],
+  );
+
+  const handleMoveStart = useCallback(() => {
+    setIsDragging(true);
+  }, []);
+
+  const handleMoveEnd = useCallback(
+    (itemId?: string, newIndex?: number) => {
+      if (itemId !== undefined && newIndex !== undefined) {
+        handleStoryPageMove?.(itemId, newIndex);
+      }
+      setIsDragging(false);
+    },
+    [handleStoryPageMove],
+  );
 
   return (
     <Panel
       title="Pages"
       extend
-      alwaysOpen
       storageId="editor-story-pages-panel"
       showCollapseArea={showCollapseArea}
       areaRef={areaRef}>
-      <SContent>
-        <SContentUp onScroll={openedPageId ? () => setOpenedPageId(undefined) : undefined}>
-          <DragAndDropList
-            uniqueKey="LeftPanelPages"
-            gap={8}
-            items={items}
-            getId={item => item.id}
-            onItemDrop={async (item, index) => {
-              setItems(old => {
-                const items = [...old];
-                items.splice(
-                  old.findIndex(o => o.id === item.id),
-                  1,
-                );
-                items.splice(index, 0, item);
-                return items;
-              });
-              await handleStoryPageMove(item.id, index);
-            }}
-            renderItem={(storyPage, i) => {
-              const title = (getFieldValue(storyPage.property.items ?? [], "title", "title") ??
-                t("Untitled")) as string;
-              const hasEmptySpace = isEmptyString(title);
-              return (
-                <PageItemWrapper
-                  key={storyPage.id}
-                  pageCount={i + 1}
-                  isSwipeable={storyPage.swipeable}>
-                  <PageItem
-                    key={i}
-                    isSelected={selectedStoryPage?.id === storyPage.id}
-                    isOpenAction={openedPageId === storyPage.id}
-                    onItemClick={() => handleStoryPageSelect(storyPage.id)}
-                    onActionClick={() => setOpenedPageId(old => (old ? undefined : storyPage.id))}
-                    onOpenChange={isOpen => {
-                      setOpenedPageId(isOpen ? storyPage.id : undefined);
-                    }}
-                    onPageDelete={() => handleStoryPageDelete(storyPage.id)}
-                    title={hasEmptySpace || !title ? t("Untitled") : title}
-                    setOpenedPageId={setOpenedPageId}
-                    propertyId={storyPage.property.id}
-                    storyPage={storyPage}
-                    onPropertyUpdate={handlePropertyValueUpdate}
-                  />
-                </PageItemWrapper>
-              );
-            }}
-          />
-        </SContentUp>
-        <SContentBottom>
-          <Action
-            icon="square"
-            title={`+ ${t("New Page")}`}
-            onClick={() => handleStoryPageAdd(false)}
-          />
-          {/* <Action icon="swiper" title={`+ ${t("New Swipe")}`} onClick={() => onPageAdd(true)} /> */}
-        </SContentBottom>
-      </SContent>
+      <ButtonWrapper>
+        <Button
+          icon="plus"
+          title="New Page"
+          size="small"
+          extendWidth
+          onClick={() => handleStoryPageAdd(false)}
+        />
+      </ButtonWrapper>
+      <Wrapper onScroll={openedPageId ? () => setOpenedPageId(undefined) : undefined}>
+        <DragAndDropList
+          items={DraggableLayerItems}
+          handleClassName={PAGES_DRAG_HANDLE_CLASS_NAME}
+          onMoveEnd={handleMoveEnd}
+          onMoveStart={handleMoveStart}
+          dragDisabled={false}
+        />
+        <EmptySpace onClick={() => handleStoryPageSelect(undefined)} />
+      </Wrapper>
     </Panel>
   );
 };
 
 export default PagesPanel;
 
-const SContent = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-`;
+const Wrapper = styled("div")(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  gap: theme.spacing.normal,
+  overflowY: "auto",
+  boxSizing: "border-box",
+  "::-webkit-scrollbar": {
+    display: "none",
+  },
+}));
 
-const SContentUp = styled.div`
-  flex-grow: 1;
-  height: 0;
-  overflow-y: auto;
-  ::-webkit-scrollbar {
-    display: none;
-  }
+const ButtonWrapper = styled("div")(({ theme }) => ({
+  padding: `${theme.spacing.small}px 0`,
+}));
 
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
-  box-sizing: border-box;
-`;
-
-const SContentBottom = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 4px;
-  margin-top: 8px;
-`;
+const EmptySpace = styled("div")(() => ({
+  flex: 1,
+  minHeight: 50,
+}));
+// const ContentWrapper = styled("div")(() => ({
+//   overflowY: "auto",
+//   boxSizing: "border-box",
+//   "::-webkit-scrollbar": {
+//     display: "none",
+//   },
+// }));
