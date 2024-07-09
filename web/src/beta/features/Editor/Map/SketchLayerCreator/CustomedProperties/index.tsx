1aa05552b75ec362d450c969e1e2df8e08b87148
mkumbobeaty <charsbeaty@gmail.com>
2024-07-09 04:09:09 +0300

    fixed unexpected deletion

---
+++ web/src/beta/features/Editor/Map/SketchLayerCreator/CustomedProperties/index.tsx
@@ -0,0 +1,74 @@
+import Button from "@reearth/beta/components/Button";
+import DragAndDropList from "@reearth/beta/components/DragAndDropList";
+import {
+  ColJustifyBetween,
+  AddButtonWrapper,
+  PropertyListHeader,
+  TitledText,
+} from "@reearth/beta/features/Editor/utils";
+import { useT } from "@reearth/services/i18n";
+import { useTheme } from "@reearth/services/theme";
+
+import { PropertyProps, SketchProps } from "..";
+
+import useHooks from "./hooks";
+import PropertyItem from "./PropertyItem";
+
+const CustomedProperties: React.FC<SketchProps> = ({
+  customPropertyList,
+  currentProperties,
+  setCurrentProperties,
+  setCustomPropertyList,
+}) => {
+  const t = useT();
+  const theme = useTheme();
+
+  const {
+    handleRemovePropertyToList,
+    handlePropertyDrop,
+    handlePropertyAdd,
+    handleKeyChange,
+    handleValueChange,
+  } = useHooks({
+    customPropertyList,
+    currentProperties,
+    setCurrentProperties,
+    setCustomPropertyList,
+  });
+
+  return (
+    <ColJustifyBetween>
+      <PropertyListHeader>
+        <TitledText size="footnote" color={theme.content.weaker}>
+          {t("Name")}
+        </TitledText>
+        <TitledText size="footnote" color={theme.content.weaker}>
+          {t("Type")}
+        </TitledText>
+      </PropertyListHeader>
+      {currentProperties && currentProperties?.length > 0 && (
+        <DragAndDropList<PropertyProps>
+          uniqueKey="custom-property"
+          gap={8}
+          items={(currentProperties as PropertyProps[]) || []}
+          getId={item => item.id}
+          onItemDrop={handlePropertyDrop}
+          renderItem={(property, idx) => (
+            <PropertyItem
+              key={property.id}
+              property={property}
+              onKeyChange={handleKeyChange(idx)}
+              onValueChange={handleValueChange(idx)}
+              onRemovePropertyItem={() => handleRemovePropertyToList(idx)}
+            />
+          )}
+        />
+      )}
+      <AddButtonWrapper>
+        <Button icon="plus" text={t("New Property")} size="small" onClick={handlePropertyAdd} />
+      </AddButtonWrapper>
+    </ColJustifyBetween>
+  );
+};
+
+export default CustomedProperties;
