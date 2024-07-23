c4edf580f2c57977acc2c6f924481b1f737cca5c
mkumbobeaty <charsbeaty@gmail.com>
2024-07-23 10:52:14 +0300

    add title in layer style

--- web/src/beta/features/Editor/Map/InspectorPanel/LayerInspector/LayerStyle/index.tsx
+++ web/src/beta/features/Editor/Map/InspectorPanel/LayerInspector/LayerStyle/index.tsx
@@ -3,8 +3,10 @@ import { FC, useCallback, useMemo } from "react";
 import { Selector } from "@reearth/beta/lib/reearth-ui/components/Selector";
 import { NLSLayer } from "@reearth/services/api/layersApi/utils";
 import { LayerStyle } from "@reearth/services/api/layerStyleApi/utils";
+import { useT } from "@reearth/services/i18n";
 
 import { LayerConfigUpdateProps } from "../../../../hooks/useLayers";
+import { InputGroup } from "../../../SharedComponent";
 
 type LayerStyleSelectorProps = {
   layerStyles?: LayerStyle[];
@@ -20,6 +22,7 @@ const LayerStyleTab: FC<LayerStyleSelectorProps> = ({
   selectedLayerId,
   onLayerConfigUpdate,
 }) => {
+  const t = useT();
   const layerStyleOptions = useMemo(
     () => [
       { value: "", label: "NO STYLE" },
@@ -47,7 +50,13 @@ const LayerStyleTab: FC<LayerStyleSelectorProps> = ({
   );
 
   return (
-    <Selector options={layerStyleOptions} value={currentValue} onChange={handleLayerStyleChange} />
+    <InputGroup label={t("Layer Style")}>
+      <Selector
+        options={layerStyleOptions}
+        value={currentValue}
+        onChange={handleLayerStyleChange}
+      />
+    </InputGroup>
   );
 };
 
