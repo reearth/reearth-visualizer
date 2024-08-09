a66503b86e9f6ad2e06822ea412d83b877fda4fa
Piyush Chauhan <42397980+pyshx@users.noreply.github.com>
2024-08-09 12:58:52 +0530

    chore(server, web): filter out classic asset from visualizer (#1095)

--- server/internal/infrastructure/mongo/asset.go
+++ server/internal/infrastructure/mongo/asset.go
@@ -77,6 +77,11 @@ func (r *Asset) FindByWorkspace(ctx context.Context, id accountdomain.WorkspaceI
 		})
 	}
 
+	// Classic's Assets shouldn't been shown @pyshx
+	filter = mongox.And(filter, "url", bson.M{
+		"$regex": primitive.Regex{Pattern: "visualizer", Options: "i"},
+	})
+
 	return r.paginate(ctx, filter, uFilter.Sort, uFilter.Pagination)
 }
 
