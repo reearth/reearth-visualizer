63e03fd45bd08d62a58ca0d5d90ce2a258909574
airslice <icesunex@hotmail.com>
2024-08-27 12:58:08 +0800

    refactor: better readability

--- web/src/beta/features/Visualizer/Crust/Infobox/Block/builtin/useExpressionEval.ts
+++ web/src/beta/features/Visualizer/Crust/Infobox/Block/builtin/useExpressionEval.ts
@@ -27,14 +27,13 @@ export default (value: unknown | undefined) => {
 
     // NOTE: core's selectedFeature is not always up to date, but the computed.features from selectedLayer is always up to date
     // TODO: fix it on core
-    let selectedFeature = visualizer.current?.layers.selectedFeature();
     const selectedLayer = visualizer.current?.layers.selectedLayer();
-
-    if (selectedLayer?.type === "simple" && selectedLayer?.data?.isSketchLayer) {
-      selectedFeature = selectedLayer.computed?.features.find(f => f.id === selectedFeature?.id);
-    } else {
-      selectedFeature = visualizer.current?.layers.selectedFeature();
-    }
+    const selectedFeature =
+      selectedLayer?.type === "simple" && selectedLayer?.data?.isSketchLayer
+        ? selectedLayer.computed?.features.find(
+            f => f.id === visualizer.current?.layers.selectedFeature()?.id,
+          )
+        : visualizer.current?.layers.selectedFeature();
 
     if (selectedFeature && selectedFeature.id !== lastFeatureSelected) {
       setLastFeatureSelected(selectedFeature.id);
