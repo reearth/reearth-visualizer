afd947b57fdf5d919edca8b6f86afe1e33dd27bc
tomokazu tantaka <t.tantaka@eukarya.io>
2024-10-16 18:59:55 +0900

    update test

--- server/e2e/mock_test.go
+++ server/e2e/mock_test.go
@@ -7,6 +7,8 @@ import (
 	"github.com/reearth/reearth/server/internal/app/config"
 )
 
+// go test -v -run TestMockAuth ./e2e/...
+
 func TestMockAuth(t *testing.T) {
 	e := StartServer(t, &config.Config{
 		Dev:      true,
@@ -15,7 +17,7 @@ func TestMockAuth(t *testing.T) {
 		AuthSrv: config.AuthSrvConfig{
 			Disabled: true,
 		},
-	}, false, nil)
+	}, true, nil)
 
 	requestBody := map[string]interface{}{
 		"email":    "mock@example.com",
@@ -31,36 +33,25 @@ func TestMockAuth(t *testing.T) {
 	response.Object().ContainsKey("id")
 	response.Object().ValueEqual("email", "mock@example.com")
 	response.Object().ValueEqual("name", "Mock User")
-	// userId := response.Object().Value("id").String().Raw()
+	userId := response.Object().Value("id").String().Raw()
 
-	// e2 := StartServer(t, &config.Config{
-	// 	Dev:      true,
-	// 	MockAuth: true,
-	// 	MockUser: userId,
-	// 	Origins:  []string{"https://example.com"},
-	// 	AuthSrv: config.AuthSrvConfig{
-	// 		Disabled: true,
-	// 	},
-	// }, false, nil)
+	// checkj query GetMe
+	requestBody2 := GraphQLRequest{
+		OperationName: "GetMe",
+		Query:         "query GetMe { \n me { \n id \n name \n email\n } \n}",
+		Variables:     map[string]any{},
+	}
+	response2 := e.POST("/api/graphql").
+		WithHeader("Origin", "https://example.com").
+		WithHeader("X-Reearth-Debug-User", userId).
+		WithHeader("Content-Type", "application/json").
+		WithJSON(requestBody2).
+		Expect().
+		Status(http.StatusOK).
+		JSON().
+		Object()
 
-	// requestBody2 := GraphQLRequest{
-	// 	OperationName: "GetMe",
-	// 	Query:         "query GetMe { \n me { \n id \n name \n email\n } \n}",
-	// 	Variables:     map[string]any{},
-	// }
-
-	// response2 := e2.POST("/api/graphql").
-	// 	WithHeader("Origin", "https://example.com").
-	// 	WithHeader("X-Reearth-Debug-User", userId).
-	// 	WithHeader("Content-Type", "application/json").
-	// 	WithJSON(requestBody2).
-	// 	Expect().
-	// 	Status(http.StatusOK).
-	// 	JSON().
-	// 	Object()
-
-	// fmt.Println(response2.Raw())
-	// response2.Value("data").Object().Value("me").Object().ContainsKey("id")
-	// response2.Value("data").Object().Value("me").Object().Value("name").String().Equal("Expected Name")
-	// response2.Value("data").Object().Value("me").Object().Value("email").String().Equal("mock@example.com")
+	response2.Value("data").Object().Value("me").Object().ContainsKey("id")
+	response2.Value("data").Object().Value("me").Object().Value("name").String().Equal("Mock User")
+	response2.Value("data").Object().Value("me").Object().Value("email").String().Equal("mock@example.com")
 }
