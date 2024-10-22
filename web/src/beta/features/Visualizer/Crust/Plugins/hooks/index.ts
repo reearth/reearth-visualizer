880e54f7bf8a7ee4bf34c6c5f5a667ede723904f
mkumbobeaty <charsbeaty@gmail.com>
2024-10-22 12:45:59 +0300

    remove layersInViewport

--- web/src/beta/features/Visualizer/Crust/Plugins/hooks/index.ts
+++ web/src/beta/features/Visualizer/Crust/Plugins/hooks/index.ts
@@ -111,7 +111,6 @@ export default function ({
     addLayer,
     findFeatureById,
     findFeaturesByIds,
-    layersInViewport,
     selectLayer,
     selectFeature,
     selectFeatures,
@@ -218,7 +217,6 @@ export default function ({
         addLayer,
         findFeatureById,
         findFeaturesByIds,
-        layersInViewport,
         selectLayer,
         selectFeature,
         selectFeatures,
@@ -257,100 +255,7 @@ export default function ({
       layersEvents,
       sketchEvents
     }),
-    [
-      engineName,
-      // viewer
-      getViewerProperty,
-      overrideViewerPropertyCommon,
-      getViewport,
-      captureScreen,
-      getInteractionMode,
-      getInEditor,
-      getIsBuilt,
-      // viewer tools
-      getLocationFromScreenCoordinate,
-      getScreenCoordinateFromPosition,
-      getTerrainHeightAsync,
-      getGlobeHeight,
-      getGlobeHeightByCamera,
-      cartographicToCartesian,
-      cartesianToCartographic,
-      transformByOffsetOnScreen,
-      isPositionVisibleOnGlobe,
-      // viewer events
-      viewerEventsOn,
-      viewerEventsOff,
-      // camera
-      getCameraPosition,
-      getCameraFov,
-      getCameraAspectRatio,
-      getCameraViewport,
-      zoomIn,
-      zoomOut,
-      setView,
-      flyTo,
-      flyToBoundingBox,
-      getGlobeIntersection,
-      enableScreenSpaceCameraController,
-      overrideScreenSpaceCameraController,
-      lookAt,
-      rotateAround,
-      rotateRight,
-      orbit,
-      move,
-      moveOverTerrain,
-      enableForceHorizontalRoll,
-      // camera events
-      cameraEventsOn,
-      cameraEventsOff,
-      // timeline
-      getTimeline,
-      // layers
-      getLayers,
-      hideLayer,
-      showLayer,
-      addLayer,
-      findFeatureById,
-      findFeaturesByIds,
-      layersInViewport,
-      selectLayer,
-      selectFeature,
-      selectFeatures,
-      getSelectedLayer,
-      getSelectedFeature,
-      getFeaturesInScreenRect,
-      bringToFront,
-      sendToBack,
-      // layers events
-      layersEventsOn,
-      layersEventsOff,
-      // sketch
-      getSketchTool,
-      setSketchTool,
-      getSketchOptions,
-      overrideSketchOptions,
-      // sketch events
-      sketchEventsOn,
-      sketchEventsOff,
-      // extension
-      getExtensionList,
-      // deprecated
-      deprecated,
-      cameraDeprecated,
-      layersDeprecated,
-      sketchDeprecated,
-      // others
-      overrideViewerProperty,
-      pluginInstances,
-      clientStorage,
-      timelineManagerRef,
-      viewerEvents,
-      selectionModeEvents,
-      cameraEvents,
-      timelineEvents,
-      layersEvents,
-      sketchEvents
-    ]
+    [engineName, getViewerProperty, overrideViewerPropertyCommon, getViewport, captureScreen, getInteractionMode, getInEditor, getIsBuilt, getLocationFromScreenCoordinate, getScreenCoordinateFromPosition, getTerrainHeightAsync, getGlobeHeight, getGlobeHeightByCamera, cartographicToCartesian, cartesianToCartographic, transformByOffsetOnScreen, isPositionVisibleOnGlobe, viewerEventsOn, viewerEventsOff, getCameraPosition, getCameraFov, getCameraAspectRatio, getCameraViewport, zoomIn, zoomOut, setView, flyTo, flyToBoundingBox, getGlobeIntersection, enableScreenSpaceCameraController, overrideScreenSpaceCameraController, lookAt, rotateAround, rotateRight, orbit, move, moveOverTerrain, enableForceHorizontalRoll, cameraEventsOn, cameraEventsOff, getTimeline, getLayers, hideLayer, showLayer, addLayer, findFeatureById, findFeaturesByIds, selectLayer, selectFeature, selectFeatures, getSelectedLayer, getSelectedFeature, getFeaturesInScreenRect, bringToFront, sendToBack, layersEventsOn, layersEventsOff, getSketchTool, setSketchTool, getSketchOptions, overrideSketchOptions, sketchEventsOn, sketchEventsOff, getExtensionList, deprecated, cameraDeprecated, layersDeprecated, sketchDeprecated, overrideViewerProperty, pluginInstances, clientStorage, timelineManagerRef, viewerEvents, selectionModeEvents, cameraEvents, timelineEvents, layersEvents, sketchEvents]
   );
 
   // expose plugin API for developers
