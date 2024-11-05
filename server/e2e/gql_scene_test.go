89d61f17faba0a0cd8f11a69605888ea8c902239
tomokazu tantaka <t.tantaka@eukarya.io>
2024-11-05 15:58:00 +0900

    update

--- server/e2e/gql_scene_test.go
+++ server/e2e/gql_scene_test.go
@@ -94,43 +94,43 @@ func TestGetScenePlaceholderEnglish(t *testing.T) {
 
 // go test -v -run TestGetScenePlaceholderJapanese ./e2e/...
 
-// func TestGetScenePlaceholderJapanese(t *testing.T) {
+func TestGetScenePlaceholderJapanese(t *testing.T) {
 
-// 	e := StartServer(
-// 		t,
-// 		&config.Config{
-// 			Origins: []string{"https://example.com"},
-// 			AuthSrv: config.AuthSrvConfig{
-// 				Disabled: true,
-// 			},
-// 		},
-// 		true,
-// 		// Japanese user Seeder
-// 		func(ctx context.Context, r *repo.Container) error {
-// 			return baseSeederWithLang(ctx, r, language.Japanese)
-// 		},
-// 	)
-// 	pID := createProjectWithExternalImage(e, "test")
-// 	_, _, sID := createScene(e, pID)
-// 	r := getScene(e, sID)
+	e := StartServer(
+		t,
+		&config.Config{
+			Origins: []string{"https://example.com"},
+			AuthSrv: config.AuthSrvConfig{
+				Disabled: true,
+			},
+		},
+		true,
+		// Japanese user Seeder
+		func(ctx context.Context, r *repo.Container) error {
+			return baseSeederWithLang(ctx, r, language.Japanese)
+		},
+	)
+	pID := createProjectWithExternalImage(e, "test")
+	_, _, sID := createScene(e, pID)
+	r := getScene(e, sID)
 
-// 	for _, group := range r.Value("property").Object().Value("schema").Object().Value("groups").Array().Iter() {
-// 		for _, field := range group.Object().Value("fields").Array().Iter() {
-// 			fieldId := field.Object().Value("fieldId").Raw().(string)
+	for _, group := range r.Value("property").Object().Value("schema").Object().Value("groups").Array().Iter() {
+		for _, field := range group.Object().Value("fields").Array().Iter() {
+			fieldId := field.Object().Value("fieldId").Raw().(string)
 
-// 			if fieldId == "tile_type" {
-// 				field.Object().Value("placeholder").Equal("あいうえお")
-// 			}
-// 			if fieldId == "tile_url" {
-// 				field.Object().Value("placeholder").Equal("あいうえお")
-// 			}
-// 			if fieldId == "tile_zoomLevel" {
-// 				field.Object().Value("placeholder").Equal("あいうえお")
-// 			}
-// 			if fieldId == "tile_opacity" {
-// 				field.Object().Value("placeholder").Equal("あいうえお")
-// 			}
-// 		}
-// 	}
+			if fieldId == "tile_type" {
+				field.Object().Value("placeholder").Equal("タイルのタイプを選択")
+			}
+			if fieldId == "tile_url" {
+				field.Object().Value("placeholder").Equal("タイルのURLを入力")
+			}
+			if fieldId == "tile_zoomLevel" {
+				field.Object().Value("placeholder").Equal("ズームレベルを入力")
+			}
+			if fieldId == "tile_opacity" {
+				field.Object().Value("placeholder").Equal("不透明性を入力")
+			}
+		}
+	}
 
-// }
+}
