3016a71584684ad6359bdc78adfffd0f2fd01f8c
Beatrice Mkumbo <charsbeaty@gmail.com>
2024-11-07 17:26:48 -0800

    refactor(web): add layer from resource (#1212)
    Co-authored-by: airslice <icesunex@hotmail.com>

--- web/src/beta/features/Editor/Map/DataSourceLayerCreator/KML/index.tsx
+++ web/src/beta/features/Editor/Map/DataSourceLayerCreator/KML/index.tsx
@@ -6,53 +6,49 @@ import {
   ContentWrapper
 } from "@reearth/beta/features/Editor/Map/shared/SharedComponent";
 import {
-  Selector,
   RadioGroup,
-  Switcher,
   Button,
   TextInput,
-  TextArea
+  TextArea,
+  Icon
 } from "@reearth/beta/lib/reearth-ui";
 import { AssetField } from "@reearth/beta/ui/fields";
 import { useT } from "@reearth/services/i18n";
+import { styled, useTheme } from "@reearth/services/theme";
 import { FC } from "react";
 
 import { DataProps } from "..";
 
 import useHooks from "./hooks";
 
-const CommonAsset: FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
+const KML: FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
   const t = useT();
 
   const {
     value,
-    fileFormat,
-    assetsTypes,
     sourceType,
     dataSourceTypeOptions,
-    fileFormatOptions,
-    prioritizePerformance,
     handleValueChange,
     handleDataSourceTypeChange,
-    handleFileFormatChange,
-    handleSubmit,
-    setPrioritizePerformance,
-    isValidExtension
+    handleSubmit
   } = useHooks({ sceneId, onSubmit, onClose });
 
+  const theme = useTheme();
   return (
     <Wrapper>
       <ContentWrapper>
-        <InputGroup
-          label={t("File Format")}
-          description={t("File format of the data source you want to add.")}
-        >
-          <Selector
-            value={fileFormat}
-            options={fileFormatOptions}
-            onChange={handleFileFormatChange}
+        <Warning>
+          <IconWrapper
+            icon="lightBulb"
+            color={theme.warning.main}
+            size="normal"
           />
-        </InputGroup>
+          <TextWrapper>
+            {t(
+              "Support for the KML format is currently experimental and remains somewhat unstable, with certain features not yet fully supported. We advise using it with caution."
+            )}
+          </TextWrapper>
+        </Warning>
         <InputGroup label={t("Source Type")}>
           <RadioGroup
             value={sourceType}
@@ -61,18 +57,18 @@ const CommonAsset: FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
           />
         </InputGroup>
 
-        {sourceType == "local" && (
+        {sourceType === "local" && (
           <InputsWrapper>
             <AssetField
               inputMethod="asset"
               title={t("Asset")}
               value={value}
-              assetsTypes={assetsTypes}
+              assetsTypes={["kml"]}
               onChange={handleValueChange}
             />
           </InputsWrapper>
         )}
-        {sourceType == "url" && (
+        {sourceType === "url" && (
           <InputGroup label={t("Resource URL")}>
             <InputsWrapper>
               <TextInput
@@ -83,7 +79,7 @@ const CommonAsset: FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
             </InputsWrapper>
           </InputGroup>
         )}
-        {sourceType == "value" && (
+        {sourceType === "value" && (
           <InputGroup label={t("Value")}>
             <InputsWrapper>
               <TextArea
@@ -95,31 +91,34 @@ const CommonAsset: FC<DataProps> = ({ sceneId, onSubmit, onClose }) => {
             </InputsWrapper>
           </InputGroup>
         )}
-        {fileFormat === "geojson" && (
-          <InputGroup label={t("Prioritize Performance")}>
-            <Switcher
-              value={prioritizePerformance}
-              onChange={(v) => setPrioritizePerformance(v)}
-            />
-          </InputGroup>
-        )}
       </ContentWrapper>
       <SubmitWrapper>
         <Button
           title={t("Add to Layer")}
           appearance="primary"
           onClick={handleSubmit}
-          disabled={
-            ((sourceType === "url" ||
-              sourceType === "value" ||
-              sourceType === "local") &&
-              !value) ||
-            !isValidExtension()
-          }
+          disabled={!value}
         />
       </SubmitWrapper>
     </Wrapper>
   );
 };
 
-export default CommonAsset;
+const Warning = styled("div")(({ theme }) => ({
+  display: "flex",
+  gap: theme.spacing.small,
+  alignItems: "center"
+}));
+
+const IconWrapper = styled(Icon)(() => ({
+  flexGrow: 0,
+  flexShrink: 0
+}));
+
+const TextWrapper = styled("div")(({ theme }) => ({
+  color: theme.warning.main,
+  fontSize: theme.fonts.sizes.body,
+  fontWeight: theme.fonts.weight.regular
+}));
+
+export default KML;
