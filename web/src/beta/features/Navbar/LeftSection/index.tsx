7121808665ebc82a105b932cfbbafa55490d5f98
mulengawilfred <m.wilfred@eukarya.io>
2024-11-12 03:20:53 +0200

    fix: remove product name from navbar

--- web/src/beta/features/Navbar/LeftSection/index.tsx
+++ web/src/beta/features/Navbar/LeftSection/index.tsx
@@ -2,11 +2,10 @@ import {
   Icon,
   IconButton,
   PopupMenu,
-  PopupMenuItem,
-  Typography
+  PopupMenuItem
 } from "@reearth/beta/lib/reearth-ui";
 import { useT } from "@reearth/services/i18n";
-import { styled, useTheme } from "@reearth/services/theme";
+import { styled } from "@reearth/services/theme";
 import { useMemo } from "react";
 import { Link } from "react-router-dom";
 
@@ -34,7 +33,6 @@ const LeftSection: React.FC<Props> = ({
   onWorkspaceChange
 }) => {
   const t = useT();
-  const theme = useTheme();
 
   const menuItems: PopupMenuItem[] = useMemo(
     () => [
@@ -60,13 +58,6 @@ const LeftSection: React.FC<Props> = ({
 
   return (
     <Wrapper>
-      {page !== "editor" && (
-        <StyledLink to={`/`}>
-          <Typography size="body" weight="bold" color={theme.dangerous.strong}>
-            {t("Visualizer")}
-          </Typography>
-        </StyledLink>
-      )}
       <Icon icon="logo" size={30} />
       <StyledLink
         to={`/dashboard/${currentWorkspace?.id}`}
