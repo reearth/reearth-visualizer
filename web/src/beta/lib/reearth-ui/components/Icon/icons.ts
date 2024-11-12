336f8231d2f8b1918c48ca2476d307b499c103d9
mulengawilfred <m.wilfred@eukarya.io>
2024-11-12 02:21:39 +0200

    fix: remove unused icon definition

--- web/src/beta/lib/reearth-ui/components/Icon/icons.ts
+++ web/src/beta/lib/reearth-ui/components/Icon/icons.ts
@@ -86,7 +86,6 @@ import ListDashes from "./Icons/ListDashes.svg?react";
 import ListDashesFilled from "./Icons/ListDashesFilled.svg?react";
 import Lock from "./Icons/Lock.svg?react";
 import Logo from "./Icons/logo.svg?react";
-import LogoWithText from "./Icons/LogoWithText.svg?react";
 import LToS from "./Icons/LToS.svg?react";
 import MagnifyingGlass from "./Icons/MagnifyingGlass.svg?react";
 import MapPin from "./Icons/MapPin.svg?react";
@@ -248,7 +247,6 @@ export default {
   list: List,
   lock: Lock,
   logo: Logo,
-  logoWithText: LogoWithText,
   magnifyingGlass: MagnifyingGlass,
   mapPin: MapPin,
   mapTrifold: MapTrifold,
