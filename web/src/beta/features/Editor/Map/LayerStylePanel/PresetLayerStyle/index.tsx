b65410f0689dceefffc2a2f7c34f1464c014d674
Beatrice Mkumbo <charsbeaty@gmail.com>
2024-11-26 10:58:16 +0300

    refactor(web): update translation (#1277)

--- web/src/beta/features/Editor/Map/LayerStylePanel/PresetLayerStyle/index.tsx
+++ web/src/beta/features/Editor/Map/LayerStylePanel/PresetLayerStyle/index.tsx
@@ -4,7 +4,6 @@ import {
   PopupMenuItem
 } from "@reearth/beta/lib/reearth-ui";
 import { LayerStyle } from "@reearth/services/api/layerStyleApi/utils";
-import { useT } from "@reearth/services/i18n";
 import { useEffect, FC, useCallback, useRef } from "react";
 
 import { LayerStyleAddProps } from "../../../hooks/useLayerStyles";
@@ -34,14 +33,12 @@ const PresetLayerStyle: FC<PresetLayerStyleProps> = ({
   onLayerStyleAdd,
   onLayerStyleSelect
 }) => {
-  const t = useT();
-
   const layerStyleAddedRef = useRef<string | undefined>(undefined);
 
   const handleLayerStyleAddition = useCallback(
     (value?: Record<string, unknown>, styleName?: string) => {
       const name = getLayerStyleName(
-        styleName ? styleName : t("Style"),
+        styleName ? styleName : "Style",
         layerStyles
       );
       onLayerStyleAdd({
@@ -50,7 +47,7 @@ const PresetLayerStyle: FC<PresetLayerStyleProps> = ({
       });
       layerStyleAddedRef.current = name;
     },
-    [t, layerStyles, onLayerStyleAdd]
+    [layerStyles, onLayerStyleAdd]
   );
 
   useEffect(() => {
@@ -66,78 +63,78 @@ const PresetLayerStyle: FC<PresetLayerStyleProps> = ({
   const menuItems: PopupMenuItem[] = [
     {
       id: "empty",
-      title: t("Empty"),
+      title: "Empty",
       onClick: () => handleLayerStyleAddition({})
     },
     {
       id: "default",
-      title: t("Default"),
+      title: "Default",
       onClick: () => handleLayerStyleAddition(defaultStyle, "Default")
     },
     {
       id: "professional",
-      title: t("Professional"),
+      title: "Professional",
       onClick: () => handleLayerStyleAddition(professionalStyle, "Professional")
     },
     {
       id: "basicGeometry",
-      title: t("Basic Geometry"),
+      title: "Basic Geometry",
       icon: "folderSimple",
       subItem: [
         {
           id: "point",
-          title: t("Points"),
+          title: "Points",
           onClick: () => handleLayerStyleAddition(pointStyle, "Points")
         },
         {
           id: "pointWithLabel",
-          title: t("Point with label"),
+          title: "Point with label",
           onClick: () =>
             handleLayerStyleAddition(pointWithLabelStyle, "Point_with_label")
         },
         {
           id: "polyline",
-          title: t("Polyline"),
+          title: "Polyline",
           onClick: () => handleLayerStyleAddition(polylineStyle, "Polyline")
         },
         {
           id: "polygon",
-          title: t("Polygon"),
+          title: "Polygon",
           onClick: () => handleLayerStyleAddition(polygonStyle, "Polygon")
         },
         {
           id: "extrudedPolygon",
-          title: t("Extruded polygon"),
+          title: "Extruded polygon",
           onClick: () =>
             handleLayerStyleAddition(extrudedPolygonStyle, "Extruded_polygon")
         },
         {
           id: "threedTiles",
-          title: t("3D Tiles"),
+          title: "3D Tiles",
           onClick: () => handleLayerStyleAddition(threeDTilesStyle, "3D_tiles")
         }
       ]
     },
     {
       id: "geometry",
-      title: t("Geometry"),
+      title: "Geometry",
       icon: "folderSimple",
       subItem: [
         {
           id: "simpleStyle",
-          title: t("Simple Style"),
+          title: "Simple Style",
           onClick: () => handleLayerStyleAddition(simpleStyle, "Simple_style")
         }
       ]
     },
     {
       id: "plateau",
-      title: t("Plateau"),
+      title: "Plateau",
       icon: "folderSimple",
       subItem: [
         {
           id: "colorBuilding",
-          title: t("Color buildings by height"),
+          title: "Color buildings by height",
           onClick: () =>
             handleLayerStyleAddition(
               colorBuildingsByHeight,
