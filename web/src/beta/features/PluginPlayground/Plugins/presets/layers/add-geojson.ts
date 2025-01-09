43d8c35e6fa332374da34c1bb17d522f3108deeb
Shogo Hirasawa <origami.no.1@gmail.com>
2025-01-09 20:22:32 +0900

    feat(web): add script that add geojson layer

--- web/src/beta/features/PluginPlayground/Plugins/presets/layers/add-geojson.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/layers/add-geojson.ts
@@ -24,13 +24,57 @@ extensions:
 const widgetFile: FileType = {
   id: "layers-add-geojson",
   title: "layers-add-geojson.js",
-  sourceCode: `
-  <script>
-  </script> 
-  `
+  sourceCode: `// Example of adding a layer with GeoJSON data
+
+// Difine the Polygon with inline
+const layerGeojsonInline = {
+  type: "simple", // Must be written on first
+  data: {
+    type: "geojson", // Write the data format
+    value: { // Ensure that "value" contains GeoJSON.
+      type: "FeatureCollection", 
+      features: [
+        {
+          type: "Feature", 
+          properties: {}, 
+          geometry: {
+            type: "Polygon", 
+            coordinates: [
+              [
+                [-97.52842673316115, 28.604966534790364],
+                [-97.52842673316115, 10.990084521105842],
+                [-82.13620852840572, 10.990084521105842],
+                [-82.13620852840572, 28.604966534790364],
+                [-97.52842673316115, 28.604966534790364],
+              ],
+            ],
+          },
+        },
+      ],
+    },
+  },
+  polygon: {}, // Settings for the feature style. This statement is required even if no style is set.
 };
 
-export const responsivePanel: PluginType = {
+// Difine the Polygon with URL
+const layerGeojsonFromUrl = {
+  type: "simple",
+  data: {
+    type: "geojson", 
+    url: "https://shogohirasawa.github.io/for_delivery/sample01.geojson", // URL to the GeoJSON file
+  },
+  polygon: {}, // Settings for the feature style.  
+};
+
+// Adding the inline GeoJSON layer to Re:Earth
+reearth.layers.add(layerGeojsonInline);
+
+// Adding the GeoJSON layer from the URL to Re:Earth
+reearth.layers.add(layerGeojsonFromUrl);
+`
+};
+
+export const addGeojson: PluginType = {
   id: "add-geojson",
   title: "Add GeoJSON",
   files: [widgetFile, yamlFile]
