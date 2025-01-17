de968b98c0530e874264cb6356298cac385a0bdd
Shogo Hirasawa <origami.no.1@gmail.com>
2025-01-17 13:39:42 +0900

    feat/create example of csv

---
+++ web/src/beta/features/PluginPlayground/Plugins/presets/layers/add-csv.ts
@@ -0,0 +1,53 @@
+import { FileType, PluginType } from "../../constants";
+
+const yamlFile: FileType = {
+  id: "layers-add-csv-reearth-yml",
+  title: "reearth.yml",
+  sourceCode: `id: layers-add-csv-plugin
+name: Add CSV
+version: 1.0.0
+extensions:
+  - id: layers-add-csv
+    type: widget
+    name: Add CSV
+    description: Add CSV
+    widgetLayout:
+      defaultLocation:
+        zone: outer
+        section: left
+        area: top
+  `,
+  disableEdit: true,
+  disableDelete: true
+};
+
+const widgetFile: FileType = {
+  id: "layers-add-csv",
+  title: "layers-add-csv.js",
+  sourceCode: `// Example of adding a layer with CSV data
+
+// Difine CSV data
+const layerCsv = {
+  type: "simple", // Required
+  data: {
+    type: "csv",
+    url: "https://api.visualizer.test.reearth.dev/assets/01jhs7xzs5ny1e7sy5zzpm9ghr.csv", // URL of CSV
+    csv: { // Difine by column number or column name
+      idColumn: "id", 
+      lngColumn: "longitude",
+      latColumn: "latitude",
+    },
+  },
+  marker: {},// Settings for the feature style. This statement is required even if no style is set.
+};
+
+// Add the CSV layer from the URL to Re:Earth
+reearth.layers.add(layerCsv);
+`
+};
+
+export const addCsv: PluginType = {
+  id: "add-csv",
+  title: "Add CSV",
+  files: [widgetFile, yamlFile]
+};
