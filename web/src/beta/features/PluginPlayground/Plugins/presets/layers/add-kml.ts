62a163d738402962424802ccb713408887f74c87
Shogo Hirasawa <origami.no.1@gmail.com>
2025-01-17 11:40:09 +0900

    feat/add a kml example

--- web/src/beta/features/PluginPlayground/Plugins/presets/layers/add-kml.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/layers/add-kml.ts
@@ -24,8 +24,38 @@ extensions:
 const widgetFile: FileType = {
   id: "layers-add-kml",
   title: "layers-add-kml.js",
-  sourceCode: `will create script
-`
+  sourceCode: `// Example of adding a layer with KML data
+
+// Difine encoded KML in a variable
+const encodedKml =
+  "data:text/plain;charset=UTF-8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22UTF-8%22%3F%3E%0A%3Ckml%20xmlns%3D%22http%3A%2F%2Fwww.opengis.net%2Fkml%2F2.2%22%3E%0A%20%20%3CPlacemark%3E%0A%20%20%20%20%3Cname%3EPolygon%3C%2Fname%3E%0A%20%20%20%20%3CPolygon%3E%0A%20%20%20%20%20%20%3CouterBoundaryIs%3E%0A%20%20%20%20%20%20%20%20%3CLinearRing%3E%0A%20%20%20%20%20%20%20%20%20%20%3Ccoordinates%3E%0A%20%20%20%20%20%20%20%20%20%20%20%20-84.69104794134181%2C55.71976583858575%2C0%0A%20%20%20%20%20%20%20%20%20%20%20%20-50.21926684954955%2C54.914116182535565%2C0%0A%20%20%20%20%20%20%20%20%20%20%20%20-59.0296334789341%2C10.029277658732568%2C0%0A%20%20%20%20%20%20%20%20%20%20%20%20-79.10835968601603%2C10.498477077010023%2C0%0A%20%20%20%20%20%20%20%20%20%20%20%20-84.69104794134181%2C55.71976583858575%2C0%0A%20%20%20%20%20%20%20%20%20%20%3C%2Fcoordinates%3E%0A%20%20%20%20%20%20%20%20%3C%2FLinearRing%3E%0A%20%20%20%20%20%20%3C%2FouterBoundaryIs%3E%0A%20%20%20%20%3C%2FPolygon%3E%0A%20%20%3C%2FPlacemark%3E%0A%3C%2Fkml%3E";
+
+// Define layers using encoded CZML
+const layerKmlEncoded = {
+  type: "simple",
+  data: {
+    type: "kml",
+    url: encodedKml,
+  },
+  polygon: {
+    fillColor: "red",
+  },
+};
+
+// Difine the KML with URL
+const layerKmlUrl = {
+  type: "simple", // Required
+  data: {
+    type: "kml", // Write the data format
+    url: "https://reearth.github.io/visualizer-plugin-sample-data/public/czml/square.kml", // URL of KML file
+  },
+};
+
+// Add the encoded CZML layer to Re:Earth
+reearth.layers.add(layerKmlEncoded);
+
+// Add the CZML layer from the URL to Re:Earth
+reearth.layers.add(layerKmlUrl);`
 };
 
 export const addKml: PluginType = {
