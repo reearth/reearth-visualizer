8fcdbff953e49f1fb68b7b96e141db384d496ea0
Shogo Hirasawa <origami.no.1@gmail.com>
2025-01-20 16:05:33 +0900

    feat(web): add manage layer example (#1362)

--- web/src/beta/features/PluginPlayground/Plugins/presets/index.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/index.ts
@@ -6,6 +6,8 @@ import { addCzml } from "./layers/add-czml";
 import { addKml } from "./layers/add-kml";
 import { addCsv } from "./layers/add-csv";
 import { add3dTiles } from "./layers/add-3Dtiles";
+import { addOsm3dTiles } from "./layers/add-OSM-3DTiles";
+import { addWms } from "./layers/add-wms";
 import { header } from "./ui/header";
 import { responsivePanel } from "./ui/responsivePanel";
 import { sidebar } from "./ui/sidebar";
@@ -43,7 +45,15 @@ export const presetPlugins: PresetPlugins = [
   {
     id: "layers",
     title: "Manage Layer",
-    plugins: [addGeojson,addCzml,addKml,addCsv,add3dTiles]
+    plugins: [
+      addGeojson,
+      addCzml,
+      addKml,
+      addCsv,
+      add3dTiles,
+      addOsm3dTiles,
+      addWms
+    ]
   },
   {
     id: "layerStyles",
