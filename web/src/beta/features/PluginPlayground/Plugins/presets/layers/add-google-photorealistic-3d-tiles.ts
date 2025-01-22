1536085cc8959d5b0e653b00d5a01f0eb7b7b88e
Shogo Hirasawa <origami.no.1@gmail.com>
2025-01-22 14:18:17 +0900

    feat: add google photorealistic 3d tiles example code

--- web/src/beta/features/PluginPlayground/Plugins/presets/layers/add-google-photorealistic-3d-tiles.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/layers/add-google-photorealistic-3d-tiles.ts
@@ -19,8 +19,38 @@ extensions:
 const widgetFile: FileType = {
   id: "layers-add-googlePhotorealistic3dTiles",
   title: "layers-add-googlePhotorealistic3dTiles.js",
-  sourceCode: `
-`
+  sourceCode: `// Example of adding a layer with Google Photorealistic 3D Tiles
+
+// Define Google Photorealistic 3D Tiles
+const layerPhotorealistic3dTiles = {
+  type: "simple", // Required
+  data: {
+    type: "google-photorealistic", // Data type
+    serviceTokens: {
+      googleMapApiKey: "put_your_api_key",
+    },
+  },
+};
+
+// Add the Google Photorealistic 3D Tiles layer to Re:Earth
+reearth.layers.add(layerPhotorealistic3dTiles);
+
+// Move the camera to the position where the Google Photorealistic 3D Tiles data is displayed.
+reearth.camera.flyTo(
+  // Define the camera position to be moved to
+  {
+    heading: 0.20219047310022553,
+    height: 261.79910347824375,
+    lat: 44.13880442335244,
+    lng: 4.8038131598778,
+    pitch: -0.5139201681525183,
+    roll: 0.000011404713798235377,
+  },
+  // Define camera movement time
+  {
+    duration: 2.0,
+  }
+);`
 };
 
 export const addGooglePhotorealistic3dTiles: PluginType = {
