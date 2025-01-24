5cace360db6210a7145e420865040245a06d537e
Shogo Hirasawa <origami.no.1@gmail.com>
2025-01-24 11:23:55 +0900

    feat: add example of show selected features info

---
+++ web/src/beta/features/PluginPlayground/Plugins/presets/layers/showSelectedFeaturesInformation.ts
@@ -0,0 +1,36 @@
+import { FileType, PluginType } from "../../constants";
+import { PRESET_PLUGIN_COMMON_STYLE } from "../common";
+
+const yamlFile: FileType = {
+  id: "layers-show-features-info-reearth-yml",
+  title: "reearth.yml",
+  sourceCode: `id: show-features-info-plugin
+name: Show Selected Features Information
+version: 1.0.0
+extensions:
+  - id: show-features-info
+    type: widget
+    name: Show Selected Features Information Widget
+    description: Selected Feature Information Widget
+    widgetLayout:
+      defaultLocation:
+        zone: outer
+        section: center
+        area: top
+  `,
+  disableEdit: true,
+  disableDelete: true
+};
+
+const widgetFile: FileType = {
+  id: "show-features-info",
+  title: "show-features-info.js",
+  sourceCode: `TBD
+`
+};
+
+export const showFeaturesInfo: PluginType = {
+  id: "show-features-info",
+  title: "Show Selected Features Information",
+  files: [widgetFile, yamlFile]
+};
