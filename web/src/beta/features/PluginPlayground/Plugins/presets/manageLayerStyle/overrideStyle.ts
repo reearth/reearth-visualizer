547174c0f04cd864d11bf376ace4cf277e4fc4a9
Shogo Hirasawa <origami.no.1@gmail.com>
2025-02-03 21:31:37 +0900

    feat:add a sample script

--- web/src/beta/features/PluginPlayground/Plugins/presets/manageLayerStyle/overrideStyle.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/manageLayerStyle/overrideStyle.ts
@@ -1,4 +1,5 @@
 import { FileType, PluginType } from "../../constants";
+import { PRESET_PLUGIN_COMMON_STYLE } from "../common";
 
 const yamlFile: FileType = {
   id: "override-style-reearth-yml",
@@ -19,110 +20,160 @@ extensions:
 const widgetFile: FileType = {
   id: "override-style",
   title: "override-style.js",
-  sourceCode: `// This example shows how to style 3D model（gltf) //
+  sourceCode: `// This example demonstrates how to override feature style
 
-// Define 3D model data
-const model3D01 = {
-  type: "simple",
+// Define Plug-in UI //
+reearth.ui.show(\`
+${PRESET_PLUGIN_COMMON_STYLE}
+  <style>
+    .scaleBtn {
+      padding: 8px;
+      border-radius: 4px;
+      border: none;
+      background: #fffafa;
+      color: #000000;
+      cursor: pointer;
+      width: 200px;
+      height: 40px;
+      font-size: 16px 
+    }
+    .scaleBtn:active {
+      background: #dcdcdc;
+    }
+
+    .button-container {
+    display: flex;        
+    gap: 8px;           
+    }
+
+    p {
+      text-align: center; 
+    }
+
+  </style>
+  <div id="wrapper">
+    <h2>Color by Height</h2>
+    <p>Choose your preferred color scheme<p>
+    <div class="button-container">
+      <button class = "scaleBtn"id="cool">Cool Style</button>
+      <button class = "scaleBtn" id="warm">Warm Style</button>
+    </div>
+  </div>
+  
+  <script>
+  const coolStyle = document.getElementById("cool");
+  const warmStyle  = document.getElementById("warm");
+
+  
+  coolStyle.addEventListener("click",() =>{
+    parent.postMessage({
+      action: "updateStylyCool",
+    }, "*");
+    })
+  
+  warmStyle.addEventListener("click",() =>{
+    parent.postMessage({
+      action: "updateStylyWarm",
+    }, "*");
+    })
+  </script>
+  \`);
+
+// Define a 3D Tiles layer
+const sample3dTiles = {
+  type: "simple", // Required
   data: {
-    type: "geojson",
-    value: {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [-85.38657958835984, 33.696966258616634],
-      },
-    },
+    type: "3dtiles", // Data type
+    url: "https://assets.cms.plateau.reearth.io/assets/8b/cce097-2d4a-46eb-a98b-a78e7178dc30/13103_minato-ku_pref_2023_citygml_1_op_bldg_3dtiles_13103_minato-ku_lod2_no_texture/tileset.json", // URL of the 3D Tiles
   },
-  model: {
-    show: true,
-    url: "https://reearth.github.io/visualizer-plugin-sample-data/public/gltf/animatedFox.gltf",
-    heightReference: "clamp",
-    minimumPixelSize: 100, // Sets the minimum visible size of the model
-    maximumPixelSize: 1000, // Sets the maximum visible size of the model
-    animation: true, // Enables animation of the 3D model if timeline is played
-    shadows: "enabled", // There are four options: "disabled", "enabled", "cast_only", "receive_only"
-    color: "#fffafa", // Defines a color for the model
+  "3dtiles": {
+    // Styling settings for the 3D Tiles
+    color: "#f8f8ff", // Initial 3D Tiles color
+    pbr: false, // Enable or disable Physically Based Rendering
   },
 };
 
-const model3D02 = {
-  type: "simple",
-  data: {
-    type: "geojson",
-    value: {
-      type: "Feature",
-      geometry: {
-        type: "Point",
-        coordinates: [-85.38657958835984, 33.696266258616634],
-      },
-    },
-  },
-  model: {
-    show: true, // Determines whether the 3D model is visible (default: true)
-    url: "https://reearth.github.io/visualizer-plugin-sample-data/public/gltf/animatedFox.gltf",
-    heightReference: "clamp", // Options: "none" | "clamp" | "relative"
-    // scale: 10000, // Sets a fixed size. Zooming doesn't affect the model's size if used.
-    minimumPixelSize: 100, // Sets the minimum visible size of the model
-    maximumPixelSize: 1000, // Sets the maximum visible size of the model
-    shadows: "enabled", // There are four options: "disabled", "enabled", "cast_only", "receive_only"
-    pbr: false, // Enables or disables Physically Based Rendering
-  },
-};
+// Add the 3D Tiles layer to Re:Earth
+const layerId = reearth.layers.add(sample3dTiles);
 
-// Add 3D models to the layer
-reearth.layers.add(model3D01);
-reearth.layers.add(model3D02);
-
-// In this example, the time width is set to set the time for the shadow to appear
-// Set the time range on the timeline
-reearth.timeline.setTime({
-  // Start time
-  start: new Date("2023-12-01T09:00:00-06:00"),
-  // End time
-  stop: new Date("2023-12-01T15:00:00-06:00"),
-  // Time to be set as the current timeline position
-  current: new Date("2023-12-01T10:00:00-06:00"),
-});
-
-// To animate the 3D model, you need to play the timeline
-reearth.timeline.play();
-
-// Set the playback speed of the timeline (1 = normal speed)
-reearth.timeline.setSpeed(1);
-
-// Enable shadow settings in the Re:Earth viewer
 reearth.viewer.overrideProperty({
-  scene: {
-    shadow: {
-      enabled: true,
-    },
+  // Enable Cesium World Terrain
+  terrain: {
+    enabled: true,
+  },
+  // Prevent buildings from floating above the terrain
+  globe: {
+    depthTestAgainstTerrain: true,
   },
 });
 
-// Move the camera to a specified position
+// Move the camera to the specified position
 reearth.camera.flyTo(
   {
-    // Defines the target camera position
-    heading: 5.358457498291187,
-    height: 274.0013699012735,
-    lat: 33.69505063085045,
-    lng: -85.38408270921741,
-    pitch: -0.7236883292676297,
-    roll: 0.00000479625186056154,
+    // Define the camera's target position
+    heading: 4.427049010960799,
+    height: 1182.6187185313975,
+    lat: 35.66552094432892,
+    lng: 139.77065176741144,
+    pitch: -0.4754550303679297,
+    roll: 0.00009119480271468916,
   },
   {
-    // Duration of the camera movement in seconds
+    // Define the duration of the camera movement (in seconds)
     duration: 2.0,
   }
 );
 
-// 3D Model Data: @AsoboStudio and @scurest (Licensed under CC BY 4.0)
+reearth.extension.on("message", (msg) => {
+  if (msg.action === "updateStylyCool") {
+    reearth.layers.override(layerId, {
+      "3dtiles": {
+        show: true,
+        pbr: false,
+        color: {
+          expression: {
+            conditions: [
+              ["\${_zmax} > 200", "color('#005f8d')"],
+              ["\${_zmax} > 150", "color('#008cdd')"],
+              ["\${_zmax} > 100", "color('#33baff')"],
+              ["\${_zmax} > 50", "color('#7cd8ff')"],
+              ["\${_zmax} > 0", "color('#d5f2ff')"],
+              ["true", "color('#003f66')"],
+            ],
+          },
+        },
+      },
+    });
+  }
+});
+
+reearth.extension.on("message", (msg) => {
+  if (msg.action === "updateStylyWarm") {
+    reearth.layers.override(layerId, {
+      "3dtiles": {
+        show: true,
+        pbr: false,
+        color: {
+          expression: {
+            conditions: [
+              ["\${_zmax} > 200", "color('#f5220f')"], 
+              ["\${_zmax} > 150", "color('#ff5e47')"], 
+              ["\${_zmax} > 100", "color('#ff9586')"],
+              ["\${_zmax} > 50", "color('#ffc1b8')"],
+              ["\${_zmax} > 0", "color('#ffe5e0')"], 
+              ["true", "color('#a1140a')"],
+            ],
+          },
+        },
+      },
+    });
+  }
+});
 `
 };
 
 export const overrideStyle: PluginType = {
   id: "override-style",
-  title: "Override style",
+  title: "Override Style",
   files: [widgetFile, yamlFile]
 };
