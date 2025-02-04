5aed76b5d3c4bffceca14f1233c56aaeda93c1a7
lby <icesunex@hotmail.com>
2025-02-04 18:36:43 +0800

    refactor(web): format code (#1405)

--- web/src/beta/features/Editor/hooks/useLayers.ts
+++ web/src/beta/features/Editor/hooks/useLayers.ts
@@ -86,23 +86,22 @@ export default function ({
 
   const [sortedLayerIds, setSortedLayerIds] = useState<string[]>([]);
 
-useEffect(() => {
-  if (!originNlsLayers) return;
+  useEffect(() => {
+    if (!originNlsLayers) return;
 
-  setSortedLayerIds((prev) => {
-    const originIds = originNlsLayers.map((l) => l.id);
-    if (
-      prev.length === originIds.length &&
-      prev.every((id, idx) => id === originIds[idx])
-    ) {
-      return prev;
-    }
-    return [...originNlsLayers]
-      .sort((a, b) => (a.index ?? 0) - (b.index ?? 0))
-      .map((l) => l.id);
-  });
-}, [originNlsLayers]);
-
+    setSortedLayerIds((prev) => {
+      const originIds = originNlsLayers.map((l) => l.id);
+      if (
+        prev.length === originIds.length &&
+        prev.every((id, idx) => id === originIds[idx])
+      ) {
+        return prev;
+      }
+      return [...originNlsLayers]
+        .sort((a, b) => (a.index ?? 0) - (b.index ?? 0))
+        .map((l) => l.id);
+    });
+  }, [originNlsLayers]);
 
   const nlsLayers: NLSLayer[] = useMemo(
     () =>
