ec85dde0d5b5faadc2eaafeb886be7fe84e1cce8
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-05 17:56:58 +0900

    Delete dataset from baseSeeder

--- server/e2e/seeder.go
+++ server/e2e/seeder.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/reearth/reearth/server/internal/usecase/repo"
-	"github.com/reearth/reearth/server/pkg/dataset"
 	"github.com/reearth/reearth/server/pkg/id"
 	"github.com/reearth/reearth/server/pkg/project"
 	"github.com/reearth/reearthx/account/accountdomain"
@@ -26,13 +25,7 @@ var (
 	pName  = "p1"
 	pAlias = "PROJECT_ALIAS"
 
-	sID    = id.NewSceneID()
-	dssID  = id.NewDatasetSchemaID()
-	dsID   = id.NewDatasetID()
-	dsfID1 = dataset.NewFieldID()
-	dsfID2 = dataset.NewFieldID()
-	dsfID3 = dataset.NewFieldID()
-	dsfID4 = dataset.NewFieldID()
+	sID = id.NewSceneID()
 
 	now = time.Date(2022, time.January, 1, 0, 0, 0, 0, time.UTC)
 )
@@ -90,33 +83,6 @@ func baseSetup(ctx context.Context, r *repo.Container, u *user.User) error {
 		Alias(pAlias).
 		MustBuild()
 	if err := r.Project.Save(ctx, p); err != nil {
-		return err
-	}
-
-	dss := dataset.NewSchema().ID(dssID).
-		Name("test.csv").
-		Scene(sID).
-		Fields([]*dataset.SchemaField{
-			dataset.NewSchemaField().ID(dsfID1).Name("f1").Type(dataset.ValueTypeString).MustBuild(),
-			dataset.NewSchemaField().ID(dsfID2).Name("f2").Type(dataset.ValueTypeNumber).MustBuild(),
-			dataset.NewSchemaField().ID(dsfID3).Name("f3").Type(dataset.ValueTypeBool).MustBuild(),
-			dataset.NewSchemaField().ID(dsfID4).Name("location").Type(dataset.ValueTypeLatLng).MustBuild(),
-		}).
-		Source("file:///dss.csv").
-		MustBuild()
-	if err := r.DatasetSchema.Save(ctx, dss); err != nil {
-		return err
-	}
-
-	ds := dataset.New().ID(dsID).Schema(dss.ID()).Scene(sID).
-		Fields([]*dataset.Field{
-			dataset.NewField(dsfID1, dataset.ValueTypeString.ValueFrom("test"), ""),
-			dataset.NewField(dsfID2, dataset.ValueTypeNumber.ValueFrom(123), ""),
-			dataset.NewField(dsfID3, dataset.ValueTypeBool.ValueFrom(true), ""),
-			dataset.NewField(dsfID4, dataset.ValueTypeLatLng.ValueFrom(dataset.LatLng{Lat: 11, Lng: 12}), ""),
-		}).
-		MustBuild()
-	if err := r.Dataset.Save(ctx, ds); err != nil {
 		return err
 	}
 
