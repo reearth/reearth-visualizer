3a06d75a20e947e96b41b44a92c48639d17119ad
Shogo Hirasawa <origami.no.1@gmail.com>
2025-02-18 18:14:41 +0900

    feat:enable terrain

--- web/src/beta/features/PluginPlayground/Plugins/presets/viewerAndSceneSettings/enableTerrain.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/viewerAndSceneSettings/enableTerrain.ts
@@ -19,8 +19,39 @@ extensions:
 const widgetFile: FileType = {
   id: "enable-terrain",
   title: "enable-terrain.js",
-  sourceCode: `TBD
-`
+  sourceCode: `
+reearth.camera.flyTo(
+  // Define the camera position to be moved to
+  {
+    heading: 0.9658416610554319,
+    height: 5632.307221882181,
+    lat: 35.30495385208046,
+    lng: 138.62843439939437,
+    pitch: -0.3232851887743784,
+    roll: 6.283155211555897,
+  },
+  // Define camera movement time
+  {
+    duration: 2.0,
+  }
+);
+
+reearth.viewer.overrideProperty({
+  // Enable Cesium World Terrain
+  terrain: {
+    enabled: true,
+  },
+  // Enable the function for buildings not to lift off the ground
+  globe: {
+    depthTestAgainstTerrain: true,
+  },
+  // Enable shadows
+  scene: {
+    shadow: {
+      enabled: true,
+    },
+  },
+});`
 };
 
 export const enableTerrain: PluginType = {
