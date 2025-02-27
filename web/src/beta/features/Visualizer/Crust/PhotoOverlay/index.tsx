91a141c3fb663661fa54fc8186f7f7e8304ca1cf
airslice <icesunex@hotmail.com>
2025-02-27 11:02:23 +0800

    refactor: use widget ui & add close button

--- web/src/beta/features/Visualizer/Crust/PhotoOverlay/index.tsx
+++ web/src/beta/features/Visualizer/Crust/PhotoOverlay/index.tsx
@@ -1,13 +1,16 @@
+import { Icon } from "@reearth/beta/lib/reearth-ui";
+import { Button } from "@reearth/beta/lib/reearth-widget-ui/components/ui/button";
 import {
   getPhotoOverlayValue,
   PhotoOverlayPreview
 } from "@reearth/beta/utils/sketch";
 import { Camera, ComputedFeature, ComputedLayer, MapRef } from "@reearth/core";
 import { NLSLayer } from "@reearth/services/api/layersApi/utils";
-import { styled } from "@reearth/services/theme";
 import {
   FC,
+  ReactNode,
   RefObject,
+  SyntheticEvent,
   useCallback,
   useEffect,
   useMemo,
@@ -117,12 +120,18 @@ const PhotoOverlay: FC<PhotoOverlayProps> = ({
             fixed={value.fill === "fixed"}
             sizeBase={sizeBase}
             onLoad={handlePhotoLoad}
-            draggable={false}
           />
         </PhotoInnerWrapper>
       </PhotoWrapper>
       {value.description && (
         <Description visible={show && ready}>{value.description}</Description>
+      )}
+      {!preview && (
+        <div className={`tw-absolute tw-right-1 tw-top-1`}>
+          <Button size="icon" onClick={handleExit}>
+            <Icon icon="close" />
+          </Button>
+        </div>
       )}
     </Wrapper>
   ) : null;
@@ -130,73 +139,86 @@ const PhotoOverlay: FC<PhotoOverlayProps> = ({
 
 export default PhotoOverlay;
 
-const Wrapper = styled("div")<{ interactive: boolean }>(
-  ({ theme, interactive }) => ({
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    left: 0,
-    top: 0,
-    zIndex: theme.zIndexes.visualizer.photoOverlay,
-    pointerEvents: interactive ? "all" : "none"
-  })
+const Wrapper: FC<{
+  interactive: boolean;
+  children: ReactNode;
+}> = ({ interactive, children }) => (
+  <div
+    className={`tw-absolute tw-w-full tw-h-full tw-left-0 tw-top-0 tw-z-photoOverlay ${interactive ? "tw-pointer-events-auto" : "tw-pointer-events-none"}`}
+  >
+    {children}
+  </div>
 );
 
-const PhotoWrapper = styled("div")<{ visible: boolean }>(({ visible }) => ({
-  position: "absolute",
-  width: "100%",
-  height: "100%",
-  left: 0,
-  top: 0,
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  opacity: visible ? 1 : 0,
-  transition: "opacity 0.25s"
-}));
+const PhotoWrapper: FC<{
+  ref: RefObject<HTMLDivElement>;
+  visible: boolean;
+  onClick: () => void;
+  children: ReactNode;
+}> = ({ ref, visible, onClick, children }) => (
+  <div
+    ref={ref}
+    onClick={onClick}
+    className={`tw-absolute tw-w-full tw-h-full tw-left-0 tw-top-0 tw-flex tw-justify-center tw-items-center tw-transition-opacity tw-duration-200 ${visible ? "tw-opacity-100" : "tw-opacity-0"}`}
+  >
+    {children}
+  </div>
+);
 
-const PhotoInnerWrapper = styled("div")<{
+const PhotoInnerWrapper: FC<{
   widthPct?: number;
   heightPct?: number;
   sizeBase: "width" | "height";
   fixed: boolean;
-}>(({ widthPct, heightPct, sizeBase, fixed }) => ({
-  position: "absolute",
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  ...(fixed
-    ? sizeBase === "width"
-      ? { height: "100%", width: `${widthPct}%` }
-      : { width: "100%", height: `${heightPct}%` }
-    : { width: "100%", height: "100%" })
-}));
+  children: ReactNode;
+}> = ({ widthPct, heightPct, sizeBase, fixed, children }) => {
+  const sizeStyles: { width: string; height: string } = useMemo(() => {
+    return fixed
+      ? sizeBase === "width"
+        ? { height: "100%", width: `${widthPct}%` }
+        : { width: "100%", height: `${heightPct}%` }
+      : { width: "100%", height: "100%" };
+  }, [fixed, sizeBase, widthPct, heightPct]);
 
-const Photo = styled("img")<{
+  return (
+    <div
+      className={`tw-absolute tw-flex tw-justify-center tw-items-center ${sizeStyles}`}
+      style={sizeStyles}
+    >
+      {children}
+    </div>
+  );
+};
+
+const Photo: FC<{
+  src?: string;
   opacity?: number;
   fixed: boolean;
   sizeBase: "width" | "height";
-}>(({ opacity, fixed, sizeBase }) => ({
-  opacity: opacity ?? 1,
-  ...(fixed
-    ? sizeBase === "width"
-      ? { width: "100%", height: "auto", maxHeight: "none" }
-      : { height: "100%", width: "auto", maxWidth: "none" }
-    : { maxWidth: "90%", maxHeight: "90%" })
-}));
+  onLoad: (e: SyntheticEvent<HTMLImageElement, Event>) => void;
+}> = ({ src, opacity, fixed, sizeBase, onLoad }) => (
+  <img
+    src={src}
+    style={{ opacity: opacity ?? 1 }}
+    className={`${
+      fixed
+        ? sizeBase === "width"
+          ? "tw-w-full tw-h-auto tw-max-h-none"
+          : "tw-h-full tw-w-auto tw-max-w-none"
+        : "tw-max-w-[90%] tw-max-h-[90%]"
+    }`}
+    onLoad={onLoad}
+    draggable={false}
+  />
+);
 
-const Description = styled("div")<{ visible: boolean }>(
-  ({ theme, visible }) => ({
-    position: "absolute",
-    bottom: 0,
-    left: 0,
-    width: "100%",
-    padding: theme.spacing.large,
-    textAlign: "center",
-    background: "#00000099",
-    color: theme.content.main,
-    pointerEvents: "all",
-    opacity: visible ? 1 : 0,
-    transition: "opacity 0.25s"
-  })
+const Description: FC<{
+  visible: boolean;
+  children: ReactNode;
+}> = ({ visible, children }) => (
+  <div
+    className={`tw-absolute tw-bottom-0 tw-left-0 tw-w-full tw-p-2 tw-text-center tw-bg-[#00000099] tw-text-sm tw-text-white tw-pointer-events-auto tw-transition-opacity tw-duration-200 ${visible ? "tw-opacity-100" : "tw-opacity-0"} tw-whitespace-pre-wrap`}
+  >
+    {children}
+  </div>
 );
