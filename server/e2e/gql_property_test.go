f9d7eb3491d640f94d1ce9563d0d1c438b7eb2c1
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-02-28 13:33:59 +0900

    test

--- server/e2e/gql_property_test.go
+++ server/e2e/gql_property_test.go
@@ -7,36 +7,52 @@ import (
 func updatePropertyValue(e *httpexpect.Expect, propertyID, schemaGroupID, itemID, fieldID string, value any, valueType any) (GraphQLRequest, *httpexpect.Value) {
 	requestBody := GraphQLRequest{
 		OperationName: "UpdatePropertyValue",
-		Query: `mutation UpdatePropertyValue($propertyId: ID!, $schemaGroupId: ID, $itemId: ID, $fieldId: ID!, $value: Any, $type: ValueType!) {
-			updatePropertyValue( input: { propertyId: $propertyId, schemaGroupId: $schemaGroupId, itemId: $itemId, fieldId: $fieldId, value: $value, type: $type } ) {
-				property {
-				id
-				schema{
-					id
-					groups{
-						fields{
-							fieldId
-							type
-							title
-							description
-							prefix
-							suffix
-							defaultValue
-							ui
-							min
-							max
-							placeholder
-						}
-					}
-				}
-				}
-				propertyField {	
-				id
-				type
-				value
-				}
-			}
-		}`,
+		Query: `mutation UpdatePropertyValue(
+  $propertyId: ID!
+  $schemaGroupId: ID
+  $itemId: ID
+  $fieldId: ID!
+  $value: Any
+  $type: ValueType!
+) {
+  updatePropertyValue(
+    input: {
+      propertyId: $propertyId
+      schemaGroupId: $schemaGroupId
+      itemId: $itemId
+      fieldId: $fieldId
+      value: $value
+      type: $type
+    }
+  ) {
+    property {
+      id
+      schema {
+        id
+        groups {
+          fields {
+            fieldId
+            type
+            title
+            description
+            prefix
+            suffix
+            defaultValue
+            ui
+            min
+            max
+            placeholder
+          }
+        }
+      }
+    }
+    propertyField {
+      id
+      type
+      value
+    }
+  }
+}`,
 		Variables: map[string]any{
 			"propertyId":    propertyID,
 			"schemaGroupId": schemaGroupID,
@@ -46,8 +62,20 @@ func updatePropertyValue(e *httpexpect.Expect, propertyID, schemaGroupID, itemID
 			"type":          valueType,
 		},
 	}
-
 	res := Request(e, uID.String(), requestBody)
-
 	return requestBody, res
 }
+
+// {
+//   "propertyId": "01jn55107e2rqww07kjd2p9f6v",
+//   "schemaGroupId": "default",
+//   "fieldId": "padding",
+//   "value": {
+//     "top": 22,
+//     "bottom": 21,
+//     "left": 22,
+//     "right": 21
+//   },
+//   "type": "SPACING",
+//   "lang": "en"
+// }
