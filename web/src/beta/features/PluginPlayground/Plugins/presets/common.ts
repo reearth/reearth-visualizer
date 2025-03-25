562b38d1b714ffb8694bbee942ef3363d6f9a747
mulengawilfred <m.wilfred@eukarya.io>
2025-03-26 02:57:32 +0900

    fix: add styles to terrain and shadow plugins

--- web/src/beta/features/PluginPlayground/Plugins/presets/common.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/common.ts
@@ -277,6 +277,10 @@ li {
   font-size: 16px;
 }
 
+.text-xl {
+  font-size: 18px;
+}
+
 .text-3xl {
   font-size: 24px;
 }
@@ -334,5 +338,49 @@ li {
   grid-template-columns: auto 80px 1fr;
   align-items: baseline;
 }
+
+/* Slider */
+ .toggle {
+   position: relative;
+   display: inline-block;
+   width: 60px;
+   height: 34px;
+   }
+   .toggle input {
+   opacity: 0;
+   width: 0;
+   height: 0;
+   }
+   .slider {
+   position: absolute;
+   cursor: pointer;
+   top: 0;
+   left: 0;
+   right: 0;
+   bottom: 0;
+   background-color: #ccc;
+   transition: .4s;
+   border-radius: 34px;
+   }
+   .slider:before {
+   position: absolute;
+   content: "";
+   height: 26px;
+   width: 26px;
+   left: 4px;
+   bottom: 4px;
+   background-color: white;
+   transition: .4s;
+   border-radius: 50%;
+   }
+   input:checked + .slider {
+   background-color: #2196F3;
+   }
+   input:focus + .slider {
+   box-shadow: 0 0 1px #2196F3;
+   }
+   input:checked + .slider:before {
+   transform: translateX(26px);
+   }
   </style>
 `;
