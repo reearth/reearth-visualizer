92d9b8ebf1e9517c4e395388bf1aaaa8ca6ca14c
Beatrice Mkumbo <charsbeaty@gmail.com>
2025-03-26 11:36:25 +0300

    refactor(web): support  tooltip on icon button [VIZ-1401] (#1514)

--- web/src/services/theme/reearthTheme/common/shadow.ts
+++ web/src/services/theme/reearthTheme/common/shadow.ts
@@ -2,7 +2,8 @@ const shadow = {
   input: "0px 2px 2px 0px rgba(0, 0, 0, 0.25) inset",
   button: "0px 2px 2px 0px rgba(0, 0, 0, 0.25)",
   card: "2px 2px 2px 0px rgba(0, 0, 0, 0.25)",
-  popup: "4px 4px 4px 0px rgba(0, 0, 0, 0.25)"
+  popup: "4px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+  tooltip: "4px 6px 12px 0px rgba(0, 0, 0, 0.60)"
 } as const;
 
 export type ShadowType = typeof shadow;
