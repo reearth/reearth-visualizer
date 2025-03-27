474a29776d0de4ea083bc34d1c0ba61e0db205c2
OpeDada <opedada@gmail.com>
2025-03-27 17:40:15 +0900

    refactor: reduce toggle button sizes

--- web/src/beta/features/PluginPlayground/Plugins/presets/common.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/common.ts
@@ -341,48 +341,48 @@ li {
 }
 
 /* Slider */
- .toggle {
-   position: relative;
-   display: inline-block;
-   width: 60px;
-   height: 34px;
-   }
-   .toggle input {
-   opacity: 0;
-   width: 0;
-   height: 0;
-   }
-   .slider {
-   position: absolute;
-   cursor: pointer;
-   top: 0;
-   left: 0;
-   right: 0;
-   bottom: 0;
-   background-color: #ccc;
-   transition: .4s;
-   border-radius: 34px;
-   }
-   .slider:before {
-   position: absolute;
-   content: "";
-   height: 26px;
-   width: 26px;
-   left: 4px;
-   bottom: 4px;
-   background-color: white;
-   transition: .4s;
-   border-radius: 50%;
-   }
-   input:checked + .slider {
-   background-color: #2196F3;
-   }
-   input:focus + .slider {
-   box-shadow: 0 0 1px #2196F3;
-   }
-   input:checked + .slider:before {
-   transform: translateX(26px);
-   }
+.toggle {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 24px;
+  }
+  .toggle input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+  }
+  .slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: -8px;
+  bottom: 0;
+  background-color: #ccc;
+  transition: .4s;
+  border-radius: 34px;
+  }
+  .slider:before {
+  position: absolute;
+  content: "";
+  height: 16px;
+  width: 16px;
+  left: 4px;
+  bottom: 4px;
+  background-color: white;
+  transition: .4s;
+  border-radius: 50%;
+  }
+  input:checked + .slider {
+  background-color: #2196F3;
+  }
+  input:focus + .slider {
+  box-shadow: 0 0 1px #2196F3;
+  }
+  input:checked + .slider:before {
+  transform: translateX(26px);
+  }
 
   .divider {
     border-top: 1px solid rgba(0, 0, 0, 0.1);
