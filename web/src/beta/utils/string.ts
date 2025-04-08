4cf7210cbc77bc5b8896a2b6b534e07d586413d0
mulengawilfred <m.wilfred@eukarya.io>
2025-04-08 13:44:14 +0900

    fix: add unit tests to util functions

--- web/src/beta/utils/string.ts
+++ web/src/beta/utils/string.ts
@@ -4,11 +4,9 @@ export const isEmptyString = function (text: string): boolean {
 
 const usableChars =
   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-const generateRandomString = (len: number): string => {
+export const generateRandomString = (len: number): string => {
   return Array.from(window.crypto.getRandomValues(new Uint8Array(len)))
     .map((n) => usableChars[n % len])
     .join("")
     .toLowerCase();
 };
-
-export default generateRandomString;
