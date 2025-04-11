9d93f080a5e5a176a454f87a7d32889f2c47b148
mkumbobeaty <charsbeaty@gmail.com>
2025-04-11 08:57:29 +0300

    check validtion

--- web/src/beta/features/Editor/Publish/PublishToolsPanel/PublishOrUpdateModal/useAlias.ts
+++ web/src/beta/features/Editor/Publish/PublishToolsPanel/PublishOrUpdateModal/useAlias.ts
@@ -1,6 +1,5 @@
-import generateRandomString from "@reearth/beta/utils/string";
 import { useProjectFetcher } from "@reearth/services/api";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { PublishItem } from "../hooks";
 
@@ -12,59 +11,37 @@ export default ({ publishItem }: { publishItem: PublishItem }) => {
     { loading: validatingAlias, data: checkProjectAliasData }
   ] = useProjectAliasCheckLazyQuery();
 
+
+
   const [aliasValid, setAliasValid] = useState(false);
   const [alias, setAlias] = useState<string | undefined>(publishItem.alias);
   const currentValidatingAlias = useRef<string>();
 
-  const createAliasCount = useRef(0);
-  const createAlias = useCallback(() => {
-    createAliasCount.current += 1;
-    // Max try 3 times
-    if (createAliasCount.current > 3) {
-      console.error("Failed to generate unique alias after 3 attempts");
-      return;
-    }
-    const newAlias = generateAlias();
-    currentValidatingAlias.current = newAlias;
+  useEffect(() => {
+    const newAlias = publishItem.alias ?? publishItem.id;
     setAlias(newAlias);
-    checkProjectAlias({ variables: { alias: newAlias } });
-  }, [checkProjectAlias]);
+    currentValidatingAlias.current = newAlias;
 
-  useEffect(() => {
-    if (!publishItem.alias) {
-      createAlias();
-    }
-  }, [publishItem.alias, createAlias]);
+    checkProjectAlias({
+      variables: { alias: newAlias }
+    });
+  }, [publishItem.alias, publishItem.id, checkProjectAlias]);
 
-  useEffect(() => {
-    if (
-      checkProjectAliasData &&
-      !checkProjectAliasData.checkProjectAlias.available
-    ) {
-      createAlias();
-    }
-  }, [checkProjectAliasData, createAlias]);
+ useEffect(() => {
+   setAliasValid(
+     !!(
+       publishItem.alias != null ||
+       (!validatingAlias &&
+         checkProjectAliasData?.checkProjectAlias?.available &&
+         currentValidatingAlias.current ===
+           checkProjectAliasData.checkProjectAlias.alias)
+     )
+   );
+ }, [validatingAlias, checkProjectAliasData, publishItem.alias]);
 
-  useEffect(() => {
-    setAliasValid(
-      // Alias is valid if it is provided by data
-      !!publishItem.alias ||
-        // Validating alias is not in progress
-        (!validatingAlias &&
-          // Validating alias is the same as the current alias, and avaliable
-          !!checkProjectAliasData &&
-          currentValidatingAlias.current ===
-            checkProjectAliasData.checkProjectAlias.alias &&
-          checkProjectAliasData.checkProjectAlias.available)
-    );
-  }, [validatingAlias, checkProjectAliasData, publishItem, publishItem.alias]);
 
   return {
     alias,
     aliasValid
   };
 };
-
-function generateAlias() {
-  return generateRandomString(10);
-}
