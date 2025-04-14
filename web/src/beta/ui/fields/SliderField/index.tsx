99d316eb9032742015b2dd61b55979eadf555e9f
mulengawilfred <m.wilfred@eukarya.io>
2025-04-14 16:18:08 +0900

    add unit test for slider component

--- web/src/beta/ui/fields/SliderField/index.tsx
+++ web/src/beta/ui/fields/SliderField/index.tsx
@@ -1,7 +1,7 @@
 import { Slider, SliderProps } from "@reearth/beta/lib/reearth-ui";
 import { FC } from "react";
 
-import CommonField, { CommonFieldProps } from "./CommonField";
+import CommonField, { CommonFieldProps } from "../CommonField";
 
 export type SliderFieldProps = CommonFieldProps & SliderProps;
 const SliderField: FC<SliderFieldProps> = ({
