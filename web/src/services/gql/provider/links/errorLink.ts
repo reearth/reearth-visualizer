314da61abac353725a92a020e8f96972c90b7e1b
mkumbobeaty <charsbeaty@gmail.com>
2025-04-24 00:19:34 +0300

    add skip global error solution

--- web/src/services/gql/provider/links/errorLink.ts
+++ web/src/services/gql/provider/links/errorLink.ts
@@ -6,25 +6,31 @@ import { GQLError } from "@reearth/services/state/gqlErrorHandling";
 export default () => {
   const { setErrors } = useSetError();
 
-  return onError(({ graphQLErrors, networkError }) => {
+  return onError(({ graphQLErrors, networkError, operation }) => {
+
+  // 'x-' prefix marks this as a custom (non-standard) header to prevent naming conflicts with official HTTP headers.
+    const skipGlobalError =
+      operation.getContext()?.headers?.["x-skip-global-error"];
+
     if (!networkError && !graphQLErrors) return;
+    if (skipGlobalError) return;
+
     let errors: GQLError[] = [];
 
     if (networkError?.message) {
       errors = [
-        { message: networkError?.message, description: networkError.message }
+        { message: networkError.message, description: networkError.message }
       ];
     } else {
       errors =
-        graphQLErrors?.map((gqlError) => {
-          return {
-            type: gqlError.path?.[0].toString(),
-            message: gqlError.message,
-            code: gqlError.extensions?.code as string,
-            description: gqlError.extensions?.description as string
-          };
-        }) ?? [];
+        graphQLErrors?.map((gqlError) => ({
+          type: gqlError.path?.[0]?.toString(),
+          message: gqlError.message,
+          code: gqlError.extensions?.code as string,
+          description: gqlError.extensions?.description as string
+        })) ?? [];
     }
+
     if (errors.length > 0) {
       setErrors(errors);
       reportError(errors);
