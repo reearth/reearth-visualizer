1bdd90c0232c99d10a6dd75ac8d0421f8df3a9f2
mulengawilfred <m.wilfred@eukarya.io>
2025-04-25 11:35:11 +0900

    test: use mergeConfig to use config from vite config

--- web/vitest.config.ts
+++ web/vitest.config.ts
@@ -1,12 +1,11 @@
-import yaml from "@rollup/plugin-yaml";
-import { PluginOption } from "vite";
-import svgr from "vite-plugin-svgr";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { defineConfig, mergeConfig, configDefaults } from "vitest/config";
+import { mergeConfig } from "vite";
+import { defineConfig, configDefaults } from "vitest/config";
+
+import viteConfig from "./vite.config";
 
 export default mergeConfig(
-  {
-    plugins: [svgr(), tsconfigPaths(), yaml() as PluginOption],
+  viteConfig,
+  defineConfig({
     test: {
       environment: "jsdom",
       setupFiles: "src/test/setup.ts",
@@ -24,17 +23,7 @@ export default mergeConfig(
           "src/**/*.test.{ts,tsx}"
         ],
         reporter: ["text", "json", "lcov"]
-      },
-      alias: [
-        { find: "crypto", replacement: "crypto" }, // reset setting for quickjs-emscripten
-        { find: "csv-parse", replacement: "csv-parse" },
-        { find: "@reearth/test/utils", replacement: "src/test/utils" },
-        {
-          find: "@reearth/services/theme/reearthTheme/common/spacing",
-          replacement: "src/services/theme/reearthTheme/common/spacing"
-        }
-      ]
+      }
     }
-  },
-  defineConfig({})
+  })
 );
