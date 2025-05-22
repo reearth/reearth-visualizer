68681387e6ef3653932b60325cd795fc45be525a
asahina <83101503+asahina820@users.noreply.github.com>
2025-05-22 10:54:34 +0900

    feat(web): add infobox example in playground (#1608)
    Co-authored-by: arisamikumo <53960439+arisamikumo@users.noreply.github.com>

--- web/src/beta/features/PluginPlayground/Plugins/presets/useTitles.ts
+++ web/src/beta/features/PluginPlayground/Plugins/presets/useTitles.ts
@@ -43,6 +43,11 @@ export default () => {
       "add-google-photorealistic-3d-tiles": t(
         "Add Google Photorealistic 3D Tiles"
       ),
+      "add-infobox-all-properties": t("Add infobox to show all properties"),
+      "add-infobox-specific-properties": t(
+        "Add infobox to show specific properties"
+      ),
+      "add-infobox-rich-block": t("Add infobox to show rich blocks"),
       "add-osm-3d-tiles": t("Add OSM 3D Tiles"),
       "hide-fly-to-delete-layer": t("Hide Fly To Delete Layer"),
       "override-layer-data": t("Override Layer Data"),
