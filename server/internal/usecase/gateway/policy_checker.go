f4425e560f299b41c5514675097520425b89538f
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-07-24 13:27:19 +0900

    test

---
+++ server/internal/usecase/gateway/policy_checker.go
@@ -0,0 +1,38 @@
+package gateway
+
+import (
+	"context"
+
+	"github.com/reearth/reearthx/account/accountdomain"
+)
+
+type PolicyCheckType string
+
+const (
+	PolicyCheckPrivateDataTransferUpload     PolicyCheckType = "cms_private_data_transfer_upload_size"
+	PolicyCheckPrivateDataTransferDownload   PolicyCheckType = "cms_private_data_transfer_download_size"
+	PolicyCheckPublicDataTransferUpload      PolicyCheckType = "cms_public_data_transfer_upload_size"
+	PolicyCheckPublicDataTransferDownload    PolicyCheckType = "cms_public_data_transfer_download_size"
+	PolicyCheckUploadAssetsSize              PolicyCheckType = "cms_upload_assets_size_from_ui"
+	PolicyCheckModelCountPerProject          PolicyCheckType = "cms_model_count_per_project"
+	PolicyCheckGeneralPrivateProjectCreation PolicyCheckType = "general_private_project_creation"
+	PolicyCheckGeneralPublicProjectCreation  PolicyCheckType = "general_public_project_creation"
+)
+
+type PolicyCheckRequest struct {
+	WorkspaceID accountdomain.WorkspaceID
+	CheckType   PolicyCheckType
+	Value       int64
+}
+
+type PolicyCheckResponse struct {
+	Allowed      bool
+	CheckType    PolicyCheckType
+	CurrentLimit string
+	Message      string
+	Value        int64
+}
+
+type PolicyChecker interface {
+	CheckPolicy(ctx context.Context, req PolicyCheckRequest) (*PolicyCheckResponse, error)
+}
