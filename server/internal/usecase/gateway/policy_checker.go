9bc9be17f071700040d10786a603e5cd285fc5b9
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-07-24 14:59:21 +0900

    test

--- server/internal/usecase/gateway/policy_checker.go
+++ server/internal/usecase/gateway/policy_checker.go
@@ -9,12 +9,12 @@ import (
 type PolicyCheckType string
 
 const (
-	PolicyCheckPrivateDataTransferUpload     PolicyCheckType = "cms_private_data_transfer_upload_size"
-	PolicyCheckPrivateDataTransferDownload   PolicyCheckType = "cms_private_data_transfer_download_size"
-	PolicyCheckPublicDataTransferUpload      PolicyCheckType = "cms_public_data_transfer_upload_size"
-	PolicyCheckPublicDataTransferDownload    PolicyCheckType = "cms_public_data_transfer_download_size"
-	PolicyCheckUploadAssetsSize              PolicyCheckType = "cms_upload_assets_size_from_ui"
-	PolicyCheckModelCountPerProject          PolicyCheckType = "cms_model_count_per_project"
+	PolicyCheckPrivateDataTransferUpload     PolicyCheckType = "visualizer_private_data_transfer_upload_size"
+	PolicyCheckPrivateDataTransferDownload   PolicyCheckType = "visualizer_private_data_transfer_download_size"
+	PolicyCheckPublicDataTransferUpload      PolicyCheckType = "visualizer_public_data_transfer_upload_size"
+	PolicyCheckPublicDataTransferDownload    PolicyCheckType = "visualizer_public_data_transfer_download_size"
+	PolicyCheckUploadAssetsSize              PolicyCheckType = "visualizer_upload_assets_size_from_ui"
+	PolicyCheckModelCountPerProject          PolicyCheckType = "visualizer_model_count_per_project"
 	PolicyCheckGeneralPrivateProjectCreation PolicyCheckType = "general_private_project_creation"
 	PolicyCheckGeneralPublicProjectCreation  PolicyCheckType = "general_public_project_creation"
 )
