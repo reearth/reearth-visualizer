4ffa7a17ca4d20dd3c11fe2049fbf5bb619b8f43
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-08-27 19:01:34 +0900

    migration adjustment

--- server/internal/infrastructure/mongo/migration/migrations.go
+++ server/internal/infrastructure/mongo/migration/migrations.go
@@ -35,5 +35,5 @@ var migrations = migration.Migrations[DBClient]{
 	250725112722: MetadataUpdate,
 	250725133631: SetPhotoOverlayDefault,
 	250725145932: ConvertNonValidProjectAliases,
-	250820113009: ChangeEsriToDefault,
+	250827185841: ChangeEsriToDefault,
 }
