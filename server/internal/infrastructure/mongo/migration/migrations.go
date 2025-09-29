248e518d6288ab577635fa16092297d035858115
wilfredmulenga <wilfred.mulenga@gmail.com>
2025-09-29 15:57:45 +0900

    Revert "feat: add migration to add new fields to project collection"
    This reverts commit 09dd8968247272531101b9c54b55b3f71c3a56d8.

--- server/internal/infrastructure/mongo/migration/migrations.go
+++ server/internal/infrastructure/mongo/migration/migrations.go
@@ -36,5 +36,4 @@ var migrations = migration.Migrations[DBClient]{
 	250725133631: SetPhotoOverlayDefault,
 	250725145932: ConvertNonValidProjectAliases,
 	250820113009: ChangeEsriToDefault,
-	250925135245: AddProjectMetadataFields,
 }
