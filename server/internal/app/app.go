cf983e33871aa055c32ec93561b5a6a63b78fa7f
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-10-22 14:48:12 +0900

    test

--- server/internal/app/app.go
+++ server/internal/app/app.go
@@ -256,39 +256,47 @@ func restAPITracingMiddleware() echo.MiddlewareFunc {
 			req := c.Request()
 			ctx := req.Context()
 
-			parentSpan := trace.SpanFromContext(ctx)
-			if !parentSpan.SpanContext().IsValid() {
+			routePath := c.Path()
+			actualPath := req.URL.Path
+			path := routePath
+			if path == "" {
+				path = actualPath
+			}
+			if req.Method == "OPTIONS" || actualPath == "/favicon.ico" {
 				return next(c)
 			}
 
-			path := c.Path()
-			if path == "" {
-				path = req.URL.Path
-			}
-
-			spanName := "REST " + req.Method + " " + path
+			spanName := req.Method + " " + path
 			ctx, span := tracer.Start(ctx, spanName,
 				trace.WithSpanKind(trace.SpanKindInternal),
 				trace.WithAttributes(
 					attribute.String("component", "rest"),
 					attribute.String("http.route", path),
+					attribute.String("http.path", actualPath),
 					attribute.String("http.method", req.Method),
 					attribute.String("http.url", req.URL.Path),
 					attribute.String("http.host", req.Host),
+					attribute.String("http.scheme", req.URL.Scheme),
 				),
 			)
 			defer span.End()
 
 			if query := req.URL.Query(); len(query) > 0 {
+				queryCount := 0
 				for key := range query {
 					span.SetAttributes(attribute.String("http.query."+key, "present"))
+					queryCount++
 				}
+				span.SetAttributes(attribute.Int("http.query.count", queryCount))
 			}
 
-			for _, paramName := range c.ParamNames() {
-				paramValue := c.Param(paramName)
-				if paramValue != "" {
-					span.SetAttributes(attribute.String("http.param."+paramName, paramValue))
+			paramNames := c.ParamNames()
+			if len(paramNames) > 0 {
+				for _, paramName := range paramNames {
+					paramValue := c.Param(paramName)
+					if paramValue != "" {
+						span.SetAttributes(attribute.String("http.param."+paramName, paramValue))
+					}
 				}
 			}
 
@@ -297,6 +305,10 @@ func restAPITracingMiddleware() echo.MiddlewareFunc {
 			err := next(c)
 
 			status := c.Response().Status
+			if status == 0 {
+				status = 200
+			}
+
 			span.SetAttributes(
 				attribute.Int("http.status_code", status),
 				attribute.Int64("http.response.size", c.Response().Size),
