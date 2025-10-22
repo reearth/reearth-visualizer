1467b71a1a8a3d5812508e4959b75c83f0fdb497
yuya-soneda <soneda.yuya@gmail.com>
2025-10-22 13:56:03 +0900

    fix(server): fix migration for UpdateProjectMetadataFields (#1881)

--- server/internal/infrastructure/mongo/migration/migrations.go
+++ server/internal/infrastructure/mongo/migration/migrations.go
@@ -37,5 +37,6 @@ var migrations = migration.Migrations[DBClient]{
 	250820113009: ChangeEsriToDefault,
 	251010100253: MultipleWidgetAlignSystems,
 	251010100300: AddDefaultDataAttributionForMobile,
+	251022100100: UpdateProjectMetadataFields,
 	251022133500: UpdateProjectMetadataFields,
 }
