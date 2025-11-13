a4433ff74dbb1e2521541faea49168d8408a66e4
airslice <icesunex@hotmail.com>
2025-11-13 13:37:28 +0800

    refactor: replace ua-parser-js with bowser

--- web/src/app/utils/device.ts
+++ web/src/app/utils/device.ts
@@ -1,12 +1,13 @@
-import { UAParser } from "ua-parser-js";
+import Bowser from "bowser";
 
 export type DeviceType = "mobile" | "desktop";
 
-export function isMobileDevice(uaString?: string): boolean {
-  const { device } = UAParser(uaString || navigator.userAgent);
-  return device.is("mobile");
+export function isMobileDevice(): boolean {
+  const parser = Bowser.getParser(window.navigator.userAgent);
+  const type = parser.getPlatformType(); // “desktop”, “mobile”, “tablet”, etc
+  return type === "mobile" || type === "tablet";
 }
 
-export function getDeviceType(uaString?: string): DeviceType {
-  return isMobileDevice(uaString) ? "mobile" : "desktop";
+export function getDeviceType(): DeviceType {
+  return isMobileDevice() ? "mobile" : "desktop";
 }
