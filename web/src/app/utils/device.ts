f1a095560e3fbba1639e9d0c740f24ffb73bd073
airslice <icesunex@hotmail.com>
2025-11-18 10:22:00 +0800

    refactor: keep interface and previous behavior regarding tablet

--- web/src/app/utils/device.ts
+++ web/src/app/utils/device.ts
@@ -2,12 +2,12 @@ import Bowser from "bowser";
 
 export type DeviceType = "mobile" | "desktop";
 
-export function isMobileDevice(): boolean {
-  const parser = Bowser.getParser(window.navigator.userAgent);
+export function isMobileDevice(uaString?: string): boolean {
+  const parser = Bowser.getParser(uaString || window.navigator.userAgent);
   const type = parser.getPlatformType(); // “desktop”, “mobile”, “tablet”, etc
-  return type === "mobile" || type === "tablet";
+  return type === "mobile";
 }
 
-export function getDeviceType(): DeviceType {
-  return isMobileDevice() ? "mobile" : "desktop";
+export function getDeviceType(uaString?: string): DeviceType {
+  return isMobileDevice(uaString) ? "mobile" : "desktop";
 }
