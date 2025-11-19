aef812d8f824a0881a56e9285021b65ea3fa9850
Tomokazu Tantaka <t.tantaka@eukarya.io>
2025-11-19 15:41:18 +0900

    test

--- mongo-init.js
+++ mongo-init.js
@@ -1,6 +1,13 @@
+// MongoDB Replica Set Initialization Script
+// Initialize with localhost for reliability
+// Services will connect using directConnection=true
+
 rs.initiate({
   _id: "rs0",
-  members: [{ _id: 0, host: "reearth-mongo:27017" }],
+  members: [{ _id: 0, host: "localhost:27017" }],
 });
+
+print("✓ Replica set initiated with localhost:27017");
+print("Services should connect with ?directConnection=true parameter");
 
 rs.status();
