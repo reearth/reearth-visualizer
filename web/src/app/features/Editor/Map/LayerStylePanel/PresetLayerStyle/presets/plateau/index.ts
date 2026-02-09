da807b16627582d9eaf38156e861e6e9d3249ff5
airslice <icesunex@hotmail.com>
2026-02-09 12:26:01 +0800

    refactor: renaming

--- web/src/app/features/Editor/Map/LayerStylePanel/PresetLayerStyle/presets/plateau/index.ts
+++ web/src/app/features/Editor/Map/LayerStylePanel/PresetLayerStyle/presets/plateau/index.ts
@@ -10,7 +10,7 @@ import {
   plateauLandslideRiskDebrisFlow,
   plateauLandslideRiskLandslide,
   plateauLandslideRiskSteepSlopeFailure
-} from "./landslide";
+} from "./landslideRisk";
 import { plateauLanduse } from "./landuse";
 import { plateauRiverFloodingRisk } from "./riverFloodingRisk";
 import { plateauTsunamiRisk } from "./tsunamiRisk";
