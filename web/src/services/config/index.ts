40eedac4bd00665841ff96534aa7a9e67341a9c8
lby <icesunex@hotmail.com>
2026-05-27 17:40:34 +0800

    refactor(web): revert: temporarily remove recent changes for release (#2228)

--- web/src/services/config/index.ts
+++ web/src/services/config/index.ts
@@ -16,6 +16,34 @@ export {
   logOutFromTenant
 } from "./authInfo";
 
+export type CustomProviders = {
+  imagery?: {
+    providers?: {
+      id: string;
+      url: string;
+      credit?: string;
+      maximumLevel?: number;
+      minimumLevel?: number;
+    }[];
+  };
+  terrain?: {
+    providers?: {
+      id: string;
+      url: string;
+      requestVertexNormals?: boolean;
+      requestWaterMask?: boolean;
+      credit?: string;
+    }[];
+  };
+  layers?: {
+    providers?: {
+      id: string;
+      url: string;
+      options?: Record<string, unknown>;
+    }[];
+  };
+};
+
 export type Config = {
   version?: string;
   api: string;
@@ -26,6 +54,7 @@ export type Config = {
   sentryDsn?: string;
   sentryEnv?: string;
   cesiumIonAccessToken?: string;
+  customProviders?: CustomProviders;
   earlyAccessAdmins?: string[];
   brand?: {
     logoUrl?: string;
@@ -57,6 +86,8 @@ export type Config = {
   featureCollection?: string;
   enableGqlPlayground?: boolean;
   platformUrl?: string;
+  tileServerBaseUrl?: string;
+  tileServerToken?: string;
 } & AuthInfo;
 
 declare global {
@@ -97,6 +128,9 @@ export default async function loadConfig() {
       config.unsafePluginUrls
     );
   }
+
+  // strictly remove cesium ion token
+  config.cesiumIonAccessToken = undefined;
 
   window.REEARTH_CONFIG = config;
 
