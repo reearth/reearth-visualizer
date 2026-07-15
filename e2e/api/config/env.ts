ec69c97012afbd424d917cf205fc449af33537a2
mkumbobeaty <charsbeaty@gmail.com>
2026-07-15 21:50:06 +0300

    remove unused code

--- e2e/api/config/env.ts
+++ e2e/api/config/env.ts
@@ -29,7 +29,6 @@ export const AUTH_MODE =
 export const AUTH0_DOMAIN = process.env.REEARTH_E2E_AUTH0_DOMAIN ?? "";
 export const AUTH0_AUDIENCE = process.env.REEARTH_E2E_AUTH0_AUDIENCE ?? "";
 export const AUTH0_CLIENT_ID = process.env.REEARTH_E2E_AUTH0_CLIENT_ID ?? "";
-export const AUTH0_CLIENT_SECRET = process.env.REEARTH_E2E_AUTH0_CLIENT_SECRET ?? "";
 export const MOCK_USER_ID = process.env.REEARTH_E2E_MOCK_USER_ID ?? "";
 export const SECOND_USER_EMAIL = process.env.REEARTH_E2E_SECOND_USER_EMAIL ?? "";
 
