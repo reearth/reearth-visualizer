c54a83ba5477e500cf6c1aded817b07487e10d38
lby <icesunex@hotmail.com>
2026-08-05 18:48:53 +0800

    fix(web): use inline-block only when not horizontally extended (#2364)

--- web/src/app/features/Visualizer/Crust/Plugins/Plugin/index.tsx
+++ web/src/app/features/Visualizer/Crust/Plugins/Plugin/index.tsx
@@ -171,6 +171,8 @@ export default function Plugin({
       uiContainerRef={uiContainerRef}
       isMarshalable={isMarshalable}
       pluginContext={pluginContext}
+      extensionType={extensionType}
+      extended={widget?.extended}
       onError={onError}
       onDispose={onDispose}
       onClick={onClick}
